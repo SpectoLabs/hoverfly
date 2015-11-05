ad044ba3cacf4f340e42bff51e5f509614ca83b2
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-11-05 16:19:24 +0000

    initial models file - getting redis connection

--- models.go
+++ models.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/garyburd/redigo/redis"
+	log "github.com/Sirupsen/logrus"
+)
+
+
+
+// getRedisPool returns thread safe Redis connection pool
+func getRedisPool() *redis.Pool {
+
+	// getting redis connection
+	maxConnections := 10
+	mc := os.Getenv("MaxConnections")
+	if (mc != "") {
+		maxCons, err := strconv.Atoi(mc)
+		if (err != nil) {
+			maxConnections = 10
+		} else {
+			maxConnections = maxCons
+		}
+	}
+	// getting redis client for state storing
+	redisPool := redis.NewPool(func() (redis.Conn, error) {
+		c, err := redis.Dial("tcp", AppConfig.redisAddress)
+
+		if err != nil {
+			log.WithFields(log.Fields{"Error": err.Error()}).Panic("Failed to create Redis connection pool!")
+			return nil, err
+		}
+		if AppConfig.redisPassword != "" {
+			if _, err := c.Do("AUTH", AppConfig.redisPassword); err != nil {
+				log.WithFields(log.Fields{
+					"Error": err.Error(),
+					"PasswordUsed": AppConfig.redisPassword,
+				}).Panic("Failed to authenticate to Redis!")
+				c.Close()
+				return nil, err
+			} else {
+				log.Info("Authenticated to Redis successfully! ")
+			}
+		}
+
+		return c, err
+
+		return c, err
+	}, maxConnections)
+
+	defer redisPool.Close()
+
+	return redisPool
+}
