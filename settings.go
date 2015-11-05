a58745004ba08b433c2951068c4b546ac653d76e
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-11-05 16:19:35 +0000

    getting application settings

--- settings.go
+++ settings.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"os"
+)
+// Initial structure of configuration
+type Configuration struct {
+	redisAddress  string
+	redisPassword string
+}
+
+// AppCondig stores application configuration
+var AppConfig Configuration
+
+func init() {
+	// getting redis connection
+	redisAddress := os.Getenv("RedisAddress")
+	if (redisAddress == "") {
+		redisAddress = ":6379"
+	}
+	AppConfig.redisAddress = redisAddress
+	// getting redis password
+	AppConfig.redisPassword = os.Getenv("RedisPassword")
+}
