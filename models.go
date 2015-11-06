b5b4caad23449ea007325330d5e9d1429d2d2685
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-11-06 16:42:01 +0000

    methods to do requests on behalf of client

--- models.go
+++ models.go
@@ -11,17 +11,76 @@ import (
 
 type DBClient struct {
 	pool *redis.Pool
+	http *http.Client
 }
 
-func (d *DBClient) recordRequest(r *http.Request) {
-	log.Info("Recording request")
+// res structure hold response body from external service, body is not decoded and is supposed
+// to be bytes, however headers should provide all required information for later decoding
+// by the client.
+type res struct {
+	Status  int `json:"status"`
+	Body    []byte `json:"body"`
+	Headers map[string]string `json:"headers"`
 }
+
+// recordRequest saves request for later playback
+func (d *DBClient) recordRequest(req *http.Request) (*http.Response, error) {
+	log.Info("Recording request")
+
+	// forwarding request
+	resp, err := d.doRequest(req)
+
+	// record request here
+
+	// return new response or error here
+	return resp, err
+
+	//	c := d.pool.Get()
+	//	defer c.Close()
+	//
+	//	_, err := c.Do("SET", r.URL.Path, r.Body)
+	//
+	//	if err != nil {
+	//		log.WithFields(log.Fields{
+	//			"error": err.Error(),
+	//		}).Error("Failed to record request...")
+	//	} else {
+	//		log.WithFields(log.Fields{
+	//		}).Info("Request recorded!")
+	//	}
+}
+
 
 func (d *DBClient) getResponse(r *http.Request) *http.Response {
 	log.Info("Returning response")
 	return nil
 }
 
+// doRequest performs original request and returns response that should be returned to client and error (if there is one)
+func (d *DBClient) doRequest(request *http.Request) (*http.Response, error) {
+	// We can't have this set. And it only contains "/pkg/net/http/" anyway
+	request.RequestURI = ""
+
+	resp, err := d.http.Do(request)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+			"host": request.Host,
+			"method": request.Method,
+			"path": request.URL.Path,
+		}).Error("Could not forward request.")
+		return nil, err
+	}
+
+	log.WithFields(log.Fields{
+	}).Info("Request forwarded!")
+
+	resp.Header.Set("Gen-proxy", "was here")
+	return resp, nil
+
+
+}
 
 // getRedisPool returns thread safe Redis connection pool
 func getRedisPool() *redis.Pool {
