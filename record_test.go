047df1e512473924055dc3549c5ceeabe84141a1
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-11-06 17:38:28 +0000

    initial testing for record

--- record_test.go
+++ record_test.go
@@ -1,1 +1,18 @@
-package main_test
+package main
+
+import (
+	"net/http"
+   "testing"
+)
+
+func TestRecord(t *testing.T){
+	server, client := testTools(200, `{'message': 'here'}`)
+	defer server.Close()
+
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	expect(t, err, nil)
+
+	response, err := client.recordRequest(req)
+
+	expect(t, response.Header.Get("Gen-proxy"), "Was-Here")
+}
