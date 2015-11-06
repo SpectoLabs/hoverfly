8ee6c1bd454855e5d8c35a8bd58a4b70baabebbf
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-11-06 17:38:38 +0000

    testing utilities

--- test_tools.go
+++ test_tools.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+// Client structure to be injected into functions to perform HTTP calls
+type Client struct {
+	HTTPClient *http.Client
+}
+
+func expect(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
+
+func refute(t *testing.T, a interface{}, b interface{}) {
+	if a == b {
+		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
+	}
+}
+
+func testTools(code int, body string) (*httptest.Server, *DBClient) {
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(code)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, body)
+	}))
+
+	tr := &http.Transport{
+		Proxy: func(req *http.Request) (*url.URL, error) {
+			return url.Parse(server.URL)
+		},
+	}
+	dbClient := &DBClient{http: &http.Client{Transport: tr} }
+
+
+//	httpClient := &http.Client{Transport: tr}
+
+//	client := &Client{httpClient}
+	return server, dbClient
+}
