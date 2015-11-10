b883c833ce8d7f4a693ac01e3cb98acfe972a4ff
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-11-10 15:12:38 +0000

    destination regexp added

--- genproxy.go
+++ genproxy.go
@@ -1,64 +1,68 @@
 package main
 
 import (
-	"github.com/elazarl/goproxy"
 	log "github.com/Sirupsen/logrus"
+	"github.com/elazarl/goproxy"
 
+	"flag"
+	"fmt"
 	"net/http"
 	"os"
-	"flag"
 	"regexp"
-	"fmt"
 )
 
-
 const DefaultPort = ":8500"
-
 
 func main() {
 	// getting proxy configuration
 	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
 	record := flag.Bool("record", false, "should proxy record")
-	externalSystem := flag.String("target", "mirage.readthedocs.org", "destination host to catch")
+	destination := flag.String("source", "^.*:80$", "source IP to catch")
 	flag.Parse()
 
 	// getting settings
 	initSettings()
 
-
 	// getting default database
 	port := os.Getenv("ProxyPort")
-	if (port == "") {
+	if port == "" {
 		port = DefaultPort
 	} else {
 		port = fmt.Sprintf(":%s", port)
 	}
 
-
 	// Output to stderr instead of stdout, could also be a file.
 	log.SetOutput(os.Stderr)
 	log.SetFormatter(&log.TextFormatter{})
 
-
 	// app starting
 	log.WithFields(log.Fields{
 		"RedisAddress": AppConfig.redisAddress,
-		"ProxyPort": port,
+		"Destination":  *destination,
+		"ProxyPort":    port,
 	}).Info("app is starting")
 
 	// getting redis connection
 	d := DBClient{pool: getRedisPool(),
-	              http: &http.Client{}}
+		http: &http.Client{}}
 
 	// creating proxy
 	proxy := goproxy.NewProxyHttpServer()
 
 	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile("^.*$"))).
-	HandleConnect(goproxy.AlwaysMitm)
+		HandleConnect(goproxy.AlwaysMitm)
 
+	proxy.OnRequest().DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			log.WithFields(log.Fields{
+				"SourceIP":    r.RemoteAddr,
+				"Destination": r.RemoteAddr,
+			}).Info("Got request")
+			return r, nil
+		})
 	// hijacking plain connections
-	proxy.OnRequest(goproxy.DstHostIs(*externalSystem)).DoFunc(
-		func(r *http.Request,ctx *goproxy.ProxyCtx)(*http.Request,*http.Response) {
+	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile(*destination))).DoFunc(
+		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
 
 			log.Info("connection found......")
 			log.Info(fmt.Sprintf("Url path:  %s", r.URL.Path))
@@ -69,9 +73,9 @@ func main() {
 				if err != nil {
 					// something bad happened, passing through
 					return r, nil
-				} else{
+				} else {
 					// discarding original requests and returns supplied response
-					return r,newResponse
+					return r, newResponse
 				}
 
 			} else {
@@ -80,7 +84,6 @@ func main() {
 				return r, nil
 			}
 		})
-
 
 	proxy.Verbose = *verbose
 	log.Fatal(http.ListenAndServe(port, proxy))
