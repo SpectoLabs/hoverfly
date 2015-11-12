57168601ae10fb3809bd62fb0c64a7d2074a1a13
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-11-12 14:54:45 +0000

    added settings for admin interface port

--- settings.go
+++ settings.go
@@ -3,10 +3,12 @@ package main
 import (
 	"os"
 )
+
 // Initial structure of configuration
 type Configuration struct {
-	redisAddress  string
-	redisPassword string
+	redisAddress   string
+	redisPassword  string
+	adminInterface string
 }
 
 // AppCondig stores application configuration
@@ -15,10 +17,13 @@ var AppConfig Configuration
 func initSettings() {
 	// getting redis connection
 	redisAddress := os.Getenv("RedisAddress")
-	if (redisAddress == "") {
+	if redisAddress == "" {
 		redisAddress = ":6379"
 	}
 	AppConfig.redisAddress = redisAddress
 	// getting redis password
 	AppConfig.redisPassword = os.Getenv("RedisPassword")
+
+	// admin interface port
+	AppConfig.adminInterface = ":8888"
 }
