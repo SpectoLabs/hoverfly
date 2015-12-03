53bec4f9777b5d3844ca0833c26ed4d60a640fc5
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-12-03 11:13:33 +0000

    test for ruby echo

--- middleware_test.go
+++ middleware_test.go
@@ -17,3 +17,18 @@ func TestChangeBodyMiddleware(t *testing.T) {
 	expect(t, err, nil)
 	expect(t, newPayload.Response.Body, "body was replaced by middleware")
 }
+
+func TestMalformedPayloadMiddleware(t *testing.T) {
+	command := "./examples/middleware/ruby_echo/echo.rb"
+
+	resp := response{Status: 201, Body: "original body"}
+	req := requestDetails{Path: "/", Method: "GET", Destination: "hostname-x", Query: ""}
+
+	payload := Payload{Response: resp, Request: req}
+
+	newPayload, err := ExecuteMiddleware(command, payload)
+
+	expect(t, err, nil)
+	expect(t, newPayload.Response.Body, "original body")
+}
+
