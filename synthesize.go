3341a3c8e28179482af623d9ee583b1a040d9d93
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-12-03 16:36:31 +0000

    synthesize mode

--- synthesize.go
+++ synthesize.go
@@ -1,1 +1,40 @@
-package hoverfly
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+)
+
+// synthesizeResponse calls middleware to populate response data, nothing gets pass proxy
+func synthesizeResponse(req *http.Request) *http.Response {
+
+	b := bufio.NewScanner(req.Body)
+
+	bodyStr := b.Text()
+
+	request := requestDetails{
+		Path:        req.URL.Path,
+		Method:      req.Method,
+		Destination: req.URL.Path,
+		Query:       req.URL.RawQuery,
+		Body:        bodyStr,
+		RemoteAddr:  req.RemoteAddr,
+		Headers:     req.Header,
+	}
+	payload := Payload{Request: request}
+
+	c := NewConstructor(req, payload)
+
+	err := c.ApplyMiddleware(AppConfig.middleware)
+	if err != nil {
+		var errorPayload Payload
+		errorPayload.Response.Status = 503
+		errorPayload.Response.Body = fmt.Sprintf("Middleware error: %s", err.Error())
+		c.payload = errorPayload
+	}
+
+	response := c.reconstructResponse()
+	return response
+
+}
