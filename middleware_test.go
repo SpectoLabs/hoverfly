24345945196c2cab4470b97868f18a7173c498b1
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-12-03 11:16:47 +0000

    test for go example

--- middleware_test.go
+++ middleware_test.go
@@ -32,3 +32,18 @@ func TestMalformedPayloadMiddleware(t *testing.T) {
 	expect(t, newPayload.Response.Body, "original body")
 }
 
+func TestMakeCustom404(t *testing.T) {
+	command := "go run ./examples/middleware/go_example/change_to_custom_404.go"
+
+	resp := response{Status: 201, Body: "original body"}
+	req := requestDetails{Path: "/", Method: "GET", Destination: "hostname-x", Query: ""}
+
+	payload := Payload{Response: resp, Request: req}
+
+	newPayload, err := ExecuteMiddleware(command, payload)
+
+	expect(t, err, nil)
+	expect(t, newPayload.Response.Body, "Custom body here")
+	expect(t, newPayload.Response.Status, 404)
+	expect(t, newPayload.Response.Headers["middleware"][0], "changed response")
+}
