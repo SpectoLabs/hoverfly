a39a61c8c7d5c9643d534068a13adb2bd0d9f3d9
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-12-03 16:36:54 +0000

    constructor struct, init function

--- response.go
+++ response.go
@@ -1,1 +1,20 @@
-package hoverfly
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+type Constructor struct {
+	request *http.Request
+	payload Payload
+}
+
+func NewConstructor(req *http.Request, payload Payload) Constructor {
+	c := Constructor{request: req, payload: payload}
+	return c
+}
+
