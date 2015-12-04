b1f560d085dc222ffc2bd0845e8826ce2069fd2c
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-12-04 11:36:40 +0000

    separated model and cache tests

--- models_test.go
+++ models_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/garyburd/redigo/redis"
 	"net/http"
 	"os"
 	"testing"
@@ -34,26 +33,6 @@ func TestRecordHeader(t *testing.T) {
 	expect(t, response.Header.Get("Gen-proxy"), "Was-Here")
 }
 
-// TestRecordingToCache tests cache wrapper get/set/delete operations
-func TestRecordingToCache(t *testing.T) {
-
-	server, dbClient := testTools(200, `{'message': 'here'}`)
-	defer server.Close()
-	defer dbClient.cache.pool.Close()
-
-	dbClient.cache.set("some_key", "value")
-
-	value, err := redis.String(dbClient.cache.get("some_key"))
-
-	expect(t, err, nil)
-
-	expect(t, string(value), "value")
-
-	err = dbClient.cache.delete("some_key")
-
-	expect(t, err, nil)
-}
-
 // TestRequestFingerprint tests whether we get correct request ID
 func TestRequestFingerprint(t *testing.T) {
 
@@ -63,34 +42,6 @@ func TestRequestFingerprint(t *testing.T) {
 	fp := getRequestFingerprint(req)
 
 	expect(t, fp, "92a65ed4ca2b7100037a4cba9afd15ea")
-
-}
-
-// TestGetAllRecords - tests recording and then getting responses
-func TestGetAllRecords(t *testing.T) {
-
-	server, dbClient := testTools(201, `{'message': 'here'}`)
-	defer server.Close()
-	defer dbClient.cache.pool.Close()
-
-	// inserting some payloads
-	for i := 0; i < 5; i++ {
-		req, err := http.NewRequest("GET", fmt.Sprintf("http://example.com/q=%d", i), nil)
-		expect(t, err, nil)
-		dbClient.captureRequest(req)
-	}
-
-	// getting all keys
-	keys, _ := dbClient.cache.getAllKeys()
-	expect(t, len(keys) > 0, true)
-	// getting requests
-	payloads, err := dbClient.getAllRecords()
-	expect(t, err, nil)
-
-	for _, payload := range payloads {
-		expect(t, payload.Request.Method, "GET")
-		expect(t, payload.Response.Status, 201)
-	}
 
 }
 
