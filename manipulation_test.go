ab4588da43b2160c2e1551035f2683ac13fed628
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-12-05 23:02:03 +0000

    some tests

--- manipulation_test.go
+++ manipulation_test.go
@@ -12,6 +12,7 @@ func TestReconstructRequest(t *testing.T) {
 	request := requestDetails{
 		Path:        "/random-path",
 		Method:      "POST",
+		Query:       "?foo=bar",
 		Destination: "changed.destination.com",
 	}
 	payload := Payload{Request: request}
@@ -21,4 +22,16 @@ func TestReconstructRequest(t *testing.T) {
 	expect(t, newRequest.Method, "POST")
 	expect(t, newRequest.URL.Path, "/random-path")
 	expect(t, newRequest.Host, "changed.destination.com")
+	expect(t, newRequest.URL.RawQuery, "?foo=bar")
+}
+
+func TestReconstructRequestEmptyPayload(t *testing.T) {
+	req, _ := http.NewRequest("GET", "http://example.com", nil)
+
+	payload := Payload{}
+	c := NewConstructor(req, payload)
+	newRequest := c.reconstructRequest()
+	expect(t, newRequest.Method, "")
+	expect(t, newRequest.Host, "")
+
 }
