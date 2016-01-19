05a8f5e8c5e9b4bc123e6c7f755c2be0356a3fc8
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-19 16:44:18 +0000

    test get all records

---
+++ admin_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetAllRecords(t *testing.T) {
+	server, dbClient := testTools(200, `{'message': 'here'}`)
+	defer server.Close()
+	m := getBoneRouter(*dbClient)
+
+	req, err := http.NewRequest("GET", "/records", nil)
+	expect(t, err, nil)
+
+	//The response recorder used to record HTTP responses
+	respRec := httptest.NewRecorder()
+
+	m.ServeHTTP(respRec, req)
+
+	expect(t, respRec.Code, http.StatusOK)
+
+	body, err := ioutil.ReadAll(respRec.Body)
+
+	rr := recordedRequests{}
+	err = json.Unmarshal(body, &rr)
+
+	expect(t, len(rr.Data), 0)
+}
