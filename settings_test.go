8ae8c6163a39d13f58413d394d34bfa2a04709d2
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-19 14:01:56 +0000

    testing admin port

--- settings_test.go
+++ settings_test.go
@@ -1,1 +1,17 @@
 package hoverfly
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestSettingsAdminPortEnv(t *testing.T) {
+	defer os.Setenv("AdminPort", "")
+
+	os.Setenv("AdminPort", "5555")
+
+	cfg := InitSettings()
+	expect(t, cfg.adminPort, "5555")
+}
+
