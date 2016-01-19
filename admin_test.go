a6c391277e43b530e0b33dd3e32c0b4ff27d0079
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-19 17:19:57 +0000

    counting records

--- admin_test.go
+++ admin_test.go
@@ -85,3 +85,35 @@ func TestGetRecordsCount(t *testing.T) {
 
 	expect(t, rc.Count, 0)
 }
+
+func TestGetRecordsCountWRecords(t *testing.T) {
+	server, dbClient := testTools(200, `{'message': 'here'}`)
+	defer server.Close()
+
+	// inserting some payloads
+	for i := 0; i < 5; i++ {
+		req, err := http.NewRequest("GET", fmt.Sprintf("http://example.com/q=%d", i), nil)
+		expect(t, err, nil)
+		dbClient.captureRequest(req)
+	}
+	// performing query
+	m := getBoneRouter(*dbClient)
+
+	req, err := http.NewRequest("GET", "/count", nil)
+	expect(t, err, nil)
+
+	//The response recorder used to record HTTP responses
+	respRec := httptest.NewRecorder()
+
+	m.ServeHTTP(respRec, req)
+
+	expect(t, respRec.Code, http.StatusOK)
+
+	body, err := ioutil.ReadAll(respRec.Body)
+
+	rc := recordsCount{}
+	err = json.Unmarshal(body, &rc)
+
+	expect(t, rc.Count, 5)
+}
+
