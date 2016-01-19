167bcaa741b3bccf3fe38dd580bd90a0bdce4b19
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-19 16:44:39 +0000

    inserting some records and testing again

--- admin_test.go
+++ admin_test.go
@@ -31,3 +31,34 @@ func TestGetAllRecords(t *testing.T) {
 
 	expect(t, len(rr.Data), 0)
 }
+
+func TestGetAllRecordsWRecords(t *testing.T) {
+	server, dbClient := testTools(200, `{'message': 'here'}`)
+	defer server.Close()
+
+	// inserting some payloads
+	for i := 0; i < 5; i++ {
+		req, err := http.NewRequest("GET", fmt.Sprintf("http://example.com/q=%d", i), nil)
+		expect(t, err, nil)
+		dbClient.captureRequest(req)
+	}
+	// performing query
+	m := getBoneRouter(*dbClient)
+
+	req, err := http.NewRequest("GET", "/records", nil)
+	expect(t, err, nil)
+
+	//The response recorder used to record HTTP responses
+	respRec := httptest.NewRecorder()
+
+	m.ServeHTTP(respRec, req)
+
+	expect(t, respRec.Code, http.StatusOK)
+
+	body, err := ioutil.ReadAll(respRec.Body)
+
+	rr := recordedRequests{}
+	err = json.Unmarshal(body, &rr)
+
+	expect(t, len(rr.Data), 5)
+}
