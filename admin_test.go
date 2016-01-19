408ff63e5e0795427a6f299a75f1f42057efb3d9
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-19 17:20:12 +0000

    testing export/import

--- admin_test.go
+++ admin_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -117,3 +118,45 @@ func TestGetRecordsCountWRecords(t *testing.T) {
 	expect(t, rc.Count, 5)
 }
 
+func TestExportImportRecords(t *testing.T) {
+	server, dbClient := testTools(200, `{'message': 'here'}`)
+	defer server.Close()
+	m := getBoneRouter(*dbClient)
+
+	// inserting some payloads
+	for i := 0; i < 5; i++ {
+		req, err := http.NewRequest("GET", fmt.Sprintf("http://example.com/q=%d", i), nil)
+		expect(t, err, nil)
+		dbClient.captureRequest(req)
+	}
+
+	req, err := http.NewRequest("GET", "/records", nil)
+	expect(t, err, nil)
+
+	//The response recorder used to record HTTP responses
+	respRec := httptest.NewRecorder()
+
+	m.ServeHTTP(respRec, req)
+
+	expect(t, respRec.Code, http.StatusOK)
+
+	body, err := ioutil.ReadAll(respRec.Body)
+
+	// deleting records
+	err = dbClient.cache.DeleteBucket(dbClient.cache.requestsBucket)
+	expect(t, err, nil)
+
+	// using body to import records again
+	importReq, err := http.NewRequest("POST", "/records", ioutil.NopCloser(bytes.NewBuffer(body)))
+	//The response recorder used to record HTTP responses
+	importRec := httptest.NewRecorder()
+
+	m.ServeHTTP(importRec, importReq)
+	expect(t, importRec.Code, http.StatusOK)
+
+	// records should be overwritten
+	payloads, err := dbClient.cache.GetAllRequests()
+	expect(t, err, nil)
+	expect(t, len(payloads), 5)
+
+}
