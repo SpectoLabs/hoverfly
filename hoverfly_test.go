74018bc7b2d8ec4d8f3cf4b06cf4331122fd113b
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-21 16:57:13 +0000

    test capture request

--- hoverfly_test.go
+++ hoverfly_test.go
@@ -13,3 +13,20 @@ func TestGetNewHoverflyCheckConfig(t *testing.T) {
 
 	expect(t, dbClient.cfg, cfg)
 }
+
+func TestProcessCaptureRequest(t *testing.T) {
+	server, dbClient := testTools(201, `{'message': 'here'}`)
+	defer server.Close()
+	defer dbClient.cache.DeleteBucket(dbClient.cache.requestsBucket)
+
+	r, err := http.NewRequest("GET", "http://somehost.com", nil)
+	expect(t, err, nil)
+
+	dbClient.cfg.SetMode("capture")
+
+	req, resp := dbClient.processRequest(r)
+
+	refute(t, req, nil)
+	refute(t, resp, nil)
+	expect(t, resp.StatusCode, 201)
+}
