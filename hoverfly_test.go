4390f1a06d065b5e04cffadc428bc925b4c2c8c9
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-21 18:52:45 +0000

    virtualize test

--- hoverfly_test.go
+++ hoverfly_test.go
@@ -30,3 +30,28 @@ func TestProcessCaptureRequest(t *testing.T) {
 	refute(t, resp, nil)
 	expect(t, resp.StatusCode, 201)
 }
+
+func TestProcessVirtualizeRequest(t *testing.T) {
+	server, dbClient := testTools(201, `{'message': 'here'}`)
+	defer server.Close()
+	defer dbClient.cache.DeleteBucket(dbClient.cache.requestsBucket)
+
+	r, err := http.NewRequest("GET", "http://somehost.com", nil)
+	expect(t, err, nil)
+
+	// capturing
+	dbClient.cfg.SetMode("capture")
+	req, resp := dbClient.processRequest(r)
+
+	refute(t, req, nil)
+	refute(t, resp, nil)
+	expect(t, resp.StatusCode, 201)
+
+	// virtualizing
+	dbClient.cfg.SetMode("virtualize")
+	newReq, newResp := dbClient.processRequest(r)
+
+	refute(t, newReq, nil)
+	refute(t, newResp, nil)
+	expect(t, newResp.StatusCode, 201)
+}
