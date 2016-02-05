3f1c49dddec913b836c24139b19b6a92b5e8d7b5
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-05 12:33:53 +0000

    license, some explanation

--- cmd/hoverfly/main.go
+++ cmd/hoverfly/main.go
@@ -1,3 +1,22 @@
+// Copyright 2015 SpectoLabs. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// hoverfly is an HTTP/s proxy configurable via flags/environment variables/admin HTTP API
+//
+// this proxy can be dynamically configured through HTTP calls when it's running, to change modes,
+// export and import requests.
+
 package main
 
 import (
@@ -108,7 +127,7 @@ func main() {
 
 	// start metrics registry flush
 	if *metrics {
-		go dbClient.Counter.Init()
+		dbClient.Counter.Init()
 	}
 
 	log.Warn(http.ListenAndServe(fmt.Sprintf(":%s", cfg.ProxyPort), proxy))
