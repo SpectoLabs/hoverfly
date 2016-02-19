8b5ef4842048eb829aafec7d443bce6df1b5f450
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:29:17 +0000

    loading settings based on env

--- authentication/settings.go
+++ authentication/settings.go
@@ -19,3 +19,30 @@ type Settings struct {
 	JWTExpirationDelta int
 }
 
+
+func LoadSettingsByEnv(env string) {
+	content, err := ioutil.ReadFile(environments[env])
+	if err != nil {
+		fmt.Println("Error while reading config file", err)
+	}
+	settings = Settings{}
+	jsonErr := json.Unmarshal(content, &settings)
+	if jsonErr != nil {
+		fmt.Println("Error while parsing config file", jsonErr)
+	}
+}
+
+func GetEnvironment() string {
+	return env
+}
+
+func Get() Settings {
+	if &settings == nil {
+		Init()
+	}
+	return settings
+}
+
+func IsTestEnvironment() bool {
+	return env == "tests"
+}
