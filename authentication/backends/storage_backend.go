cd86cbc216fe130e09913e2950a4b40e05813b93
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:15:23 +0000

    creating new boltdb auth backend

--- authentication/backends/storage_backend.go
+++ authentication/backends/storage_backend.go
@@ -11,3 +11,25 @@ type AuthBackend interface {
 	GetValue(key []byte) ([]byte, error)
 	Delete(key []byte) error
 }
+
+func NewBoltDBAuthBackend(db *bolt.DB, tokenBucket, userBucket []byte) *BoltAuth {
+	return &BoltAuth{
+		DS:          db,
+		TokenBucket: []byte(tokenBucket),
+		UserBucket:  []byte(userBucket),
+	}
+}
+
+// UserBucketName - default name for BoltDB bucket that stores user info
+const UserBucketName = "authbucket"
+
+// TokenBucketName
+const TokenBucketName = "tokenbucket"
+
+// BoltCache - container to implement Cache instance with BoltDB backend for storage
+type BoltAuth struct {
+	DS          *bolt.DB
+	TokenBucket []byte
+	UserBucket  []byte
+}
+
