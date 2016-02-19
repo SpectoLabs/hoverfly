58201f80c9dc6cc4fda42a48ebe6c1f3c25ddf27
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:30:38 +0000

    JWTAuthenticationBackend container

--- authentication/jwt_backend.go
+++ authentication/jwt_backend.go
@@ -1,1 +1,15 @@
 package authentication
+
+type JWTAuthenticationBackend struct {
+	privateKey  *rsa.PrivateKey
+	PublicKey   *rsa.PublicKey
+	AuthBackend backends.AuthBackend
+}
+
+const (
+	tokenDuration = 72
+	expireOffset  = 3600
+)
+
+var authBackendInstance *JWTAuthenticationBackend = nil
+
