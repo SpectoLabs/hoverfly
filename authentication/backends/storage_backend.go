d5daed2047259ed8c447e10c325fdf2f23131577
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:11:48 +0000

    auth backend interface

--- authentication/backends/storage_backend.go
+++ authentication/backends/storage_backend.go
@@ -1,1 +1,13 @@
 package backends
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/boltdb/bolt"
+)
+
+type AuthBackend interface {
+	SetValue(key, value []byte) error
+	GetValue(key []byte) ([]byte, error)
+	Delete(key []byte) error
+}
