f08db4168347c45f2c4f76709ccd43c1d645579d
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:17:06 +0000

    getting new auth middleware

--- authentication/middlewares.go
+++ authentication/middlewares.go
@@ -3,3 +3,7 @@ package authentication
 type AuthMiddleware struct {
 	AB backends.AuthBackend
 }
+
+func GetNewAuthenticationMiddleware(authBackend backends.AuthBackend) *AuthMiddleware {
+	return &AuthMiddleware{AB: authBackend}
+}
