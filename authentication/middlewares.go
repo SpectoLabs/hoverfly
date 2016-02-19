5d35162695e7cffeab886346eac8d219567759c8
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:17:22 +0000

    enforcing token auth

--- authentication/middlewares.go
+++ authentication/middlewares.go
@@ -7,3 +7,21 @@ type AuthMiddleware struct {
 func GetNewAuthenticationMiddleware(authBackend backends.AuthBackend) *AuthMiddleware {
 	return &AuthMiddleware{AB: authBackend}
 }
+
+func (a *AuthMiddleware) RequireTokenAuthentication(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+	authBackend := InitJWTAuthenticationBackend(a.AB)
+
+	token, err := jwt.ParseFromRequest(req, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		} else {
+			return authBackend.PublicKey, nil
+		}
+	})
+
+	if err == nil && token.Valid && !authBackend.IsInBlacklist(req.Header.Get("Authorization")) {
+		next(rw, req)
+	} else {
+		rw.WriteHeader(http.StatusUnauthorized)
+	}
+}
