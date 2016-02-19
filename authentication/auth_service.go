d7902abcca6d6da58c90d09c0b544e0d2c8676dd
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:10:03 +0000

    initial user, tokenauth containers

--- authentication/auth_service.go
+++ authentication/auth_service.go
@@ -1,1 +1,18 @@
 package authentication
+
+import (
+	"encoding/json"
+	"github.com/SpectoLabs/hoverfly/authentication/backends"
+	jwt "github.com/dgrijalva/jwt-go"
+	"net/http"
+)
+
+type TokenAuthentication struct {
+	Token string `json:"token" form:"token"`
+}
+
+type User struct {
+	UUID     string `json:"uuid" form:"-"`
+	Username string `json:"username" form:"username"`
+	Password string `json:"password" form:"password"`
+}
