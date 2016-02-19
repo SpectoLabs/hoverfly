3bc1fe5c24e3a067287b82a98058af93cf510b2e
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:28:56 +0000

    settings, environment settings

--- authentication/settings.go
+++ authentication/settings.go
@@ -1,1 +1,21 @@
 package authentication
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var environments = map[string]string{
+	"production":    "../../authentication/settings/prod.json",
+	"preproduction": "../../authentication/settings/pre.json",
+	"tests":         "../../authentication/settings/tests.json",
+}
+
+type Settings struct {
+	PrivateKeyPath     string
+	PublicKeyPath      string
+	JWTExpirationDelta int
+}
+
