8d0e82ce8a90c45de723b50a1e1bae9c91d3f765
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:30:58 +0000

    token encoding/decoding

--- authentication/jwt_backend.go
+++ authentication/jwt_backend.go
@@ -13,3 +13,29 @@ const (
 
 var authBackendInstance *JWTAuthenticationBackend = nil
 
+// Token - container for jwt.Token for encoding
+type Token struct {
+	Token *jwt.Token
+}
+
+func (t *Token) Encode() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	err := enc.Encode(t)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func decodeToken(data []byte) (*Token, error) {
+	var t *Token
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	err := dec.Decode(&t)
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
+}
+
