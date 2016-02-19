5e0e9d76f2e8060fc8a15103c309171ca5c1df31
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-19 15:48:59 +0000

    login controller

--- authentication/controllers/auth_controller.go
+++ authentication/controllers/auth_controller.go
@@ -7,3 +7,15 @@ type AuthController struct {
 func GetNewAuthenticationController(authBackend backends.AuthBackend) *AuthController {
 	return &AuthController{AB: authBackend}
 }
+
+func (a *AuthController) Login(w http.ResponseWriter, r *http.Request) {
+	requestUser := new(authentication.User)
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&requestUser)
+
+	responseStatus, token := authentication.Login(requestUser, a.AB)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(responseStatus)
+	w.Write(token)
+}
+
