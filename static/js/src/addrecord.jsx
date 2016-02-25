c20eca158a4e842c86b5ac3936f5a73e429f9304
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-02-25 14:19:42 +0000

    cleanup

--- static/js/src/addrecord.jsx
+++ static/js/src/addrecord.jsx
@@ -8,15 +8,6 @@ var modalStyle = {
 };
 
 
-var backdropStyle = {
-    backgroundColor: 'red'
-};
-
-var contentStyle = {
-    backgroundColor: 'blue',
-    height: '100%'
-};
-
 let AddRecordComponent = React.createClass({
     displayName: "AddNewRecordComponent",
 
@@ -36,55 +27,54 @@ let AddRecordComponent = React.createClass({
                         <form className="form-signin" role="form" method="post" action="/add">
 
                             <h4 className="form-signin-heading">Request info</h4>
-                            <label for="inputDestination" className="sr-only">Destination</label>
+                            <label htmlFor="inputDestination" className="sr-only">Destination</label>
                             <input id="inputDestination" name="inputDestination" className="form-control"
                                    placeholder="www.google.com" required autofocus/>
 
-                            <label for="inputMethod" className="sr-only">Method</label>
+                            <label htmlFor="inputMethod" className="sr-only">Method</label>
                             <input id="inputMethod" name="inputMethod" className="form-control" placeholder="GET"
                                    required/>
 
-                            <label for="inputPath" className="sr-only">Path</label>
+                            <label htmlFor="inputPath" className="sr-only">Path</label>
                             <input id="inputPath" name="inputPath" className="form-control"
                                    placeholder="/api/something"/>
 
-                            <label for="inputQuery" className="sr-only">Query</label>
+                            <label htmlFor="inputQuery" className="sr-only">Query</label>
                             <input id="inputQuery" name="inputQuery" className="form-control"
                                    placeholder="?search=forthis"/>
 
 
-                            <label for="inputRequestBody">Request body</label>
-                            <textarea className="u-full-width" value="<Request> value here </Request"
+                            <label htmlFor="inputRequestBody">Request body</label>
+                            <textarea className="u-full-width"
+                                      name="inputRequestBody"
                                       id="inputRequestBody"> </textarea>
 
 
                             <h4 className="form-signin-heading">Response info</h4>
 
-                            <label for="inputContentType">Content Type</label>
-                            <select className="u-full-width" id="inputContentType" name="inputContentType" required>
+                            <label htmlFor="inputContentType">Content Type</label>
+                            <select className="u-full-width" id="inputContentType" name="inputContentType" value="json" required>
                                 <option value="xml">application/xml</option>
                                 <option value="json">application/json</option>
                                 <option value="text">text/html</option>
                             </select>
 
 
-                            <label for="inputResponseStatusCode" className="sr-only">Status Code</label>
+                            <label htmlFor="inputResponseStatusCode" className="sr-only">Status Code</label>
                             <input type="number" id="inputResponseStatusCode"
                                    name="inputResponseStatusCode" className="form-control" placeholder="200"
                                    required/>
 
-                            <label for="inputResponseBody">Response body</label>
-                            <textarea className="u-full-width" value="<Response> value here </Response"
+                            <label htmlFor="inputResponseBody">Response body</label>
+                            <textarea className="u-full-width"
                                       name="inputResponseBody"
                                       id="inputResponseBody"> </textarea>
 
                             <div className="row">
-                                <button className="btn btn-lg btn-primary btn-block" type="submit">Add</button>
-                                <button onClick={this.hideModal}>Close</button>
+                                <button className="button-primary" type="submit">Add</button>  <button onClick={this.hideModal}>Close</button>
                             </div>
 
                         </form>
-
                     </div>
 
                 </Modal>
