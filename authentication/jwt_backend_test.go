84caaa26d035d0a8c78d4dbc3b3850961a67dd47
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-03-04 14:48:50 +0000

    gofmt

--- authentication/jwt_backend_test.go
+++ authentication/jwt_backend_test.go
@@ -1,13 +1,12 @@
 package authentication
 
 import (
-    "testing"
-    "os"
-    
+	"os"
+	"testing"
+
 	"github.com/SpectoLabs/hoverfly/authentication/backends"
 	"github.com/dgrijalva/jwt-go"
 )
-
 
 // TestMain prepares database for testing and then performs a cleanup
 func TestMain(m *testing.M) {
@@ -20,69 +19,69 @@ func TestMain(m *testing.M) {
 }
 
 func TestGenerateToken(t *testing.T) {
-    ab := backends.NewBoltDBAuthBackend(TestDB, []byte(backends.TokenBucketName), []byte(backends.UserBucketName))
-    jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
-    
-    token, err := jwtBackend.GenerateToken("userUUIDhereVeryLong", "userx")
-    expect(t, err, nil)
-    expect(t, len(token) > 0, true)
+	ab := backends.NewBoltDBAuthBackend(TestDB, []byte(backends.TokenBucketName), []byte(backends.UserBucketName))
+	jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
+
+	token, err := jwtBackend.GenerateToken("userUUIDhereVeryLong", "userx")
+	expect(t, err, nil)
+	expect(t, len(token) > 0, true)
 }
 
 func TestAuthenticate(t *testing.T) {
-    ab := backends.NewBoltDBAuthBackend(TestDB, []byte(backends.TokenBucketName), []byte(backends.UserBucketName))
-    username := []byte("beloveduser")
-    passw := []byte("12345")
-    ab.AddUser(username, passw, true)
-    
-    jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
-    user := &backends.User{
-        Username: string(username), 
-        Password: string(passw),
-         UUID: "uuid_here",
-        IsAdmin: true}
-        
-     success := jwtBackend.Authenticate(user)
-     expect(t, success, true)
+	ab := backends.NewBoltDBAuthBackend(TestDB, []byte(backends.TokenBucketName), []byte(backends.UserBucketName))
+	username := []byte("beloveduser")
+	passw := []byte("12345")
+	ab.AddUser(username, passw, true)
+
+	jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
+	user := &backends.User{
+		Username: string(username),
+		Password: string(passw),
+		UUID:     "uuid_here",
+		IsAdmin:  true}
+
+	success := jwtBackend.Authenticate(user)
+	expect(t, success, true)
 }
 
 func TestAuthenticateFail(t *testing.T) {
-    ab := backends.NewBoltDBAuthBackend(TestDB, []byte(backends.TokenBucketName), GetRandomName(10))
-    
-    jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
-    user := &backends.User{
-        Username: "shouldntbehere", 
-        Password: "secret",
-         UUID: "uuid_here",
-        IsAdmin: true}
-        
-     success := jwtBackend.Authenticate(user)
-     expect(t, success, false)
+	ab := backends.NewBoltDBAuthBackend(TestDB, []byte(backends.TokenBucketName), GetRandomName(10))
+
+	jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
+	user := &backends.User{
+		Username: "shouldntbehere",
+		Password: "secret",
+		UUID:     "uuid_here",
+		IsAdmin:  true}
+
+	success := jwtBackend.Authenticate(user)
+	expect(t, success, false)
 }
 
 func TestLogout(t *testing.T) {
-    ab := backends.NewBoltDBAuthBackend(TestDB, GetRandomName(10), GetRandomName(10))
-    
-    jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
-    
-    tokenString := "exampletokenstring"
-    token := jwt.New(jwt.SigningMethodHS512)
-    
-    err := jwtBackend.Logout(tokenString, token)
-    expect(t, err, nil)
-    
-    // checking whether token is in blacklist
-    
-    blacklisted := jwtBackend.IsInBlacklist(tokenString)
-    expect(t, blacklisted, true)
+	ab := backends.NewBoltDBAuthBackend(TestDB, GetRandomName(10), GetRandomName(10))
+
+	jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
+
+	tokenString := "exampletokenstring"
+	token := jwt.New(jwt.SigningMethodHS512)
+
+	err := jwtBackend.Logout(tokenString, token)
+	expect(t, err, nil)
+
+	// checking whether token is in blacklist
+
+	blacklisted := jwtBackend.IsInBlacklist(tokenString)
+	expect(t, blacklisted, true)
 }
 
 func TestNotBlacklisted(t *testing.T) {
-    ab := backends.NewBoltDBAuthBackend(TestDB, GetRandomName(10), GetRandomName(10))
-    
-    jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
-    
-    tokenString := "exampleTokenStringThatIsNotBlacklisted"
-    
-    blacklisted := jwtBackend.IsInBlacklist(tokenString)
-    expect(t, blacklisted, false)
+	ab := backends.NewBoltDBAuthBackend(TestDB, GetRandomName(10), GetRandomName(10))
+
+	jwtBackend := InitJWTAuthenticationBackend(ab, []byte("verysecret"), 100)
+
+	tokenString := "exampleTokenStringThatIsNotBlacklisted"
+
+	blacklisted := jwtBackend.IsInBlacklist(tokenString)
+	expect(t, blacklisted, false)
 }
