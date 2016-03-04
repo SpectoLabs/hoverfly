d2ce304af46176ae3fd4afdaecd76d29b055811e
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-03-04 16:52:53 +0000

    metadata interface

--- metadata.go
+++ metadata.go
@@ -1,1 +1,18 @@
 package hoverfly
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/boltdb/bolt"
+)
+
+// Metadata - interface to store and retrieve any metadata that is related to Hoverfly
+type Metadata interface {
+	Set(key, value []byte) error
+	Get(key []byte) ([]byte, error)
+	Delete(key []byte) error
+	GetAll() ([]MetaObject, error)
+	CloseDB()
+}
+
