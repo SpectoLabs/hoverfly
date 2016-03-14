0c97c149cc2b827d65f5a8dc40c155c1b80b46ca
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-03-14 14:01:26 +0000

    test start hoverfly listener

--- listener_test.go
+++ listener_test.go
@@ -1,1 +1,31 @@
 package hoverfly
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestHoverflyListener(t *testing.T) {
+	server, dbClient := testTools(201, `{'message': 'here'}`)
+	defer server.Close()
+
+	proxyPort := "9777"
+
+	dbClient.Cfg.ProxyPort = proxyPort
+	// starting hoverfly
+	proxy, _ := GetNewHoverfly(dbClient.Cfg, dbClient.Cache)
+	StartHoverflyProxy(dbClient.Cfg, proxy)
+
+	// checking whether it's running
+	response, err := http.Get(fmt.Sprintf("http://localhost:%s/", proxyPort))
+	expect(t, err, nil)
+
+	expect(t, response.StatusCode, 500)
+
+	body, err := ioutil.ReadAll(response.Body)
+	expect(t, err, nil)
+	expect(t, strings.Contains(string(body), "is a proxy server"), true)
+}
