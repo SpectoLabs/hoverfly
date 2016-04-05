389b481587d3f9272e3e078d6ffd4e2cc5b2ad07
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-04-05 16:44:25 +0100

    navigation reducer added to root reducer

--- static/admin/src/redux/rootReducer.js
+++ static/admin/src/redux/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
 import counter from './modules/counter'
 import modes from './modules/modes'
+import navigation from './modules/navigation'
 
 export default combineReducers({
   counter,
   modes,
+  navigation,
   router
 })
