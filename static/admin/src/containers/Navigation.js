b48ad823c4ab8d6a8dc353d90e5f8c9ea0bf92d1
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-04-05 16:43:14 +0100

    moving from cursed menuitems to tabs

--- static/admin/src/containers/Navigation.js
+++ static/admin/src/containers/Navigation.js
@@ -2,21 +2,28 @@
  * Created by karolisrusenas on 04/04/2016.
  */
 import React, {PropTypes} from 'react'
-// import { push } from 'react-router-redux'
 import AppBar from 'material-ui/lib/app-bar'
-import LeftNav from 'material-ui/lib/left-nav'
-import Menu from 'material-ui/lib/menus/menu'
-import MenuItem from 'material-ui/lib/menus/menu-item'
 import {Tabs, Tab} from 'material-ui'
+
+import {connect} from 'react-redux'
+import {pushPath} from '../redux/modules/navigation'
+
+type Props = {
+  path: string,
+  pushPath: Function
+};
 
 export default class Navigation extends React.Component<void, Props, void> {
   static propTypes = {
-    store: PropTypes.object.isRequired
+    // path: PropTypes.string.isRequired,
+    pushPath: PropTypes.func.isRequired
   };
 
   constructor (props) {
     super(props)
     this.state = {open: false}
+    this.handleActive = this.handleActive.bind(this)
+    // this.goToPage = this.goToPage.bind(this)
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
@@ -25,8 +32,20 @@ export default class Navigation extends React.Component<void, Props, void> {
 
   onRequestChange = (open) => this.setState({open})
 
+  handleActive (tab) {
+    this.props.pushPath(tab.props.route)
+  }
+
   render () {
-    let myTabs = (
+    let myLeftTabs = (
+      <Tabs>
+        <Tab label='State' route='state' onActive={this.handleActive}/>
+        <Tab label='Records' route='records' onActive={this.handleActive}/>
+        <Tab label='Middleware' route='middleware' onActive={this.handleActive}/>
+      </Tabs>
+    )
+
+    let myRightTabs = (
       <Tabs>
         <Tab label='Logout' route='/logout'/>
       </Tabs>
@@ -34,19 +53,9 @@ export default class Navigation extends React.Component<void, Props, void> {
 
     return (
       <div>
-        <AppBar title='Hoverfly' onLeftIconButtonTouchTap={this.handleToggle} iconElementRight={myTabs}/>
-        <LeftNav
-          ref='leftNav'
-          docked={false}
-          open={this.state.open}
-          onRequestChange={this.onRequestChange}
-        >
-          <Menu>
-            <MenuItem primaryText='State'/>
-            <MenuItem disabled primaryText='Records'/>
-            <MenuItem disabled primaryText='Middlewares'/>
-          </Menu>
-        </LeftNav>
+        <AppBar
+          onLeftIconButtonTouchTap={this.handleToggle}
+          iconElementLeft={myLeftTabs} iconElementRight={myRightTabs}/>
       </div>
     )
   }
@@ -57,4 +66,8 @@ Navigation.contextTypes = {
   history: React.PropTypes.object
 }
 
-export default Navigation
+const mapStateToProps = (state) => ({})
+
+export default connect((mapStateToProps), {
+  pushPath
+})(Navigation)
