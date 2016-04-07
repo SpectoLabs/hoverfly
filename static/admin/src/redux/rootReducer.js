8d3ee378206c8a3db4123aeb61bf1043a55c9f92
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-04-07 13:52:04 +0100

    including auth to root reducer

--- static/admin/src/redux/rootReducer.js
+++ static/admin/src/redux/rootReducer.js
@@ -3,8 +3,10 @@ import { routerReducer as router } from 'react-router-redux'
 import counter from './modules/counter'
 import modes from './modules/modes'
 import navigation from './modules/navigation'
+import auth from './modules/authReducers/auth'
 
 export default combineReducers({
+  auth,
   counter,
   modes,
   navigation,
