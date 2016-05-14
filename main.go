415de37c413e1f45e6b19b09cf5a04452e25282c
Benji Hooper <benji@sixp.in>
2016-05-14 12:40:15 +0100

    Refactored a little bit ready for more modes.

--- main.go
+++ main.go
@@ -21,9 +21,13 @@ func main() {
 }
 
 func captureHandler() {
-	request, _ := sling.New().Post("http://localhost:8888/api/state").Body(strings.NewReader(`{"mode":"capture"}`)).Request()
-	response, _ := http.DefaultClient.Do(request)
+	response := setHoverflyMode("capture")
 	defer response.Body.Close()
-	fmt.Println(response.Status)
-	fmt.Println("I am capturing")
+	fmt.Println("Hoverfly set to capture mode")
+}
+
+func setHoverflyMode(mode string) (*http.Response) {
+	request, _ := sling.New().Post("http://localhost:8888/api/state").Body(strings.NewReader(`{"mode":"` + mode + `"}`)).Request()
+	response, _ := http.DefaultClient.Do(request)
+	return response
 }
