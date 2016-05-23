48c922973e43e30005fb7296d1ac950e65279bc4
Benji Hooper <benji@sixp.in>
2016-05-23 10:51:55 +0100

    Testing that we throw an appriopriate error if the file does not exist

--- localcache_test.go
+++ localcache_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	. "github.com/onsi/gomega"
 	"os"
+	"fmt"
 )
 
 var testDirectory = "/tmp/hoverfly-cli-tests"
@@ -30,6 +31,21 @@ func Test_LocalCache_WriteSimulation(t *testing.T) {
 
 	Expect(err).To(BeNil())
 	Expect(data).To(Equal([]byte("this is a test file")))
+
+	teardown()
+}
+
+func Test_LocalCache_WriteSimulation_ErrorsWhenFileIsMissing(t *testing.T) {
+	RegisterTestingT(t)
+	setup()
+
+	localCache := LocalCache{Uri: "/tmp"}
+
+	data, err := localCache.ReadSimulation(Hoverfile{Vendor: "vendor", Name: "name", Version: "v1"})
+	fmt.Println(string(data))
+	Expect(err).ToNot(BeNil())
+	Expect(err.Error()).To(Equal("Simulation not found"))
+	Expect(data).To(BeNil())
 
 	teardown()
 }
