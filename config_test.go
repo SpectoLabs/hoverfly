863c6b252231cf9a1404922ad898d156d300948b
Benji Hooper <benji@sixp.in>
2016-06-10 11:29:54 +0100

    Testing the writing of config to disk

--- config_test.go
+++ config_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"testing"
 	. "github.com/onsi/gomega"
+	"os"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
 )
 
 
@@ -78,6 +81,29 @@ func Test_GetConfigOverridesDefaultValueWithAllOverrides(t *testing.T) {
 	Expect(result.HoverflyHost).To(Equal(hoverflyHost))
 	Expect(result.HoverflyAdminPort).To(Equal(hoverflyAdminPort))
 	Expect(result.HoverflyProxyPort).To(Equal(hoverflyProxyPort))
+	Expect(result.SpectoLabHost).To(Equal("localhost"))
+	Expect(result.SpectoLabPort).To(Equal("81"))
+	Expect(result.SpectoLabApiKey).To(Equal(""))
+}
+
+func Test_ConfigWriteToFileWritesTheConfigObjectToAFileInAYamlFormat(t *testing.T) {
+	RegisterTestingT(t)
+
+	SetConfigurationDefaults()
+	config := GetConfig("", "", "")
+
+	workingDir, _ := os.Getwd()
+	config.WriteToFile(workingDir)
+
+	data, _ := ioutil.ReadFile(workingDir + "/config.yaml")
+	os.Remove(workingDir + "/config.yaml")
+
+	var result Config
+	yaml.Unmarshal(data, &result)
+
+	Expect(result.HoverflyHost).To(Equal("localhost"))
+	Expect(result.HoverflyAdminPort).To(Equal("8888"))
+	Expect(result.HoverflyProxyPort).To(Equal("8500"))
 	Expect(result.SpectoLabHost).To(Equal("localhost"))
 	Expect(result.SpectoLabPort).To(Equal("81"))
 	Expect(result.SpectoLabApiKey).To(Equal(""))
