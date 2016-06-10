7ddcc7aa78a4ec4afb04348e2ed7331f6f6d343d
Benji Hooper <benji@sixp.in>
2016-06-10 11:21:36 +0100

    Starting to add tests around config

--- config_test.go
+++ config_test.go
@@ -3,24 +3,82 @@ package main
 import (
 	"testing"
 	. "github.com/onsi/gomega"
-	"github.com/mitchellh/go-homedir"
 )
 
 
-func Test_getHoverflyDirectory(t *testing.T) {
+func Test_GetConfigWillReturnTheDefaultValues(t *testing.T) {
 	RegisterTestingT(t)
 
-	result := getHoverflyDirectory("/test/dir/.hoverfly/config.yaml")
+	SetConfigurationDefaults()
+	result := GetConfig("", "", "")
 
-	Expect(result).To(Equal("/test/dir/.hoverfly"))
+	Expect(result.HoverflyHost).To(Equal("localhost"))
+	Expect(result.HoverflyAdminPort).To(Equal("8888"))
+	Expect(result.HoverflyProxyPort).To(Equal("8500"))
+	Expect(result.SpectoLabHost).To(Equal("localhost"))
+	Expect(result.SpectoLabPort).To(Equal("81"))
+	Expect(result.SpectoLabApiKey).To(Equal(""))
 }
 
-func Test_getHoverflyDirectory_ReturnsHomeDirIfConfigUriIsEmpty(t *testing.T) {
+func Test_GetConfigOverridesDefaultValueWithAHoverflyHost(t *testing.T) {
 	RegisterTestingT(t)
 
-	result := getHoverflyDirectory("")
+	SetConfigurationDefaults()
+	hoverflyHost := "testhost"
+	result := GetConfig(hoverflyHost, "", "")
 
-	homeDir, _ := homedir.Dir()
 
-	Expect(result).To(Equal(homeDir + "/.hoverfly"))
+	Expect(result.HoverflyHost).To(Equal(hoverflyHost))
+	Expect(result.HoverflyAdminPort).To(Equal("8888"))
+	Expect(result.HoverflyProxyPort).To(Equal("8500"))
+	Expect(result.SpectoLabHost).To(Equal("localhost"))
+	Expect(result.SpectoLabPort).To(Equal("81"))
+	Expect(result.SpectoLabApiKey).To(Equal(""))
+}
+
+func Test_GetConfigOverridesDefaultValueWithAHoverflyAdminPort(t *testing.T) {
+	RegisterTestingT(t)
+
+	SetConfigurationDefaults()
+	hoverflyAdminPort := "5"
+	result := GetConfig("", hoverflyAdminPort, "")
+
+	Expect(result.HoverflyHost).To(Equal("localhost"))
+	Expect(result.HoverflyAdminPort).To(Equal(hoverflyAdminPort))
+	Expect(result.HoverflyProxyPort).To(Equal("8500"))
+	Expect(result.SpectoLabHost).To(Equal("localhost"))
+	Expect(result.SpectoLabPort).To(Equal("81"))
+	Expect(result.SpectoLabApiKey).To(Equal(""))
+}
+
+func Test_GetConfigOverridesDefaultValueWithAHoverflyProxyPort(t *testing.T) {
+	RegisterTestingT(t)
+
+	SetConfigurationDefaults()
+	hoverflyProxyPort := "7"
+	result := GetConfig("", "", hoverflyProxyPort)
+
+	Expect(result.HoverflyHost).To(Equal("localhost"))
+	Expect(result.HoverflyAdminPort).To(Equal("8888"))
+	Expect(result.HoverflyProxyPort).To(Equal(hoverflyProxyPort))
+	Expect(result.SpectoLabHost).To(Equal("localhost"))
+	Expect(result.SpectoLabPort).To(Equal("81"))
+	Expect(result.SpectoLabApiKey).To(Equal(""))
+}
+
+func Test_GetConfigOverridesDefaultValueWithAllOverrides(t *testing.T) {
+	RegisterTestingT(t)
+
+	SetConfigurationDefaults()
+	hoverflyHost := "specto.io"
+	hoverflyAdminPort := "7654"
+	hoverflyProxyPort := "1523"
+	result := GetConfig(hoverflyHost, hoverflyAdminPort, hoverflyProxyPort)
+
+	Expect(result.HoverflyHost).To(Equal(hoverflyHost))
+	Expect(result.HoverflyAdminPort).To(Equal(hoverflyAdminPort))
+	Expect(result.HoverflyProxyPort).To(Equal(hoverflyProxyPort))
+	Expect(result.SpectoLabHost).To(Equal("localhost"))
+	Expect(result.SpectoLabPort).To(Equal("81"))
+	Expect(result.SpectoLabApiKey).To(Equal(""))
 }
