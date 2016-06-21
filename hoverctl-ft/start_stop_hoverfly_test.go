ecc55b424d876f6222180bf042fa1d929323e82b
Benji Hooper <benji@sixp.in>
2016-06-21 15:21:14 +0100

    Indentation problem

--- hoverctl-ft/start_stop_hoverfly_test.go
+++ hoverctl-ft/start_stop_hoverfly_test.go
@@ -11,70 +11,70 @@ import (
 
 var _ = Describe("When I use hoverctl", func() {
 
-		Describe("without a running hoverfly", func() {
+	Describe("without a running hoverfly", func() {
 
-			BeforeEach(func() {
-				exec.Command(hoverctlBinary, "stop", "-v").Run()
-			})
+		BeforeEach(func() {
+			exec.Command(hoverctlBinary, "stop", "-v").Run()
+		})
 
-			AfterEach(func() {
-				exec.Command(hoverctlBinary, "stop", "-v").Run()
-			})
+		AfterEach(func() {
+			exec.Command(hoverctlBinary, "stop", "-v").Run()
+		})
 
-			Context("I can control a process of hoverfly", func() {
+		Context("I can control a process of hoverfly", func() {
 
-				It("by starting hoverfly", func() {
-					setOutput, _ := exec.Command(hoverctlBinary, "start", "-v").Output()
+			It("by starting hoverfly", func() {
+				setOutput, _ := exec.Command(hoverctlBinary, "start", "-v").Output()
 
-					output := strings.TrimSpace(string(setOutput))
-					Expect(output).To(ContainSubstring("Hoverfly is now running"))
+				output := strings.TrimSpace(string(setOutput))
+				Expect(output).To(ContainSubstring("Hoverfly is now running"))
 
-					data, err := ioutil.ReadFile("./.hoverfly/hoverfly.8888.8500.pid")
+				data, err := ioutil.ReadFile("./.hoverfly/hoverfly.8888.8500.pid")
 
-					if err != nil {
-						Fail("Could not find pid file")
-					}
+				if err != nil {
+					Fail("Could not find pid file")
+				}
 
-					Expect(data).ToNot(BeEmpty())
+				Expect(data).ToNot(BeEmpty())
 
-					if _, err := strconv.Atoi(string(data)); err != nil {
-						Fail("Pid file not have an integer in it")
-					}
-
-				})
-
-				It("by stopping  hoverfly", func() {
-					exec.Command(hoverctlBinary, "start").Run()
-
-					setOutput, _ := exec.Command(hoverctlBinary, "stop").Output()
-
-					output := strings.TrimSpace(string(setOutput))
-					Expect(output).To(ContainSubstring("Hoverfly has been stopped"))
-
-					_, err := ioutil.ReadFile("./.hoverfly/hoverfly.8888.8500.pid")
-
-					if err == nil {
-						Fail("Could not find pid file")
-					}
- 				})
-
-				It("but you cannot start hoverfly if already running", func() {
-					exec.Command(hoverctlBinary, "start").Run()
-
-					setOutput, _ := exec.Command(hoverctlBinary, "start").Output()
-
-					output := strings.TrimSpace(string(setOutput))
-					Expect(output).To(ContainSubstring("Hoverfly is already running"))
-				})
-
-				It("but you cannot stop hoverfly if is not running", func() {
-					setOutput, _ := exec.Command(hoverctlBinary, "stop").Output()
-
-					output := strings.TrimSpace(string(setOutput))
-					Expect(output).To(ContainSubstring("Hoverfly is not running"))
-				})
-
+				if _, err := strconv.Atoi(string(data)); err != nil {
+					Fail("Pid file not have an integer in it")
+				}
 
 			})
+
+			It("by stopping  hoverfly", func() {
+				exec.Command(hoverctlBinary, "start").Run()
+
+				setOutput, _ := exec.Command(hoverctlBinary, "stop").Output()
+
+				output := strings.TrimSpace(string(setOutput))
+				Expect(output).To(ContainSubstring("Hoverfly has been stopped"))
+
+				_, err := ioutil.ReadFile("./.hoverfly/hoverfly.8888.8500.pid")
+
+				if err == nil {
+					Fail("Could not find pid file")
+				}
+			})
+
+			It("but you cannot start hoverfly if already running", func() {
+				exec.Command(hoverctlBinary, "start").Run()
+
+				setOutput, _ := exec.Command(hoverctlBinary, "start").Output()
+
+				output := strings.TrimSpace(string(setOutput))
+				Expect(output).To(ContainSubstring("Hoverfly is already running"))
+			})
+
+			It("but you cannot stop hoverfly if is not running", func() {
+				setOutput, _ := exec.Command(hoverctlBinary, "stop").Output()
+
+				output := strings.TrimSpace(string(setOutput))
+				Expect(output).To(ContainSubstring("Hoverfly is not running"))
+			})
+
+
 		})
 	})
+})
