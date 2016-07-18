f95824954961b2f364ae9d2818a1c4f77bad15e5
Benji Hooper <benji@sixp.in>
2016-07-18 11:09:19 +0100

    Created a logfile struct to encapsulate the log reading functionality

---
+++ hoverctl/logfile.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"path/filepath"
+	"fmt"
+	"io/ioutil"
+	"errors"
+)
+
+type LogFile struct {
+	Path string
+	Name string
+}
+
+func NewLogFile(directory HoverflyDirectory, adminPort, proxyPort string) (LogFile) {
+	fileName := fmt.Sprintf("hoverfly.%v.%v.log", adminPort, proxyPort)
+
+	filePath := filepath.Join(directory.Path, fileName)
+
+	return LogFile{
+		Path: filePath,
+		Name: fileName,
+	}
+}
+
+func (l *LogFile) GetLogs() (string, error) {
+	content, err := ioutil.ReadFile(l.Path)
+	if err != nil {
+		log.Debug(err.Error())
+		return "", errors.New("Unable to open Hoverfly log file")
+	}
+
+	return string(content), nil
+}
---
+++ hoverctl/logfile_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	. "github.com/onsi/gomega"
+	"testing"
+	"os"
+	"io/ioutil"
+)
+
+var logFile_testDirectory = "/tmp/hoverctl-tests-logfile"
+
+func logFile_setup() {
+	os.Mkdir(logFile_testDirectory, 0777)
+}
+
+func logFile_teardown() {
+	os.RemoveAll(logFile_testDirectory)
+}
+
+func Test_NewLogfile(t *testing.T) {
+	RegisterTestingT(t)
+	logFile_setup()
+	defer logFile_teardown()
+
+	hoverflyDirectory := HoverflyDirectory{
+		Path: "/test/dir",
+	}
+
+	logfile := NewLogFile(hoverflyDirectory, "1234", "4321")
+
+	Expect(logfile.Path).To(Equal("/test/dir/hoverfly.1234.4321.log"))
+	Expect(logfile.Name).To(Equal("hoverfly.1234.4321.log"))
+}
+
+func Test_Logfile_GetLogs(t *testing.T) {
+	RegisterTestingT(t)
+	logFile_setup()
+	defer logFile_teardown()
+
+	hoverflyDirectory := HoverflyDirectory{
+		Path: logFile_testDirectory,
+	}
+
+	logfile := NewLogFile(hoverflyDirectory, "9856", "6589")
+
+	ioutil.WriteFile(logfile.Path, []byte("testing rocks"), 0644)
+
+	logs, err := logfile.GetLogs()
+	Expect(err).To(BeNil())
+
+	Expect(logs).To(Equal("testing rocks"))
+}
+
+func Test_Logfile_GetLogs_ReturnsErrorIfLogDoesNotExist(t *testing.T) {
+	RegisterTestingT(t)
+	logFile_setup()
+	defer logFile_teardown()
+
+	hoverflyDirectory := HoverflyDirectory{
+		Path: logFile_testDirectory,
+	}
+
+	logfile := NewLogFile(hoverflyDirectory, "9856", "6589")
+
+	_, err := logfile.GetLogs()
+	Expect(err).ToNot(BeNil())
+	Expect(err.Error()).To(Equal("Unable to open Hoverfly log file"))
+}
