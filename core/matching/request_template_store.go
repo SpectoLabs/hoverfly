52ea283dbe98a42d73bf6aff9af9f91619407f71
Benji Hooper <benji@sixp.in>
2016-07-21 13:51:06 +0100

    Changed the structs in the RequestTemplateStore

--- core/matching/request_template_store.go
+++ core/matching/request_template_store.go
@@ -8,15 +8,25 @@ import (
 )
 
 type RequestTemplateStore struct {
-	RequestTemplates	[]Entry
+	RequestTemplates []RequestTemplatePayload
 }
 
-type Entry struct {
-	Payload	*models.Payload
-	RequestTemplate RequestTemplate
+type RequestTemplatePayload struct {
+	RequestTemplate RequestTemplate        `json: "requestTemplate"`
+	Response        models.ResponseDetails `json: "response"`
+}
+
+type RequestTemplatePayloadJson struct {
+	Data *[]RequestTemplatePayload `json:"data"`
 }
 
 type RequestTemplate struct {
+	Path        string              `json:"path"`
+	Method      string              `json:"method"`
+	Destination string              `json:"destination"`
+	Scheme      string              `json:"scheme"`
+	Query       string              `json:"query"`
+	Body        string              `json:"body"`
 	Headers     map[string][]string `json:"headers"`
 }
 
@@ -25,9 +35,9 @@ func(this *RequestTemplateStore) GetPayload(req *http.Request) (*models.Payload,
 	for _, entry := range this.RequestTemplates {
 		//TODO: not matching by default on URL and body - need to enable this
 		// TODO: need to enable regex matches
-		if headerMatch(entry.RequestTemplate.Headers, req.Header) {
+		if headerMatch(entry, req.Header) {
 			// return the first template to match
-			return entry.Payload, nil
+			return models.Payload{Response: *entry.Response}, nil
 		}
 	}
 	return nil, errors.New("No match found")
