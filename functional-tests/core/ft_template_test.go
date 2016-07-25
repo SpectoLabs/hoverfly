c1442cb00e98e56d8c4d720308f23659beaede21
Ben Vickers <ben.vickers@opencredo.com>
2016-07-25 15:41:01 +0100

    need to recreate the json template string for each test

--- functional-tests/core/ft_template_test.go
+++ functional-tests/core/ft_template_test.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"bytes"
 	"github.com/dghubble/sling"
-	"net/http/httptest"
 	"fmt"
 )
 
@@ -15,8 +14,13 @@ var _ = Describe("Using Hoverfly to return responses by request templates", func
 	Context("With a request template loaded for matching on URL + headers", func () {
 
 		var (
-			jsonPayload = bytes.NewBufferString(`{"data":[{"request": {"path": "/path1", "method": "GET", "destination": "www.virtual.com", "headers": {"Header": ["value1"]}}, "response": {"status": 201, "encodedBody": false, "body": "body1", "headers": {"Header": ["value1"]}}}, {"requestTemplate": {"path": "/path2", "method": "GET", "destination": "www.virtual.com", "headers": {"Header": ["value2"]}}, "response": {"status": 202, "body": "body2", "headers": {"Header": ["value2"]}}}]}`)
+			jsonPayload *bytes.Buffer
 		)
+
+		BeforeEach(func() {
+			jsonPayload = bytes.NewBufferString(`{"data":[{"requestTemplate": {"path": "/path1", "method": "GET", "destination": "www.virtual.com"}, "response": {"status": 201, "encodedBody": false, "body": "body1", "headers": {"Header": ["value1"]}}}, {"requestTemplate": {"path": "/path2", "method": "GET", "destination": "www.virtual.com", "headers": {"Header": ["value2"]}}, "response": {"status": 202, "body": "body2", "headers": {"Header": ["value2"]}}}]}`)
+
+		})
 
 		Context("When running in proxy mode", func() {
 
@@ -34,7 +38,6 @@ var _ = Describe("Using Hoverfly to return responses by request templates", func
 				resp := DoRequestThroughProxy(sling.New().Get("http://www.virtual.com/path2").Add("Header", "value2"))
 				body, err := ioutil.ReadAll(resp.Body)
 				Expect(err).To(BeNil())
-				fmt.Println()
 				Expect(resp.StatusCode).To(Equal(202))
 				Expect(string(body)).To(Equal("body2"))
 			})
@@ -60,5 +63,6 @@ var _ = Describe("Using Hoverfly to return responses by request templates", func
 			})
 		})
 	})
+
 })
 
