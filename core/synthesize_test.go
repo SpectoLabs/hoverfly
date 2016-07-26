5a7dd5a1fd9d95267fea773644657adf457eb7c7
Benji Hooper <benji@sixp.in>
2016-07-26 17:10:50 +0100

    Cleaning up the synthesize_test by replacing testutils with Gomega

--- core/synthesize_test.go
+++ core/synthesize_test.go
@@ -1,38 +1,41 @@
 package hoverfly
 
 import (
-	"github.com/SpectoLabs/hoverfly/core/testutil"
+	. "github.com/onsi/gomega"
 	"net/http"
 	"testing"
 )
 
 func TestSynthesizeResponse(t *testing.T) {
+	RegisterTestingT(t)
 
 	req, err := http.NewRequest("GET", "http://example.com", nil)
-	testutil.Expect(t, err, nil)
+	Expect(err).To(BeNil())
 
 	sr, err := SynthesizeResponse(req, "./examples/middleware/synthetic_service/synthetic.py")
-	testutil.Expect(t, err, nil)
+	Expect(err).To(BeNil())
 
-	testutil.Expect(t, sr.StatusCode, 200)
+	Expect(sr.StatusCode).To(Equal(http.StatusOK))
 }
 
 func TestSynthesizeResponseWOMiddleware(t *testing.T) {
+	RegisterTestingT(t)
 
 	req, err := http.NewRequest("GET", "http://example.com", nil)
-	testutil.Expect(t, err, nil)
+	Expect(err).To(BeNil())
 
 	_, err = SynthesizeResponse(req, "")
-	testutil.Refute(t, err, nil)
+	Expect(err).ToNot(BeNil())
 
-	testutil.Expect(t, err.Error(), "Synthesize failed, middleware not provided")
+	Expect(err).To(MatchError("Synthesize failed, middleware not provided"))
 }
 
 func TestSynthesizeMiddlewareFailure(t *testing.T) {
+	RegisterTestingT(t)
 
 	req, err := http.NewRequest("GET", "http://example.com", nil)
-	testutil.Expect(t, err, nil)
+	Expect(err).To(BeNil())
 
 	_, err = SynthesizeResponse(req, "./examples/middleware/this_is_not_there.py")
-	testutil.Refute(t, err, nil)
+	Expect(err).ToNot(BeNil())
 }
