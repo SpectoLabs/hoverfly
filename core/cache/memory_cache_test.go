98bfa558943d1385305a099987d55dcf8a3654ce
Benji Hooper <benji@sixp.in>
2016-07-27 15:12:07 +0100

    Cleaning up the memory_cache__test by replacing testutils with Gomega

--- core/cache/memory_cache_test.go
+++ core/cache/memory_cache_test.go
@@ -1,202 +1,182 @@
 package cache
 
 import (
-	"github.com/SpectoLabs/hoverfly/core/testutil"
-	"reflect"
+	. "github.com/onsi/gomega"
 	"testing"
 )
 
-func TestEmptyByDefault(t *testing.T) {
+var (
+	expectedKey1 = []byte{'1'}
+	expectedKey2 = []byte{'2'}
+	expectedValue1 = []byte{'A'}
+	expectedValue2 = []byte{'B'}
+)
+
+func TestCacheGetIsEmptyByDefault(t *testing.T) {
+	RegisterTestingT(t)
+
 	cache := &InMemoryCache{}
 
 	expectedKey := []byte{'k'}
 
-	if actualValue, err := cache.Get(expectedKey); err == nil || len(actualValue) != 0 {
-		t.Fatal("Cache should be empty by default")
-	}
+	actualValue, err := cache.Get(expectedKey);
+	Expect(err).ToNot(BeNil())
+	Expect(actualValue).To(HaveLen(0))
+}
 
-	if actualValue, err := cache.GetAllEntries(); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if len(actualValue) != 0 {
-		t.Fatal("Cache should be empty by default")
-	}
+func TestCacheGetAllEntriesIsEmptyByDefault(t *testing.T) {
+	RegisterTestingT(t)
 
-	if actualValue, err := cache.GetAllKeys(); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if len(actualValue) != 0 {
-		t.Fatal("Cache should be empty by default")
-	}
+	cache := &InMemoryCache{}
 
-	if actualValue, err := cache.GetAllValues(); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if len(actualValue) != 0 {
-		t.Fatal("Cache should be empty by default")
-	}
+	actualValue, err := cache.GetAllEntries()
+	Expect(err).To(BeNil())
+	Expect(actualValue).To(HaveLen(0))
+}
 
-	if actualValue, err := cache.GetAllEntries(); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if len(actualValue) != 0 {
-		t.Fatal("Cache should be empty by default")
-	}
+func TestCacheGetAllKeysIsEmptyByDefault(t *testing.T) {
+	RegisterTestingT(t)
+
+	cache := &InMemoryCache{}
+
+	actualValue, err := cache.GetAllKeys();
+	Expect(err).To(BeNil())
+	Expect(actualValue).To(HaveLen(0))
+}
+
+func TestCacheGetAllValuesIsEmptyByDefault(t *testing.T) {
+	RegisterTestingT(t)
+
+	cache := &InMemoryCache{}
+
+	actualValue, err := cache.GetAllValues();
+	Expect(err).To(BeNil())
+	Expect(actualValue).To(HaveLen(0))
 }
 
 func TestSetAndGet(t *testing.T) {
+	RegisterTestingT(t)
+
 	cache := NewInMemoryCache()
 
-	expectedKey1 := []byte{'1'}
-	expectedKey2 := []byte{'2'}
-	expectedValue1 := []byte{'A'}
-	expectedValue2 := []byte{'B'}
+	err := cache.Set(expectedKey1, expectedValue1);
+	Expect(err).To(BeNil())
 
-	if err := cache.Set(expectedKey1, expectedValue1); err != nil {
-		t.Fatalf("err: %v", err)
-	}
+	err = cache.Set(expectedKey2, expectedValue2);
+	Expect(err).To(BeNil())
 
-	if err := cache.Set(expectedKey2, expectedValue2); err != nil {
-		t.Fatalf("err: %v", err)
-	}
+	actualValue, err := cache.Get(expectedKey1)
+	Expect(err).To(BeNil())
+	Expect(actualValue).To(Equal(expectedValue1))
 
-	if actualValue, err := cache.Get(expectedKey1); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if !reflect.DeepEqual(expectedValue1, actualValue) {
-		t.Fatalf("Expected value %s does not match actual value %s", expectedValue1, actualValue)
-	}
-
-	if actualValue, err := cache.Get(expectedKey2); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if !reflect.DeepEqual(expectedValue2, actualValue) {
-		t.Fatalf("Expected value %s does not match actual value %s", expectedValue1, actualValue)
-	}
+	actualValue, err = cache.Get(expectedKey2)
+	Expect(err).To(BeNil())
+	Expect(actualValue).To(Equal(expectedValue2))
 }
 
 func TestGetAllKeysMem(t *testing.T) {
+	RegisterTestingT(t)
 
 	cache := NewInMemoryCache()
-
-	expectedKey1 := []byte{'1'}
-	expectedKey2 := []byte{'2'}
-	expectedValue1 := []byte{'A'}
-	expectedValue2 := []byte{'B'}
 
 	cache.Set(expectedKey1, expectedValue1)
 	cache.Set(expectedKey2, expectedValue2)
 
-	if actualKey, err := cache.GetAllKeys(); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if len(actualKey) != 2 {
-		t.Fatal("Cache should have two keys")
-	} else if !actualKey[string(expectedKey1)] {
-		t.Fatalf("Cache does not contain key %s", expectedKey1)
-	} else if !actualKey[string(expectedKey2)] {
-		t.Fatalf("Cache does not contain key %s", expectedKey2)
-	}
+	keys, err := cache.GetAllKeys()
+	Expect(err).To(BeNil())
+
+	Expect(keys).To(HaveLen(2))
+	Expect(keys).To(HaveKey(string(expectedKey1)))
+	Expect(keys).To(HaveKey(string(expectedKey2)))
 }
 
 func TestGetAllValuesMem(t *testing.T) {
+	RegisterTestingT(t)
 
 	cache := NewInMemoryCache()
-
-	expectedKey1 := []byte{'1'}
-	expectedKey2 := []byte{'2'}
-	expectedValue1 := []byte{'A'}
-	expectedValue2 := []byte{'B'}
 
 	cache.Set(expectedKey1, expectedValue1)
 	cache.Set(expectedKey2, expectedValue2)
 
-	if values, err := cache.GetAllValues(); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if len(values) != 2 {
-		t.Fatalf("Expected %v values but got %s", 2, len(values))
-	} else if !testutil.Contains(values, expectedValue1) {
-		t.Fatalf("Value %s was not present in %s", expectedValue1, values)
-	} else if !testutil.Contains(values, expectedValue2) {
-		t.Fatalf("Value %s was not present in %s", expectedValue2, values)
-	}
+	values, err := cache.GetAllValues()
+	Expect(err).To(BeNil())
+
+	Expect(values).To(HaveLen(2))
+	Expect(values).To(ContainElement(expectedValue1))
+	Expect(values).To(ContainElement(expectedValue2))
 }
 
 func TestGetAllEntriesMem(t *testing.T) {
-	cache := NewInMemoryCache()
+	RegisterTestingT(t)
 
-	expectedKey1 := []byte{'1'}
-	expectedKey2 := []byte{'2'}
-	expectedValue1 := []byte{'A'}
-	expectedValue2 := []byte{'B'}
+	cache := NewInMemoryCache()
 
 	cache.Set(expectedKey1, expectedValue1)
 	cache.Set(expectedKey2, expectedValue2)
 
-	if entries, err := cache.GetAllEntries(); err != nil {
-		t.Fatalf("err: %v", err)
-	} else if len(entries) != 2 {
-		t.Fatalf("Expected %v values but got %v", 2, len(entries))
-	} else if reflect.DeepEqual(entries[string(expectedKey1)], string(expectedValue1)) {
-		t.Fatalf("Entry %s %s was not present in %s", expectedKey1, expectedValue1, entries)
-	} else if reflect.DeepEqual(entries[string(expectedKey1)], string(expectedValue2)) {
-		t.Fatalf("Entry %s %s was not present in %s", expectedKey2, expectedValue2, entries)
-	}
+	entries, err := cache.GetAllEntries()
+	Expect(err).To(BeNil())
+
+	Expect(entries).To(HaveLen(2))
+	Expect(entries).To(HaveKeyWithValue(string(expectedKey1), expectedValue1))
+	Expect(entries).To(HaveKeyWithValue(string(expectedKey2), expectedValue2))
 }
 
 func TestGetRecordCount(t *testing.T) {
-	cache := NewInMemoryCache()
+	RegisterTestingT(t)
 
-	expectedKey1 := []byte{'1'}
-	expectedKey2 := []byte{'2'}
-	expectedValue1 := []byte{'A'}
-	expectedValue2 := []byte{'B'}
+	cache := NewInMemoryCache()
 
 	cache.Set(expectedKey1, expectedValue1)
 	cache.Set(expectedKey2, expectedValue2)
 
-	if recordCount, _ := cache.RecordsCount(); recordCount != 2 {
-		t.Fatalf("Expected %v records but got %v", 2, recordCount)
-	}
+	recordCount, err := cache.RecordsCount()
+	Expect(err).To(BeNil())
+
+	Expect(recordCount).To(Equal(2))
 }
 
 func TestDeleteData(t *testing.T) {
-	cache := NewInMemoryCache()
+	RegisterTestingT(t)
 
-	expectedKey1 := []byte{'1'}
-	expectedKey2 := []byte{'2'}
-	expectedValue1 := []byte{'A'}
-	expectedValue2 := []byte{'B'}
+	cache := NewInMemoryCache()
 
 	cache.Set(expectedKey1, expectedValue1)
 	cache.Set(expectedKey2, expectedValue2)
 
 	cache.DeleteData()
 
-	if recordCount, _ := cache.RecordsCount(); recordCount != 0 {
-		t.Fatalf("Expected %v records but got %v", 0, recordCount)
-	}
+	recordCount, err := cache.RecordsCount()
+	Expect(err).To(BeNil())
+
+	Expect(recordCount).To(Equal(0))
 }
 
 func TestDeleteKey(t *testing.T) {
-	cache := NewInMemoryCache()
+	RegisterTestingT(t)
 
-	expectedKey1 := []byte{'1'}
-	expectedKey2 := []byte{'2'}
-	expectedValue1 := []byte{'A'}
-	expectedValue2 := []byte{'B'}
+	cache := NewInMemoryCache()
 
 	cache.Set(expectedKey1, expectedValue1)
 	cache.Set(expectedKey2, expectedValue2)
 
 	cache.Delete([]byte(expectedKey1))
 
-	if value, err := cache.Get(expectedKey1); err == nil {
-		t.Fatalf("Error %v", err)
-	} else if len(value) != 0 {
-		t.Fatalf("Expected nil value but got %v", value)
-	} else if value, _ := cache.RecordsCount(); value != 1 {
-		t.Fatalf("Expected %v records but got %v", 1, cache.RecordsCount)
-	}
+	value, err := cache.Get(expectedKey1)
+	Expect(err).ToNot(BeNil())
+
+	Expect(value).To(HaveLen(0))
+
+	count, err := cache.RecordsCount();
+	Expect(err).To(BeNil())
+
+	Expect(count).To(Equal(1))
 }
 
 func TestThrowErrorIfGettingANotExistingKey(t *testing.T) {
+	RegisterTestingT(t)
+
 	cache := NewInMemoryCache()
 	_, err := cache.Get([]byte("key"))
-	if (err == nil) {
-		t.Fatal("Expected cache to throw an error when getting a non-existing key")
-	}
+	Expect(err).ToNot(BeNil())
 }
