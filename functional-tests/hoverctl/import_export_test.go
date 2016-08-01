04f50563a1d732a108498f5bef27a6d5347d7f5c
Benji Hooper <benji@sixp.in>
2016-08-01 14:30:30 +0100

    Reusing the hoverflyData throughout the import_export_test

--- functional-tests/hoverctl/import_export_test.go
+++ functional-tests/hoverctl/import_export_test.go
@@ -66,34 +66,7 @@ var _ = Describe("When I use hoverctl", func() {
 		Describe("Managing Hoverflies data using the CLI", func() {
 
 			BeforeEach(func() {
-				DoRequest(sling.New().Post(fmt.Sprintf("http://localhost:%v/api/records", adminPort)).Body(strings.NewReader(`
-					{
-						"data": [{
-							"request": {
-								"path": "/api/bookings",
-								"method": "POST",
-								"destination": "www.my-test.com",
-								"scheme": "http",
-								"query": "",
-								"body": "{\"flightId\": \"1\"}",
-								"headers": {
-									"Content-Type": [
-										"application/json"
-									]
-								}
-							},
-							"response": {
-								"status": 201,
-								"body": "",
-								"encodedBody": false,
-								"headers": {
-									"Location": [
-										"http://localhost/api/bookings/1"
-									]
-								}
-							}
-						}]
-					}`)))
+				DoRequest(sling.New().Post(fmt.Sprintf("http://localhost:%v/api/records", adminPort)).Body(strings.NewReader(hoverflyData)))
 
 				resp := DoRequest(sling.New().Get(fmt.Sprintf("http://localhost:%v/api/records", adminPort)))
 				bytes, _ := ioutil.ReadAll(resp.Body)
@@ -121,34 +94,7 @@ var _ = Describe("When I use hoverctl", func() {
 
 				resp := DoRequest(sling.New().Get(fmt.Sprintf("http://localhost:%v/api/records", adminPort)))
 				bytes, _ := ioutil.ReadAll(resp.Body)
-				Expect(string(bytes)).To(MatchJSON(`
-					{
-						"data": [{
-							"request": {
-								"path": "/api/bookings",
-								"method": "POST",
-								"destination": "www.my-test.com",
-								"scheme": "http",
-								"query": "",
-								"body": "{\"flightId\": \"1\"}",
-								"headers": {
-									"Content-Type": [
-										"application/json"
-									]
-								}
-							},
-							"response": {
-								"status": 201,
-								"body": "",
-								"encodedBody": false,
-								"headers": {
-									"Location": [
-										"http://localhost/api/bookings/1"
-									]
-								}
-							}
-						}]
-					}`))
+				Expect(string(bytes)).To(MatchJSON(hoverflyData))
 			})
 
 		})
