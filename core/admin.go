f770070e0b03e7f6e3f00201315fad2b904f85f6
Benji Hooper <benji@sixp.in>
2016-09-23 11:37:12 +0100

    Adding the new HoverflyMiddlewareHandler to the admin API

--- core/admin.go
+++ core/admin.go
@@ -114,6 +114,7 @@ func GetAllHandlers(hoverfly *Hoverfly) []handlers.AdminHandler {
 	list = append(list, &v1.StatsHandler{Hoverfly: hoverfly})
 	list = append(list, &v1.TemplatesHandler{Hoverfly: hoverfly})
 	list = append(list, &v2.HoverflyModeHandler{Hoverfly: hoverfly})
+	list = append(list, &v2.HoverflyMiddlewareHandler{Hoverfly: hoverfly})
 
 	return list
 }
