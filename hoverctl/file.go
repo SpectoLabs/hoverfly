6c713c683f0fed7fac3d293429223585e14d5944
Benji Hooper <benji@sixp.in>
2016-11-08 10:28:44 +0000

    Added some simple helpers for reading and writing files

---
+++ hoverctl/file.go
@@ -0,0 +1,11 @@
+package main
+
+import "io/ioutil"
+
+func WriteFile(fileName string, data []byte) error {
+	return ioutil.WriteFile(fileName, data, 0644)
+}
+
+func ReadFile(fileName string) ([]byte, error) {
+	return ioutil.ReadFile(fileName)
+}
