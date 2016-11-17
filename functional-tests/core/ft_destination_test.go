49be5af2ca77d242d568f8414351374b9c533d0a
Benji Hooper <benji@sixp.in>
2016-11-17 12:11:15 +0000

    Added a positive test case for destination config

--- functional-tests/core/ft_destination_test.go
+++ functional-tests/core/ft_destination_test.go
@@ -5,7 +5,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
+
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -16,7 +17,6 @@ var _ = Describe("Running Hoverfly", func() {
 	Context("in capture mode", func() {
 
 		var fakeServer *httptest.Server
-		var fakeServerUrl *url.URL
 
 		BeforeEach(func() {
 			hoverflyCmd = startHoverfly(adminPort, proxyPort)
@@ -32,15 +32,16 @@ var _ = Describe("Running Hoverfly", func() {
 
 			fakeServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "text/plain")
-				w.Header().Set("Date", "date")
+				w.Header().Set("date", "date")
 				w.Write([]byte("Hello world"))
 			}))
 
 			defer fakeServer.Close()
 
-			fakeServerUrl, _ = url.Parse(fakeServer.URL)
 			resp := CallFakeServerThroughProxy(fakeServer)
+
 			Expect(resp.StatusCode).To(Equal(200))
+			Expect(resp.Header.Get("date")).To(Equal("date"))
 
 			recordsJson, err := ioutil.ReadAll(ExportHoverflyRecords())
 			Expect(err).To(BeNil())
@@ -48,6 +49,69 @@ var _ = Describe("Running Hoverfly", func() {
 				`{
 					  "data": null
 					}`)))
+		})
+
+		It("Should capture capture if destination is 127.0.0.1", func() {
+			fakeServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "text/plain")
+				w.Header().Set("date", "date")
+				w.Write([]byte("Hello world"))
+			}))
+
+			defer fakeServer.Close()
+
+			SetHoverflyDestination("127.0.0.1")
+
+			resp := CallFakeServerThroughProxy(fakeServer)
+
+			Expect(resp.StatusCode).To(Equal(200))
+			Expect(resp.Header.Get("date")).To(Equal("date"))
+
+			recordsJson, err := ioutil.ReadAll(ExportHoverflyRecords())
+			Expect(err).To(BeNil())
+			Expect(recordsJson).To(MatchJSON(fmt.Sprintf(
+				`{
+					"data": [
+						{
+						"response": {
+							"status": 200,
+							"body": "Hello world",
+							"encodedBody": false,
+							"headers": {
+							"Content-Length": [
+								"11"
+							],
+							"Content-Type": [
+								"text/plain"
+							],
+							"Date": [
+								"date"
+							],
+							"Hoverfly": [
+								"Was-Here"
+							]
+							}
+						},
+						"request": {
+							"requestType": "recording",
+							"path": "/",
+							"method": "GET",
+							"destination": "%v",
+							"scheme": "http",
+							"query": "",
+							"body": "",
+							"headers": {
+							"Accept-Encoding": [
+								"gzip"
+							],
+							"User-Agent": [
+								"Go-http-client/1.1"
+							]
+							}
+						}
+						}
+					]
+					}`, strings.Replace(fakeServer.URL, "http://", "", 1))))
 		})
 	})
 })
