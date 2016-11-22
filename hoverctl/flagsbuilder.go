45f552984432b0cd9aad9a1d5ba80d277363d2b0
Benji Hooper <benji@sixp.in>
2016-11-22 15:30:44 +0000

    Added the --disable-tls flag to disable tls-verficiation on Hoverfly

--- hoverctl/flagsbuilder.go
+++ hoverctl/flagsbuilder.go
@@ -4,18 +4,26 @@ type FlagsBuilder struct {
 	Webserver   string
 	Certificate string
 	Key         string
+	DisableTls  bool
 }
 
 func (this FlagsBuilder) BuildFlags() []string {
 	flags := []string{}
+
 	if this.Webserver == "webserver" {
 		flags = append(flags, "-webserver")
 	}
+
 	if this.Certificate != "" {
 		flags = append(flags, "-cert="+this.Certificate)
 	}
+
 	if this.Key != "" {
 		flags = append(flags, "-key="+this.Key)
+	}
+
+	if this.DisableTls {
+		flags = append(flags, "-tls-verification=false")
 	}
 
 	return flags
