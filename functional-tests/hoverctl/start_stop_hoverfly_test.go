92bdfe24cb0261c129afbd50f413b735674d179f
Benji Hooper <benji@sixp.in>
2016-11-23 16:38:07 +0000

    Added a functional test around launching Hoverfly with settings from config.yml

--- functional-tests/hoverctl/start_stop_hoverfly_test.go
+++ functional-tests/hoverctl/start_stop_hoverfly_test.go
@@ -267,5 +267,15 @@ var _ = Describe("When I use hoverctl", func() {
 				Expect(data).To(ContainSubstring("tls certificate verification is now turned off!"))
 			})
 		})
+
+		Context("You can start a hoverfly based on config from config.yml", func() {
+			It("will start on the admin and proxy ports", func() {
+				WriteConfiguration("localhost", "5543", "6478")
+				setOutput, _ := exec.Command(hoverctlBinary, "start", "-v").Output()
+
+				output := strings.TrimSpace(string(setOutput))
+				Expect(output).To(ContainSubstring("hoverfly -ap=5543 -pp=6478"))
+			})
+		})
 	})
 })
