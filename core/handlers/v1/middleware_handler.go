83fc611c18872f72cb0984f3ae573e2d5b28357f
Benji Hooper <benji@sixp.in>
2016-12-08 16:15:47 +0000

    v1 middleware API now serves redirects

--- core/handlers/v1/middleware_handler.go
+++ core/handlers/v1/middleware_handler.go
@@ -1,77 +1,28 @@
 package v1
 
 import (
-	"encoding/json"
-	log "github.com/Sirupsen/logrus"
+	"net/http"
+
 	"github.com/SpectoLabs/hoverfly/core/handlers"
 	"github.com/codegangsta/negroni"
 	"github.com/go-zoo/bone"
-	"io/ioutil"
-	"net/http"
 )
 
-type HoverflyMiddleware interface {
-	GetMiddleware() string
-	SetMiddleware(string) error
-}
-
-type MiddlewareHandler struct {
-	Hoverfly HoverflyMiddleware
-}
+type MiddlewareHandler struct{}
 
 func (this *MiddlewareHandler) RegisterRoutes(mux *bone.Mux, am *handlers.AuthHandler) {
 	mux.Get("/api/middleware", negroni.New(
 		negroni.HandlerFunc(am.RequireTokenAuthentication),
-		negroni.HandlerFunc(this.Get),
+		negroni.HandlerFunc(this.Redirect),
 	))
 
 	mux.Post("/api/middleware", negroni.New(
 		negroni.HandlerFunc(am.RequireTokenAuthentication),
-		negroni.HandlerFunc(this.Post),
+		negroni.HandlerFunc(this.Redirect),
 	))
 }
 
-func (this *MiddlewareHandler) Get(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-	var resp MiddlewareSchema
-
-	resp.Middleware = this.Hoverfly.GetMiddleware()
-
-	jsonResp, _ := json.Marshal(resp)
-
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.Write(jsonResp)
-}
-
-func (this *MiddlewareHandler) Post(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		// failed to read response body
-		log.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Error("Could not read response body!")
-		http.Error(w, "Failed to read request body.", 400)
-		return
-	}
-
-	var middlewareReq MiddlewareSchema
-
-	err = json.Unmarshal(body, &middlewareReq)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Error("Could not deserialize middleware")
-		http.Error(w, "Unable to deserialize request body.", 400)
-		return
-	}
-
-	err = this.Hoverfly.SetMiddleware(middlewareReq.Middleware)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Error("Could not execute middleware")
-		http.Error(w, "Invalid middleware: "+err.Error(), 400)
-		return
-	}
-
-	this.Get(w, req, next)
+func (this *MiddlewareHandler) Redirect(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+	http.Redirect(w, req, "/api/v2/hoverfly/middleware", http.StatusPermanentRedirect)
+	return
 }
