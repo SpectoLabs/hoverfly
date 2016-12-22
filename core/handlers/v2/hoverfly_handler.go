44c5486baaac76be076592ccc34c06033f07a4ae
Benji Hooper <benji@sixp.in>
2016-12-22 17:48:36 +0000

    Updated Hoverfly handler to use V2 middleware

--- core/handlers/v2/hoverfly_handler.go
+++ core/handlers/v2/hoverfly_handler.go
@@ -2,16 +2,17 @@ package v2
 
 import (
 	"encoding/json"
+	"net/http"
+
 	"github.com/SpectoLabs/hoverfly/core/handlers"
 	"github.com/SpectoLabs/hoverfly/core/metrics"
 	"github.com/codegangsta/negroni"
 	"github.com/go-zoo/bone"
-	"net/http"
 )
 
 type Hoverfly interface {
 	GetDestination() string
-	GetMiddleware() string
+	GetMiddlewareV2() (string, string, string)
 	GetMode() string
 	GetStats() metrics.Stats
 }
@@ -32,7 +33,7 @@ func (this *HoverflyHandler) Get(w http.ResponseWriter, req *http.Request, next 
 
 	hoverflyView.Destination = this.Hoverfly.GetDestination()
 	hoverflyView.Mode = this.Hoverfly.GetMode()
-	hoverflyView.Middleware = this.Hoverfly.GetMiddleware()
+	hoverflyView.Binary, hoverflyView.Script, hoverflyView.Remote = this.Hoverfly.GetMiddlewareV2()
 	hoverflyView.Usage = this.Hoverfly.GetStats()
 
 	bytes, _ := json.Marshal(hoverflyView)
