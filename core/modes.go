a225f51a8a8f5ac8ed9f511e9ce0965e93741b56
Benji Hooper <benji@sixp.in>
2017-01-06 19:48:54 +0000

    Moved CaptureMode into its own file

--- core/modes.go
+++ core/modes.go
@@ -12,19 +12,6 @@ type Mode interface {
 	Process(*http.Request, models.RequestDetails) (*http.Response, error)
 }
 
-type Simulate struct {
-	hoverfly *Hoverfly
-}
-
-func (this Simulate) Process(request *http.Request, details models.RequestDetails) (*http.Response, error) {
-	response, err := this.hoverfly.GetResponse(request, details)
-	if err != nil {
-		return hoverflyError(request, err, err.Error(), err.StatusCode), err
-	}
-
-	return response, nil
-}
-
 type Modify struct {
 	hoverfly *Hoverfly
 }
