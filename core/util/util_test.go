575b9ca066f5cc1eb07fbb6630e02512404f7e99
Benji Hooper <benji@sixp.in>
2017-01-06 15:34:53 +0000

    Removed extractBody as the new implementations are cleaner, make more sense and are tested

--- core/util/util_test.go
+++ core/util/util_test.go
@@ -12,23 +12,22 @@ import (
 func Test_GetRequestBody_GettingTheRequestBodyGetsTheCorrectData(t *testing.T) {
 	RegisterTestingT(t)
 
-	request, err := http.NewRequest("GET", "http://hoverfly.io", bytes.NewBuffer([]byte("test")))
-	Expect(err).To(BeNil())
+	request := &http.Request{}
+	request.Body = ioutil.NopCloser(bytes.NewBuffer([]byte("test")))
 
 	requestBody, err := GetRequestBody(request)
 	Expect(err).To(BeNil())
 
 	Expect(requestBody).To(Equal("test"))
-
 }
 
 func Test_GetRequestBody_GettingTheRequestBodySetsTheSameBodyAgain(t *testing.T) {
 	RegisterTestingT(t)
 
-	request, err := http.NewRequest("GET", "http://hoverfly.io", bytes.NewBuffer([]byte("test-preserve")))
-	Expect(err).To(BeNil())
+	request := &http.Request{}
+	request.Body = ioutil.NopCloser(bytes.NewBuffer([]byte("test-preserve")))
 
-	_, err = GetRequestBody(request)
+	_, err := GetRequestBody(request)
 	Expect(err).To(BeNil())
 
 	newRequestBody, err := ioutil.ReadAll(request.Body)
