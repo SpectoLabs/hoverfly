8692f09ca3130d56b73eb936f0e2ad4a23b1ac13
Benji Hooper <benji@sixp.in>
2017-01-09 15:35:39 +0000

    Added some basic tests for SimulateMode

---
+++ core/modes/simulate_mode_test.go
@@ -0,0 +1,69 @@
+package modes
+
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/SpectoLabs/hoverfly/core/matching"
+	"github.com/SpectoLabs/hoverfly/core/models"
+	. "github.com/onsi/gomega"
+)
+
+type hoverflyStub struct{}
+
+func (this hoverflyStub) GetResponse(request models.RequestDetails) (*models.ResponseDetails, *matching.MatchingError) {
+	if request.Destination == "positive-match.com" {
+		return &models.ResponseDetails{
+			Status: 200,
+		}, nil
+	} else {
+		return nil, &matching.MatchingError{
+			Description: "Test error",
+			StatusCode:  500,
+		}
+	}
+}
+
+func (this hoverflyStub) ApplyMiddlewareIfSet(pair models.RequestResponsePair) (models.RequestResponsePair, error) {
+	return pair, nil
+}
+
+func Test_SimulateMode_WhenGivenAMatchingRequestItReturnsTheCorrectResponse(t *testing.T) {
+	RegisterTestingT(t)
+
+	unit := &SimulateMode{
+		Hoverfly: hoverflyStub{},
+	}
+
+	request := models.RequestDetails{
+		Destination: "positive-match.com",
+	}
+
+	response, err := unit.Process(nil, request)
+	Expect(err).To(BeNil())
+
+	Expect(response.StatusCode).To(Equal(200))
+}
+
+func Test_SimulateMode_WhenGivenANonMatchingRequestItReturnsAnError(t *testing.T) {
+	RegisterTestingT(t)
+
+	unit := &SimulateMode{
+		Hoverfly: hoverflyStub{},
+	}
+
+	request := models.RequestDetails{
+		Destination: "negative-match.com",
+	}
+
+	response, err := unit.Process(&http.Request{}, request)
+	Expect(err).ToNot(BeNil())
+
+	Expect(response.StatusCode).To(Equal(500))
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	Expect(err).To(BeNil())
+
+	Expect(string(responseBody)).To(Equal("Hoverfly Error! Test error. Got error: Test error \n"))
+}
