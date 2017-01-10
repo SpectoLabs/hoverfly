e77e05aa378ebf7c178580555791151a37cdcb56
Benji Hooper <benji@sixp.in>
2017-01-10 17:18:41 +0000

    Removed old Synthesize function

--- core/modes.go
+++ core/modes.go
@@ -6,7 +6,6 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/SpectoLabs/hoverfly/core/models"
-	"github.com/SpectoLabs/hoverfly/core/modes"
 )
 
 type Modify struct {
@@ -25,40 +24,4 @@ func (this Modify) Process(request *http.Request, details models.RequestDetails)
 	}
 
 	return response, nil
-}
-
-type Synthesize struct {
-	hoverfly *Hoverfly
-}
-
-func (this Synthesize) Process(request *http.Request, details models.RequestDetails) (*http.Response, error) {
-	pair := models.RequestResponsePair{Request: details}
-
-	log.WithFields(log.Fields{
-		"middleware":  this.hoverfly.Cfg.Middleware.toString(),
-		"body":        details.Body,
-		"destination": details.Destination,
-	}).Debug("Synthesizing new response")
-
-	if !this.hoverfly.IsMiddlewareSet() {
-		err := fmt.Errorf("Middleware not set")
-		return hoverflyError(request, err, "Synthesize failed, middleware not provided", http.StatusServiceUnavailable), err
-	}
-
-	pair, err := this.hoverfly.ApplyMiddleware(pair)
-
-	if err != nil {
-		return hoverflyError(request, err, "Could not create synthetic response!", http.StatusServiceUnavailable), err
-	}
-
-	log.WithFields(log.Fields{
-		"mode":        this.hoverfly.Cfg.Mode,
-		"middleware":  this.hoverfly.Cfg.Middleware,
-		"path":        request.URL.Path,
-		"rawQuery":    request.URL.RawQuery,
-		"method":      request.Method,
-		"destination": request.Host,
-	}).Info("synthetic response created successfuly")
-
-	return modes.ReconstructResponse(request, pair), nil
 }
