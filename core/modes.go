8524a5ab956797e86771e91b16bdb37b327545ed
Benji Hooper <benji@sixp.in>
2017-01-10 13:29:35 +0000

    Moved Capture mode into the modes package

--- core/modes.go
+++ core/modes.go
@@ -1,15 +1,12 @@
 package hoverfly
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/SpectoLabs/hoverfly/core/models"
 	"github.com/SpectoLabs/hoverfly/core/modes"
-	"github.com/SpectoLabs/hoverfly/core/util"
 )
 
 type Modify struct {
@@ -64,68 +61,4 @@ func (this Synthesize) Process(request *http.Request, details models.RequestDeta
 	}).Info("synthetic response created successfuly")
 
 	return modes.ReconstructResponse(request, pair), nil
-}
-
-type Capture struct {
-	hoverfly *Hoverfly
-}
-
-func (this Capture) Process(request *http.Request, details models.RequestDetails) (*http.Response, error) {
-	// response, err := this.hoverfly.captureRequest(request)
-
-	// this is mainly for testing, since when you create
-	if request.Body == nil {
-		request.Body = ioutil.NopCloser(bytes.NewBuffer([]byte("")))
-	}
-
-	// outputting request body if verbose logging is set
-	log.WithFields(log.Fields{
-		"body": details.Body,
-		"mode": "capture",
-	}).Debug("got request body")
-
-	modifiedReq, response, err := this.hoverfly.DoRequest(request)
-
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error":       err.Error(),
-			"mode":        "capture",
-			"Path":        request.URL.Path,
-			"Method":      request.Method,
-			"Destination": request.Host,
-			"Scheme":      request.URL.Scheme,
-			"Query":       request.URL.RawQuery,
-			"Body":        string(details.Body),
-			"Headers":     request.Header,
-		}).Error("Got error when executing request")
-
-		return nil, err
-	}
-
-	requestObj, _ := models.NewRequestDetailsFromHttpRequest(modifiedReq)
-
-	respBody, _ := util.GetResponseBody(response)
-
-	responseObj := &models.ResponseDetails{
-		Status:  response.StatusCode,
-		Body:    string(respBody),
-		Headers: response.Header,
-	}
-
-	// saving response body with request/response meta to cache
-	this.hoverfly.Save(&requestObj, responseObj)
-
-	if err != nil {
-		return hoverflyError(request, err, "Could not capture request", http.StatusServiceUnavailable), err
-	}
-	log.WithFields(log.Fields{
-		"mode":        "capture",
-		"middleware":  this.hoverfly.Cfg.Middleware,
-		"path":        request.URL.Path,
-		"rawQuery":    request.URL.RawQuery,
-		"method":      request.Method,
-		"destination": request.Host,
-	}).Info("request and response captured")
-
-	return response, nil
 }
