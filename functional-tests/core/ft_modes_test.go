a0d4340e2297960721f17eabe13036dcf72a58f2
Benji Hooper <benji@sixp.in>
2017-01-20 17:03:36 +0000

    Moved capture tests without middleware into their own test file now, and they also use the new Hoverfly functional test wrapper

--- functional-tests/core/ft_modes_test.go
+++ functional-tests/core/ft_modes_test.go
@@ -2,7 +2,6 @@ package hoverfly_test
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -11,7 +10,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/SpectoLabs/hoverfly/core/views"
 	"github.com/dghubble/sling"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -23,188 +21,6 @@ var _ = Describe("Running Hoverfly in various modes", func() {
 
 		var fakeServer *httptest.Server
 		var fakeServerUrl *url.URL
-
-		Context("without middleware", func() {
-
-			BeforeEach(func() {
-				hoverflyCmd = startHoverfly(adminPort, proxyPort)
-
-				SetHoverflyMode("capture")
-			})
-
-			AfterEach(func() {
-				stopHoverfly()
-			})
-
-			It("Should capture the request and response", func() {
-
-				fakeServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					w.Header().Set("Content-Type", "text/plain")
-					w.Header().Set("Date", "date")
-					w.Write([]byte("Hello world"))
-				}))
-
-				defer fakeServer.Close()
-
-				fakeServerUrl, _ = url.Parse(fakeServer.URL)
-				resp := CallFakeServerThroughProxy(fakeServer)
-				Expect(resp.StatusCode).To(Equal(200))
-
-				expectedDestination := strings.Replace(fakeServerUrl.String(), "http://", "", 1)
-
-				recordsJson, err := ioutil.ReadAll(ExportHoverflyRecords())
-				Expect(err).To(BeNil())
-				Expect(recordsJson).To(MatchJSON(fmt.Sprintf(
-					`{
-					  "data": [
-					    {
-					      "response": {
-						"status": 200,
-						"body": "Hello world",
-						"encodedBody": false,
-						"headers": {
-						  "Content-Length": [
-						    "11"
-						  ],
-						  "Content-Type": [
-						    "text/plain"
-						  ],
-						  "Date": [
-						    "date"
-						  ],
-						  "Hoverfly": [
-						    "Was-Here"
-						  ]
-						}
-					      },
-					      "request": {
-					      	"requestType": "recording",
-						"path": "/",
-						"method": "GET",
-						"destination": "%v",
-						"scheme": "http",
-						"query": "",
-						"body": "",
-						"headers": {
-						  "Accept-Encoding": [
-						    "gzip"
-						  ],
-						  "User-Agent": [
-						    "Go-http-client/1.1"
-						  ]
-						}
-					      }
-					    }
-					  ]
-					}`, expectedDestination)))
-			})
-
-			It("Should capture the request and response in capture mode after the same request is made in simulate mode", func() {
-
-				fakeServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					w.Header().Set("Content-Type", "text/plain")
-					w.Header().Set("Date", "date")
-					w.Write([]byte("Hello world"))
-				}))
-
-				defer fakeServer.Close()
-
-				fakeServerUrl, _ = url.Parse(fakeServer.URL)
-
-				SetHoverflyMode("simulate")
-				resp := CallFakeServerThroughProxy(fakeServer)
-				Expect(resp.StatusCode).To(Equal(http.StatusBadGateway))
-				SetHoverflyMode("capture")
-
-				resp = CallFakeServerThroughProxy(fakeServer)
-				Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-				expectedDestination := strings.Replace(fakeServerUrl.String(), "http://", "", 1)
-
-				recordsJson, err := ioutil.ReadAll(ExportHoverflyRecords())
-				Expect(err).To(BeNil())
-				Expect(recordsJson).To(MatchJSON(fmt.Sprintf(
-					`{
-					  "data": [
-					    {
-					      "response": {
-						"status": 200,
-						"body": "Hello world",
-						"encodedBody": false,
-						"headers": {
-						  "Content-Length": [
-						    "11"
-						  ],
-						  "Content-Type": [
-						    "text/plain"
-						  ],
-						  "Date": [
-						    "date"
-						  ],
-						  "Hoverfly": [
-						    "Was-Here"
-						  ]
-						}
-					      },
-					      "request": {
-					      	"requestType": "recording",
-						"path": "/",
-						"method": "GET",
-						"destination": "%v",
-						"scheme": "http",
-						"query": "",
-						"body": "",
-						"headers": {
-						  "Accept-Encoding": [
-						    "gzip"
-						  ],
-						  "User-Agent": [
-						    "Go-http-client/1.1"
-						  ]
-						}
-					      }
-					    }
-					  ]
-					}`, expectedDestination)))
-			})
-
-			It("Should capture a redirect response", func() {
-
-				fakeServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					w.Header().Set("Content-Type", "text/plain")
-					w.Header().Set("Date", "date")
-					w.Write([]byte("redirection got you here"))
-				}))
-				fakeServerUrl, _ := url.Parse(fakeServer.URL)
-				fakeServerRedirect := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					w.Header().Set("Location", fakeServer.URL)
-					w.Header().Set("Content-Type", "text/plain")
-					w.WriteHeader(301)
-				}))
-				fakeServerRedirectUrl, _ := url.Parse(fakeServerRedirect.URL)
-
-				defer fakeServer.Close()
-				defer fakeServerRedirect.Close()
-
-				resp := CallFakeServerThroughProxy(fakeServerRedirect)
-				Expect(resp.StatusCode).To(Equal(301))
-
-				expectedRedirectDestination := strings.Replace(fakeServerRedirectUrl.String(), "http://", "", 1)
-
-				recordsJson, err := ioutil.ReadAll(ExportHoverflyRecords())
-				Expect(err).To(BeNil())
-
-				payload := views.RequestResponsePairPayload{}
-
-				json.Unmarshal(recordsJson, &payload)
-				Expect(payload.Data).To(HaveLen(1))
-
-				Expect(payload.Data[0].Request.Destination).To(Equal(expectedRedirectDestination))
-
-				Expect(payload.Data[0].Response.Status).To(Equal(301))
-				Expect(payload.Data[0].Response.Headers["Location"][0]).To(Equal(fakeServerUrl.String()))
-			})
-		})
 
 		Context("with middleware", func() {
 			BeforeEach(func() {
