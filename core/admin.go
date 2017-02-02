ffa26a47c11cdb154b4331c40dbfc73cfb63a883
Benji Hooper <benji@sixp.in>
2017-02-02 10:56:13 +0000

    Added functional test for new version API endpoint

--- core/admin.go
+++ core/admin.go
@@ -116,6 +116,7 @@ func GetAllHandlers(hoverfly *Hoverfly) []handlers.AdminHandler {
 	list = append(list, &v2.HoverflyModeHandler{Hoverfly: hoverfly})
 	list = append(list, &v2.HoverflyMiddlewareHandler{Hoverfly: hoverfly})
 	list = append(list, &v2.HoverflyUsageHandler{Hoverfly: hoverfly})
+	list = append(list, &v2.HoverflyVersionHandler{Hoverfly: hoverfly})
 	list = append(list, &v2.SimulationHandler{Hoverfly: hoverfly})
 
 	return list
