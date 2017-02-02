cfc66c56062be9f5464f413fde99b811e1e05c06
Benji Hooper <benji@sixp.in>
2017-02-02 10:56:13 +0000

    Added functional test for new version API endpoint

--- functional-tests/core/ft_api_test.go
+++ functional-tests/core/ft_api_test.go
@@ -490,6 +490,18 @@ var _ = Describe("Interacting with the API", func() {
 		})
 	})
 
+	Context("GET /api/v2/hoverfly/mode", func() {
+
+		It("Should get the mode", func() {
+			req := sling.New().Get(hoverflyAdminUrl + "/api/v2/hoverfly/version")
+			res := functional_tests.DoRequest(req)
+			Expect(res.StatusCode).To(Equal(200))
+			modeJson, err := ioutil.ReadAll(res.Body)
+			Expect(err).To(BeNil())
+			Expect(string(modeJson)).To(MatchRegexp(`{"version":"v\d+.\d+.\d+"}`))
+		})
+	})
+
 	Context("GET /api/records", func() {
 
 		BeforeEach(func() {
