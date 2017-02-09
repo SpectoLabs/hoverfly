8b1027ea35fe8b237e5d3fcc77d8818647bd3b82
Tom Cunliffe <tjcunliffe@users.noreply.github.com>
2017-02-09 12:47:08 +0000

    Update middleware.go

--- hoverctl/cmd/middleware.go
+++ hoverctl/cmd/middleware.go
@@ -11,18 +11,18 @@ var middlewareBinary, middlewareScript, middlewareRemote string
 
 var middlewareCmd = &cobra.Command{
 	Use:   "middleware",
-	Short: "Get and set Hoverfly's middleware",
+	Short: "Get and set Hoverfly middleware",
 	Long: `
-Without flags, middleware will print the middleware
-configuration values from Hoverfly.
-
-When flags are provided, those flags will be set on
-Hoverfly. Hoverfly can be set with the following
+Hoverfly middleware can be set using the following
 combinations of flags: 
 
 	--binary
 	--binary --script
 	--remote
+
+If flags are not used, the current Hoverfly middleware
+configuration will be shown.
+
 `,
 
 	Run: func(cmd *cobra.Command, args []string) {
@@ -32,19 +32,19 @@ combinations of flags:
 		if middlewareBinary == "" && middlewareScript == "" && middlewareRemote == "" {
 			middleware, err = hoverfly.GetMiddleware()
 			handleIfError(err)
-			log.Info("Hoverfly is currently set to run the following as middleware")
+			log.Info("Hoverfly middleware configuration is currently set to")
 		} else {
 			if middlewareRemote != "" {
 				middleware, err = hoverfly.SetMiddleware("", "", middlewareRemote)
 				handleIfError(err)
-				log.Info("Hoverfly is now set to run the following as middleware")
+				log.Info("Hoverfly middleware configuration has been set to")
 			} else {
 				script, err := wrapper.ReadFile(middlewareScript)
 				handleIfError(err)
 
 				middleware, err = hoverfly.SetMiddleware(middlewareBinary, string(script), "")
 				handleIfError(err)
-				log.Info("Hoverfly is now set to run the following as middleware")
+				log.Info("Hoverfly middleware configuration has been set to")
 			}
 		}
 
@@ -65,9 +65,9 @@ combinations of flags:
 func init() {
 	RootCmd.AddCommand(middlewareCmd)
 	middlewareCmd.PersistentFlags().StringVar(&middlewareBinary, "binary", "",
-		"Given a binary, the binary will be executed by Hoverfly as middleware")
+		"An absolute or relative path to a binary that Hoverfly will execute as middleware")
 	middlewareCmd.PersistentFlags().StringVar(&middlewareScript, "script", "",
-		"Given a path, the contents will be read and will be executed with a binary by Hoverfly as middleware")
+		"An absolute or relative path to a script that will be executed by the middleware binary")
 	middlewareCmd.PersistentFlags().StringVar(&middlewareRemote, "remote", "",
-		"Given a URL, the URL will The remote address will be called by Hoverfly as middleware")
+		"A URL to a remote address that will be called by Hoverfly as middleware")
 }
