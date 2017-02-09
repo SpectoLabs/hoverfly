6a4e8d407453b36e2ff586ca21bb879b840ab0a3
Tom Cunliffe <tjcunliffe@users.noreply.github.com>
2017-02-09 12:19:23 +0000

    Update export.go

--- hoverctl/cmd/export.go
+++ hoverctl/cmd/export.go
@@ -8,10 +8,10 @@ import (
 
 var exportCmd = &cobra.Command{
 	Use:   "export [path to simulation]",
-	Short: "export a simulation into Hoverfly",
+	Short: "export a simulation from Hoverfly",
 	Long: `
-Will export a simulation from Hoverfly. The simulation
-will be written to the file provied.
+Exports a simulation from Hoverfly. The simulation JSON
+will be written to the file path provided.
 	`,
 
 	Run: func(cmd *cobra.Command, args []string) {
@@ -21,7 +21,7 @@ will be written to the file provied.
 		err = wrapper.WriteFile(args[0], simulationData)
 		handleIfError(err)
 
-		log.Info("Successfully exported to ", args[0])
+		log.Info("Successfully exported simulation to ", args[0])
 	},
 }
 
