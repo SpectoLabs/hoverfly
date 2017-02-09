a3ab26887334a2bb7cce634f2e9e3d3a0d4c496e
Tom Cunliffe <tjcunliffe@users.noreply.github.com>
2017-02-09 12:23:15 +0000

    Update import.go

--- hoverctl/cmd/import.go
+++ hoverctl/cmd/import.go
@@ -13,9 +13,9 @@ var importCmd = &cobra.Command{
 	Use:   "import [path to simulation]",
 	Short: "Import a simulation into Hoverfly",
 	Long: `
-Will import a simulation into Hoverfly. The simulation
-provided can be an absolute or relative path to a 
-JSON file.
+Imports a simulation into Hoverfly. An absolute or
+relative path to a Hoverfly simulation JSON file
+must be provided.
 	`,
 
 	Run: func(cmd *cobra.Command, args []string) {
@@ -25,11 +25,11 @@ JSON file.
 		err = hoverfly.ImportSimulation(string(simulationData), importV1)
 		handleIfError(err)
 
-		log.Info("Successfully imported from ", args[0])
+		log.Info("Successfully imported simulation from ", args[0])
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(importCmd)
-	importCmd.Flags().BoolVar(&importV1, "v1", false, "Tells Hoverfly that this simulation is formatted in the old v1 style schema used in Hoverfly pre v0.9.0")
+	importCmd.Flags().BoolVar(&importV1, "v1", false, "Tells Hoverfly that the simulation is formatted according to the old v1 simulation JSON schema used in Hoverfly pre v0.9.0")
 }
