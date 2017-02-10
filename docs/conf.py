bd4369fb29b5a34418f6d14b34c0cd4668555b58
Tom Cunliffe <tom.cunliffe@specto.io>
2017-02-10 16:46:22 +0000

    try customization again

--- docs/conf.py
+++ docs/conf.py
@@ -154,13 +154,13 @@ if 'READTHEDOCS' not in os.environ:
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
-#html_context = {
-#    'css_files': [
-#        '_static/theme_overrides.css',  
-#        ],
-#    }
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',  
+        ],
+    }
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
