ae0a558a613155f86f1d5d04bdd4a31fdd7dc841
Benji Hooper <benji@sixp.in>
2017-02-17 13:48:48 +0000

    Moved headerMatch into template_matcher.go

--- core/matching/request_template_store.go
+++ core/matching/request_template_store.go
@@ -3,7 +3,6 @@ package matching
 import (
 	"errors"
 	"fmt"
-	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/SpectoLabs/hoverfly/core/handlers/v1"
@@ -90,39 +89,6 @@ func (this *RequestTemplateStore) ImportPayloads(pairPayload v1.RequestTemplateR
 func (this *RequestTemplateStore) Wipe() {
 	// don't change the pointer here!
 	*this = RequestTemplateStore{}
-}
-
-/**
-Check keys and corresponding values in template headers are also present in request headers
-*/
-func headerMatch(templateHeaders, requestHeaders map[string][]string) bool {
-
-	for templateHeaderKey, templateHeaderValues := range templateHeaders {
-		for requestHeaderKey, requestHeaderValues := range requestHeaders {
-			delete(requestHeaders, requestHeaderKey)
-			requestHeaders[strings.ToLower(requestHeaderKey)] = requestHeaderValues
-
-		}
-
-		requestTemplateValues, templateHeaderMatched := requestHeaders[strings.ToLower(templateHeaderKey)]
-		if !templateHeaderMatched {
-			return false
-		}
-
-		for _, templateHeaderValue := range templateHeaderValues {
-			templateValueMatched := false
-			for _, requestHeaderValue := range requestTemplateValues {
-				if glob.Glob(strings.ToLower(templateHeaderValue), strings.ToLower(requestHeaderValue)) {
-					templateValueMatched = true
-				}
-			}
-
-			if !templateValueMatched {
-				return false
-			}
-		}
-	}
-	return true
 }
 
 func (this RequestTemplateStore) GetPayload() v1.RequestTemplateResponsePairPayload {
---
+++ core/matching/template_matcher_test.go
@@ -0,0 +1,145 @@
+package matching
+
+import (
+	"testing"
+
+	"github.com/SpectoLabs/hoverfly/core/models"
+	. "github.com/onsi/gomega"
+)
+
+// func Test_Match_EmptyTemplateShouldMatchOnAnyRequest(t *testing.T) {
+// 	RegisterTestingT(t)
+
+// 	var templates []interfaces.RequestResponsePair
+
+// 	templates = append(templates, models.RequestResponsePair{
+// 		Request: models.RequestDetails{},
+// 		Response: models.ResponseDetails{
+// 			Body: "test-body",
+// 		},
+// 	})
+
+// 	r := models.RequestDetails{
+// 		Method:      "GET",
+// 		Destination: "somehost.com",
+// 		Headers: map[string][]string{
+// 			"sdv": []string{"ascd"},
+// 		},
+// 	}
+// 	result, _ := Match(r, false, templates)
+
+// 	Expect(result.Body).To(Equal("test-body"))
+// }
+
+func Test_headerMatch_ReturnResponseWithMultiValuedHeaderMatch(t *testing.T) {
+	RegisterTestingT(t)
+
+	response := models.ResponseDetails{
+		Body: "test-body",
+	}
+	headers := map[string][]string{
+		"header1": []string{"val1-a", "val1-b"},
+		"header2": []string{"val2"},
+	}
+	templateEntry := RequestTemplateResponsePair{
+		RequestTemplate: RequestTemplate{
+			Headers: headers,
+		},
+		Response: response,
+	}
+	store := RequestTemplateStore{templateEntry}
+
+	r := models.RequestDetails{
+		Method:      "GET",
+		Destination: "http://somehost.com",
+		Body:        "test-body",
+		Headers: map[string][]string{
+			"header1": []string{"val1-a", "val1-b"},
+			"header2": []string{"val2"},
+		},
+	}
+	result, _ := store.GetResponse(r, false)
+
+	Expect(result.Body).To(Equal("test-body"))
+}
+
+func Test_headerMatch(t *testing.T) {
+	RegisterTestingT(t)
+
+	tmplHeaders := map[string][]string{
+		"header1": []string{"val1"},
+		"header2": []string{"val2"},
+	}
+
+	res := headerMatch(tmplHeaders, tmplHeaders)
+	Expect(res).To(BeTrue())
+}
+
+func Test_headerMatch_IgnoreTestCaseInsensitive(t *testing.T) {
+	RegisterTestingT(t)
+
+	tmplHeaders := map[string][]string{
+		"header1": []string{"val1"},
+		"header2": []string{"val2"},
+	}
+	reqHeaders := map[string][]string{
+		"HEADER1": []string{"val1"},
+		"Header2": []string{"VAL2"},
+	}
+	res := headerMatch(tmplHeaders, reqHeaders)
+	Expect(res).To(BeTrue())
+}
+
+func Test_HeaderMatch_MatchingTemplateHasMoreHeaderKeysThanRequestMatchesFalse(t *testing.T) {
+	RegisterTestingT(t)
+
+	tmplHeaders := map[string][]string{
+		"header1": []string{"val1"},
+		"header2": []string{"val2"},
+	}
+	reqHeaders := map[string][]string{
+		"header1": []string{"val1"},
+	}
+	res := headerMatch(tmplHeaders, reqHeaders)
+	Expect(res).To(BeFalse())
+}
+
+func Test_headerMatch_MatchingTemplateHasMoreHeaderValuesThanRequestMatchesFalse(t *testing.T) {
+	RegisterTestingT(t)
+
+	tmplHeaders := map[string][]string{
+		"header2": []string{"val1", "val2"},
+	}
+	reqHeaders := map[string][]string{
+		"header2": []string{"val1"},
+	}
+	res := headerMatch(tmplHeaders, reqHeaders)
+	Expect(res).To(BeFalse())
+}
+
+func Test_headerMatch_MatchingRequestHasMoreHeaderKeysThanTemplateMatchesFalse(t *testing.T) {
+	RegisterTestingT(t)
+
+	tmplHeaders := map[string][]string{
+		"header2": []string{"val2"},
+	}
+	reqHeaders := map[string][]string{
+		"HEADER1": []string{"val1"},
+		"header2": []string{"val2"},
+	}
+	res := headerMatch(tmplHeaders, reqHeaders)
+	Expect(res).To(BeTrue())
+}
+
+func Test_headerMatch_MatchingRequestHasMoreHeaderValuesThanTemplateMatchesFalse(t *testing.T) {
+	RegisterTestingT(t)
+
+	tmplHeaders := map[string][]string{
+		"header2": []string{"val2"},
+	}
+	reqHeaders := map[string][]string{
+		"header2": []string{"val1", "val2"},
+	}
+	res := headerMatch(tmplHeaders, reqHeaders)
+	Expect(res).To(BeTrue())
+}
