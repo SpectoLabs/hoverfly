4c0c1c78dffe779b1a5e06dfebb523d88ea32e4d
Benji Hooper <benji@sixp.in>
2017-02-17 15:04:45 +0000

    Removed v1 template API as no longer being used by hoverctl

--- core/admin.go
+++ core/admin.go
@@ -110,7 +110,6 @@ func GetAllHandlers(hoverfly *Hoverfly) []handlers.AdminHandler {
 	list = append(list, &v1.RecordsHandler{Hoverfly: hoverfly})
 	list = append(list, &v1.StateHandler{Hoverfly: hoverfly})
 	list = append(list, &v1.StatsHandler{Hoverfly: hoverfly})
-	list = append(list, &v1.TemplatesHandler{Hoverfly: hoverfly})
 	list = append(list, &v2.HoverflyHandler{Hoverfly: hoverfly})
 	list = append(list, &v2.HoverflyDestinationHandler{Hoverfly: hoverfly})
 	list = append(list, &v2.HoverflyModeHandler{Hoverfly: hoverfly})
