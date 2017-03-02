ebf60bcce597a146635c6e6ed793ec6e1aacab8c
Benji Hooper <benji@sixp.in>
2017-03-02 16:15:55 +0000

    Added functional tests for the logs endpoint

---
+++ functional-tests/core/ft_api_v2_logs_test.go
@@ -0,0 +1,69 @@
+package hoverfly_test
+
+import (
+	"io/ioutil"
+
+	"github.com/SpectoLabs/hoverfly/functional-tests"
+	"github.com/antonholmquist/jason"
+	"github.com/dghubble/sling"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("/api/v2/logs", func() {
+
+	var (
+		hoverfly *functional_tests.Hoverfly
+	)
+
+	BeforeEach(func() {
+		hoverfly = functional_tests.NewHoverfly()
+		hoverfly.Start()
+		hoverfly.SetMode("simulate")
+		hoverfly.ImportSimulation(functional_tests.JsonPayload)
+		hoverfly.Proxy(sling.New().Get("http://template-server.com"))
+	})
+
+	AfterEach(func() {
+		hoverfly.Stop()
+	})
+
+	Context("GET", func() {
+
+		It("should get logs", func() {
+			req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs")
+			res := functional_tests.DoRequest(req)
+			Expect(res.StatusCode).To(Equal(200))
+			responseJson, err := ioutil.ReadAll(res.Body)
+			Expect(err).To(BeNil())
+
+			jsonObject, err := jason.NewObjectFromBytes(responseJson)
+			Expect(err).To(BeNil())
+
+			logsArray, err := jsonObject.GetObjectArray("logs")
+			Expect(err).To(BeNil())
+
+			Expect(len(logsArray)).To(BeNumerically(">", 1))
+
+			Expect(logsArray[0].GetString("msg")).Should(Equal("payloads imported"))
+		})
+
+		It("should limit the logs it returns", func() {
+			req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs?limit=1")
+			res := functional_tests.DoRequest(req)
+			Expect(res.StatusCode).To(Equal(200))
+			responseJson, err := ioutil.ReadAll(res.Body)
+			Expect(err).To(BeNil())
+
+			jsonObject, err := jason.NewObjectFromBytes(responseJson)
+			Expect(err).To(BeNil())
+
+			logsArray, err := jsonObject.GetObjectArray("logs")
+			Expect(err).To(BeNil())
+
+			Expect(len(logsArray)).To(Equal(1))
+
+			Expect(logsArray[0].GetString("msg")).Should(Equal("payloads imported"))
+		})
+	})
+})
