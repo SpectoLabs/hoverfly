2d543caee07ce286179d8a51a686d715e2e4dfe7
Benji Hooper <benji@sixp.in>
2017-03-02 14:39:10 +0000

    StoreLogHook prepends entries instead of append

--- core/store_logs_hook_test.go
+++ core/store_logs_hook_test.go
@@ -27,7 +27,24 @@ func Test_StoreLogsHook_Fire_SavesEntryToEntriesArray(t *testing.T) {
 
 	Expect(unit.Entries).To(HaveLen(1))
 	Expect(unit.Entries[0].Message).To(Equal("test entry"))
+}
 
+func Test_StoreLogsHook_Fire_LogsArePrepended(t *testing.T) {
+	RegisterTestingT(t)
+
+	unit := NewStoreLogsHook()
+
+	unit.Fire(&logrus.Entry{
+		Message: "oldest",
+	})
+
+	unit.Fire(&logrus.Entry{
+		Message: "newest",
+	})
+
+	Expect(unit.Entries).To(HaveLen(2))
+	Expect(unit.Entries[0].Message).To(Equal("newest"))
+	Expect(unit.Entries[1].Message).To(Equal("oldest"))
 }
 
 func Test_StoreLogsHook_GetLogsView_BuildsLogsViewFromEntriesArray(t *testing.T) {
