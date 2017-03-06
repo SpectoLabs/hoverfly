a579b1dcf0942cb041569a8818c0840f6dbf55cc
Benji Hooper <benji@sixp.in>
2017-03-06 15:23:15 +0000

    Added an upgrade function for v1 simulations

--- core/handlers/v2/simulation_views_test.go
+++ core/handlers/v2/simulation_views_test.go
@@ -29,3 +29,121 @@ func Test_RequestDetailsViewV1_GetQuery_ReturnsNilIfNil(t *testing.T) {
 	queryString := unit.GetQuery()
 	Expect(queryString).To(BeNil())
 }
+
+func Test_SimulationViewV1_Upgrade_ReturnsAV2Simulation(t *testing.T) {
+	RegisterTestingT(t)
+
+	unit := v2.SimulationViewV1{
+		v2.DataViewV1{
+			RequestResponsePairViewV1: []v2.RequestResponsePairViewV1{
+				v2.RequestResponsePairViewV1{
+					Request: v2.RequestDetailsViewV1{
+						Scheme:      util.StringToPointer("http"),
+						Body:        util.StringToPointer("body"),
+						Destination: util.StringToPointer("destination"),
+						Method:      util.StringToPointer("GET"),
+						Path:        util.StringToPointer("/path"),
+						Query:       util.StringToPointer("query=query"),
+						Headers: map[string][]string{
+							"Test": []string{"headers"},
+						},
+					},
+					Response: v2.ResponseDetailsView{
+						Status:      200,
+						Body:        "body",
+						EncodedBody: false,
+						Headers: map[string][]string{
+							"Test": []string{"headers"},
+						},
+					},
+				},
+			},
+		},
+		v2.MetaView{
+			SchemaVersion:   "v1",
+			HoverflyVersion: "test",
+			TimeExported:    "today",
+		},
+	}
+
+	simulationViewV2 := unit.Upgrade()
+
+	Expect(simulationViewV2.RequestResponsePairs).To(HaveLen(1))
+
+	Expect(*simulationViewV2.RequestResponsePairs[0].Request.Scheme).To(Equal(v2.RequestFieldMatchersView{
+		ExactMatch: util.StringToPointer("http"),
+	}))
+	Expect(*simulationViewV2.RequestResponsePairs[0].Request.Body).To(Equal(v2.RequestFieldMatchersView{
+		ExactMatch: util.StringToPointer("body"),
+	}))
+	Expect(*simulationViewV2.RequestResponsePairs[0].Request.Destination).To(Equal(v2.RequestFieldMatchersView{
+		ExactMatch: util.StringToPointer("destination"),
+	}))
+	Expect(*simulationViewV2.RequestResponsePairs[0].Request.Method).To(Equal(v2.RequestFieldMatchersView{
+		ExactMatch: util.StringToPointer("GET"),
+	}))
+	Expect(*simulationViewV2.RequestResponsePairs[0].Request.Path).To(Equal(v2.RequestFieldMatchersView{
+		ExactMatch: util.StringToPointer("/path"),
+	}))
+	Expect(*simulationViewV2.RequestResponsePairs[0].Request.Query).To(Equal(v2.RequestFieldMatchersView{
+		ExactMatch: util.StringToPointer("query=query"),
+	}))
+	Expect(simulationViewV2.RequestResponsePairs[0].Request.Headers).To(HaveKeyWithValue("Test", []string{"headers"}))
+
+	Expect(simulationViewV2.RequestResponsePairs[0].Response.Status).To(Equal(200))
+	Expect(simulationViewV2.RequestResponsePairs[0].Response.Body).To(Equal("body"))
+	Expect(simulationViewV2.RequestResponsePairs[0].Response.EncodedBody).To(BeFalse())
+	Expect(simulationViewV2.RequestResponsePairs[0].Response.Headers).To(HaveKeyWithValue("Test", []string{"headers"}))
+
+	Expect(simulationViewV2.SchemaVersion).To(Equal("v2"))
+	Expect(simulationViewV2.HoverflyVersion).To(Equal("test"))
+	Expect(simulationViewV2.TimeExported).To(Equal("today"))
+}
+
+func Test_SimulationViewV1_Upgrade_CanReturnAnIncompleteRequest(t *testing.T) {
+	RegisterTestingT(t)
+
+	unit := v2.SimulationViewV1{
+		v2.DataViewV1{
+			RequestResponsePairViewV1: []v2.RequestResponsePairViewV1{
+				v2.RequestResponsePairViewV1{
+					Request: v2.RequestDetailsViewV1{
+						Method: util.StringToPointer("POST"),
+					},
+					Response: v2.ResponseDetailsView{
+						Status:      200,
+						Body:        "body",
+						EncodedBody: false,
+						Headers: map[string][]string{
+							"Test": []string{"headers"},
+						},
+					},
+				},
+			},
+		},
+		v2.MetaView{
+			SchemaVersion:   "v1",
+			HoverflyVersion: "test",
+			TimeExported:    "today",
+		},
+	}
+
+	simulationViewV2 := unit.Upgrade()
+
+	Expect(simulationViewV2.RequestResponsePairs).To(HaveLen(1))
+
+	Expect(simulationViewV2.RequestResponsePairs[0].Request.Scheme).To(BeNil())
+	Expect(simulationViewV2.RequestResponsePairs[0].Request.Body).To(BeNil())
+	Expect(simulationViewV2.RequestResponsePairs[0].Request.Destination).To(BeNil())
+	Expect(*simulationViewV2.RequestResponsePairs[0].Request.Method).To(Equal(v2.RequestFieldMatchersView{
+		ExactMatch: util.StringToPointer("POST"),
+	}))
+	Expect(simulationViewV2.RequestResponsePairs[0].Request.Path).To(BeNil())
+	Expect(simulationViewV2.RequestResponsePairs[0].Request.Query).To(BeNil())
+	Expect(simulationViewV2.RequestResponsePairs[0].Request.Headers).To(BeNil())
+
+	Expect(simulationViewV2.RequestResponsePairs[0].Response.Status).To(Equal(200))
+	Expect(simulationViewV2.RequestResponsePairs[0].Response.Body).To(Equal("body"))
+	Expect(simulationViewV2.RequestResponsePairs[0].Response.EncodedBody).To(BeFalse())
+	Expect(simulationViewV2.RequestResponsePairs[0].Response.Headers).To(HaveKeyWithValue("Test", []string{"headers"}))
+}
