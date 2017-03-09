29776f9c2e44c987d6ef9c4b3ac6b733ba7c399e
Benji Hooper <benji@sixp.in>
2017-03-09 11:14:57 +0000

    Added a functional test for jsonpath matching

--- functional-tests/testdata.go
+++ functional-tests/testdata.go
@@ -125,6 +125,34 @@ var XpathSimulation = `{
     }
 }`
 
+var JsonMatchSimulation = `{
+    "data": {
+        "pairs": [
+            {
+                "response": {
+                    "status": 200,
+                    "body": "json match",
+                    "encodedBody": false,
+                    "headers": {}
+                },
+                "request": {
+                    "body": {
+						"jsonMatch": "$.items[4]"
+                    }
+                }
+            }
+        ],
+        "globalActions": {
+            "delays": []
+        }
+    },
+    "meta": {
+        "schemaVersion": "v2",
+        "hoverflyVersion": "v0.10.2",
+        "timeExported": "2017-02-23T12:43:48Z"
+    }
+}`
+
 var JsonSimulationGetAndPost = `{
     "data": {
         "pairs": [
