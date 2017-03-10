aeb8899b20f32849560fbb2d11033c414a07bc0a
Benji Hooper <benji@sixp.in>
2017-03-10 15:40:39 +0000

    Moved matching tests to their own package

--- core/matching/field_matcher_test.go
+++ core/matching/field_matcher_test.go
@@ -1,9 +1,10 @@
-package matching
+package matching_test
 
 import (
 	"encoding/xml"
 	"testing"
 
+	"github.com/SpectoLabs/hoverfly/core/matching"
 	"github.com/SpectoLabs/hoverfly/core/models"
 	"github.com/SpectoLabs/hoverfly/core/util"
 	. "github.com/onsi/gomega"
@@ -12,7 +13,7 @@ import (
 func Test_FieldMatcher_MatchesTrueWithExactMatch(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		ExactMatch: util.StringToPointer("yes"),
 	}, "yes")).To(BeTrue())
 }
@@ -20,7 +21,7 @@ func Test_FieldMatcher_MatchesTrueWithExactMatch(t *testing.T) {
 func Test_FieldMatcher_MatchesFalseWithIncorrectExactMatch(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		ExactMatch: util.StringToPointer("yes"),
 	}, "no")).To(BeFalse())
 }
@@ -28,13 +29,13 @@ func Test_FieldMatcher_MatchesFalseWithIncorrectExactMatch(t *testing.T) {
 func Test_FieldMatcher_MatchesTrueWithNilMatchers(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(nil, "no")).To(BeTrue())
+	Expect(matching.FieldMatcher(nil, "no")).To(BeTrue())
 }
 
 func Test_FieldMatcher_MatchesTrueWithXpathMatch(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XpathMatch: util.StringToPointer("/root/text"),
 	}, xml.Header+"<root><text>test</text></root>")).To(BeTrue())
 }
@@ -42,7 +43,7 @@ func Test_FieldMatcher_MatchesTrueWithXpathMatch(t *testing.T) {
 func Test_FieldMatcher_MatchesFalseWithIncorectXpathMatch(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XpathMatch: util.StringToPointer("/pop"),
 	}, xml.Header+"<root><text>test</text></root>")).To(BeFalse())
 }
@@ -50,7 +51,7 @@ func Test_FieldMatcher_MatchesFalseWithIncorectXpathMatch(t *testing.T) {
 func Test_FieldMatcher_MatchesTrueWithXpathMatch_GetAnElementFromAnArray(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XpathMatch: util.StringToPointer("/list/item[1]/field"),
 	}, xml.Header+"<list><item><field>test</field></item></list>")).To(BeTrue())
 }
@@ -58,7 +59,7 @@ func Test_FieldMatcher_MatchesTrueWithXpathMatch_GetAnElementFromAnArray(t *test
 func Test_FieldMatcher_MatchesFalseWithInvalidXpathMatch_GetAnElementFromAnArray(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XpathMatch: util.StringToPointer("/list/item[1]/pop"),
 	}, xml.Header+"<list><item><field>test</field></item></list>")).To(BeFalse())
 }
@@ -66,7 +67,7 @@ func Test_FieldMatcher_MatchesFalseWithInvalidXpathMatch_GetAnElementFromAnArray
 func Test_FieldMatcher_MatchesTrueWithXpathMatch_GetAttributeFromElement(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XpathMatch: util.StringToPointer("/list/item/field[@test]"),
 	}, xml.Header+"<list><item><field test=\"value\">test</field></item></list>")).To(BeTrue())
 }
@@ -74,7 +75,7 @@ func Test_FieldMatcher_MatchesTrueWithXpathMatch_GetAttributeFromElement(t *test
 func Test_FieldMatcher_MatchesFalseWithInvalidXpathMatch_GetAttributeFromElement(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XpathMatch: util.StringToPointer("/list/item/field[@pop]"),
 	}, xml.Header+"<list><item><field test=\"value\">test</field></item></list>")).To(BeFalse())
 }
@@ -82,7 +83,7 @@ func Test_FieldMatcher_MatchesFalseWithInvalidXpathMatch_GetAttributeFromElement
 func Test_FieldMatcher_MatchesTrueWithXpathMatch_GetElementWithNoValue(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XpathMatch: util.StringToPointer("/list/item/field"),
 	}, xml.Header+"<list><item><field></field></item></list>")).To(BeTrue())
 }
@@ -90,7 +91,7 @@ func Test_FieldMatcher_MatchesTrueWithXpathMatch_GetElementWithNoValue(t *testin
 func Test_FieldMatcher_MatchesFalseWithInvalidJsonPath(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonPathMatch: util.StringToPointer("test"),
 	}, `{"test": "field"}`)).To(BeFalse())
 }
@@ -98,7 +99,7 @@ func Test_FieldMatcher_MatchesFalseWithInvalidJsonPath(t *testing.T) {
 func Test_FieldMatcher_MatchesTrueWithJsonMatch_GetSingleElement(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonPathMatch: util.StringToPointer("$.test"),
 	}, `{"test": "field"}`)).To(BeTrue())
 }
@@ -106,7 +107,7 @@ func Test_FieldMatcher_MatchesTrueWithJsonMatch_GetSingleElement(t *testing.T) {
 func Test_FieldMatcher_MatchesFalseWithIncorrectJsonMatch_GetSingleElement(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonPathMatch: util.StringToPointer("$.notAField"),
 	}, `{"test": "field"}`)).To(BeFalse())
 }
@@ -114,7 +115,7 @@ func Test_FieldMatcher_MatchesFalseWithIncorrectJsonMatch_GetSingleElement(t *te
 func Test_FieldMatcher_MatchesTrueWithJsonMatch_GetElementFromArray(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonPathMatch: util.StringToPointer("$.test[1]"),
 	}, `{"test": [{}, {}]}`)).To(BeTrue())
 }
@@ -122,7 +123,7 @@ func Test_FieldMatcher_MatchesTrueWithJsonMatch_GetElementFromArray(t *testing.T
 func Test_FieldMatcher_MatchesFalseWithIncorrectJsonMatch_GetElementFromArray(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonPathMatch: util.StringToPointer("$.test[2]"),
 	}, `{"test": [{}, {}]}`)).To(BeFalse())
 }
@@ -130,7 +131,7 @@ func Test_FieldMatcher_MatchesFalseWithIncorrectJsonMatch_GetElementFromArray(t 
 func Test_FieldMatcher_MatchesTrueWithJsonMatch_WithExpression(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonPathMatch: util.StringToPointer("$.test[*]?(@.field == \"test\")"),
 	}, `{"test": [{"field": "test"}]}`)).To(BeTrue())
 }
@@ -138,7 +139,7 @@ func Test_FieldMatcher_MatchesTrueWithJsonMatch_WithExpression(t *testing.T) {
 func Test_FieldMatcher_MatchesFalseWithIncorrectJsonMatch_WithExpression(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonPathMatch: util.StringToPointer("$.test[*]?(@.field == \"test\")"),
 	}, `{"test": [{"field": "not-test"}]}`)).To(BeFalse())
 }
@@ -146,7 +147,7 @@ func Test_FieldMatcher_MatchesFalseWithIncorrectJsonMatch_WithExpression(t *test
 func Test_FieldMatcher_MatchesTrueWithRegexMatch(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		RegexMatch: util.StringToPointer("t[o|a|e]st"),
 	}, `test`)).To(BeTrue())
 }
@@ -154,7 +155,7 @@ func Test_FieldMatcher_MatchesTrueWithRegexMatch(t *testing.T) {
 func Test_FieldMatcher_MatchesFalseWithIncorrectRegexMatch(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		RegexMatch: util.StringToPointer("t[o|a]st"),
 	}, `test`)).To(BeFalse())
 }
@@ -162,7 +163,7 @@ func Test_FieldMatcher_MatchesFalseWithIncorrectRegexMatch(t *testing.T) {
 func Test_FieldMatcher_MatchesTrueWithGlobMatch(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		RegexMatch: util.StringToPointer("t*st"),
 	}, `test`)).To(BeTrue())
 }
@@ -170,7 +171,7 @@ func Test_FieldMatcher_MatchesTrueWithGlobMatch(t *testing.T) {
 func Test_FieldMatcher_MatchesFalseWithIncorrectGlobMatch(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(FieldMatcher(&models.RequestFieldMatchers{
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		RegexMatch: util.StringToPointer("t*st"),
 	}, `tset`)).To(BeFalse())
 }
