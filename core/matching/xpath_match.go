dca7be5adc469ced21d393b5ea32463ff82aa7df
Benji Hooper <benji@sixp.in>
2017-03-13 17:01:23 +0000

    Moved each match into its own file

---
+++ core/matching/xpath_match.go
@@ -0,0 +1,27 @@
+package matching
+
+import (
+	"bytes"
+
+	"github.com/ChrisTrenkamp/goxpath"
+	"github.com/ChrisTrenkamp/goxpath/tree/xmltree"
+)
+
+func XpathMatch(matchingString *string, toMatch string) bool {
+	xpathRule, err := goxpath.Parse(*matchingString)
+	if err != nil {
+		return false
+	}
+
+	xTree, err := xmltree.ParseXML(bytes.NewBufferString(toMatch))
+	if err != nil {
+		return false
+	}
+
+	results, err := xpathRule.ExecNode(xTree)
+	if err != nil {
+		return false
+	}
+
+	return len(results) > 0
+}
