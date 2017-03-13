5219f8f059eb75c613815b009232a37402df7d64
Benji Hooper <benji@sixp.in>
2017-03-13 17:01:23 +0000

    Moved each match into its own file

---
+++ core/matching/exact_match.go
@@ -0,0 +1,5 @@
+package matching
+
+func ExactMatch(matchingString *string, toMatch string) bool {
+	return *matchingString == toMatch
+}
---
+++ core/matching/json_match.go
@@ -0,0 +1,19 @@
+package matching
+
+import "github.com/NodePrime/jsonpath"
+
+func JsonMatch(matchingString *string, toMatch string) bool {
+	paths, err := jsonpath.ParsePaths(*matchingString)
+	if err != nil {
+		return false
+	}
+
+	eval, err := jsonpath.EvalPathsInBytes([]byte(toMatch), paths)
+	if err != nil {
+		return false
+	}
+
+	_, ok := eval.Next()
+
+	return ok
+}
