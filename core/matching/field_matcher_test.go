56a64d5ecb7eef5214f9557c3173d9912273256e
Benji Hooper <benji@sixp.in>
2017-03-13 17:08:05 +0000

    You can now use multiple matchers and it'll will check each of them

--- core/matching/field_matcher_test.go
+++ core/matching/field_matcher_test.go
@@ -175,3 +175,30 @@ func Test_FieldMatcher_MatchesFalseWithIncorrectGlobMatch(t *testing.T) {
 		RegexMatch: util.StringToPointer("t*st"),
 	}, `tset`)).To(BeFalse())
 }
+
+func Test_FieldMatcher_MatchesTrueWithACombinationOfMatchers(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
+		ExactMatch: util.StringToPointer("testtesttest"),
+		RegexMatch: util.StringToPointer("test"),
+	}, `testtesttest`)).To(BeTrue())
+}
+
+func Test_FieldMatcher_MatchesFalseWithACombinationOfMatchers(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
+		ExactMatch: util.StringToPointer("testtesttest"),
+		RegexMatch: util.StringToPointer("tst"),
+	}, `testtesttest`)).To(BeFalse())
+}
+
+func Test_FieldMatcher_MatchesFalseWithADifferentCombinationOfMatchers(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
+		GlobMatch:  util.StringToPointer("*test"),
+		RegexMatch: util.StringToPointer("tst"),
+	}, `testtesttest`)).To(BeFalse())
+}
