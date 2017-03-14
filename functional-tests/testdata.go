171132505e2a88cb4eacce479c0a9a140ef55354
Benji Hooper <benji@sixp.in>
2017-03-14 11:19:40 +0000

    Added functional tests for multiple matchers

--- functional-tests/testdata.go
+++ functional-tests/testdata.go
@@ -209,6 +209,49 @@ var GlobMatchSimulation = `{
     }
 }`
 
+var MultipleMatchSimulation = `{
+    "data": {
+        "pairs": [
+            {
+                "response": {
+                    "status": 200,
+                    "body": "multiple matches",
+                    "encodedBody": false,
+                    "headers": {}
+                },
+                "request": {
+                    "body": {
+						"globMatch": "*<item field=*>*",
+                        "regexMatch": "something"
+                    }
+                }
+            },
+            {
+                "response": {
+                    "status": 200,
+                    "body": "multiple matches 2",
+                    "encodedBody": false,
+                    "headers": {}
+                },
+                "request": {
+                    "destination": {
+                        "exactMatch": "destination.com",
+                        "globMatch": "*.com"
+                    }
+                }
+            }
+        ],
+        "globalActions": {
+            "delays": []
+        }
+    },
+    "meta": {
+        "schemaVersion": "v2",
+        "hoverflyVersion": "v0.10.2",
+        "timeExported": "2017-02-23T12:43:48Z"
+    }
+}`
+
 var JsonSimulationGetAndPost = `{
     "data": {
         "pairs": [
