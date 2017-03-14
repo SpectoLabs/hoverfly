1426a5f9b0c37c874558b571b876a5e931187648
Benji Hooper <benji@sixp.in>
2017-03-14 09:49:24 +0000

    Moved tests for GlobMatch into their only go file

--- core/matching/glob_match.go
+++ core/matching/glob_match.go
@@ -2,6 +2,6 @@ package matching
 
 import glob "github.com/ryanuber/go-glob"
 
-func GlobMatch(matchingString *string, toMatch string) bool {
-	return glob.Glob(*matchingString, toMatch)
+func GlobMatch(matchingString string, toMatch string) bool {
+	return glob.Glob(matchingString, toMatch)
 }
