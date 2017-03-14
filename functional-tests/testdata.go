e6fd0d189fe552459188e3468d97513ac6cf29bf
Benji Hooper <benji@sixp.in>
2017-03-14 15:14:28 +0000

    Added functional tests for xmlMatch matcher

--- functional-tests/testdata.go
+++ functional-tests/testdata.go
@@ -97,6 +97,34 @@ var JsonPayload = `{
     }
 }`
 
+var XmlSimulation = `{
+    "data": {
+        "pairs": [
+            {
+                "response": {
+                    "status": 200,
+                    "body": "xml match",
+                    "encodedBody": false,
+                    "headers": {}
+                },
+                "request": {
+                    "body": {
+						"xmlMatch": "<items><item>one</item></items>"
+                    }
+                }
+            }
+        ],
+        "globalActions": {
+            "delays": []
+        }
+    },
+    "meta": {
+        "schemaVersion": "v2",
+        "hoverflyVersion": "v0.10.2",
+        "timeExported": "2017-02-23T12:43:48Z"
+    }
+}`
+
 var XpathSimulation = `{
     "data": {
         "pairs": [
