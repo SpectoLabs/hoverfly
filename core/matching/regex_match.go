25a8ed03f67cef41884ed923ac4b3eef0216ccd5
Benji Hooper <benji@sixp.in>
2017-03-14 09:45:05 +0000

    Moved tests for RegexMatch into their only go file

--- core/matching/regex_match.go
+++ core/matching/regex_match.go
@@ -2,8 +2,8 @@ package matching
 
 import "regexp"
 
-func RegexMatch(matchingString *string, toMatch string) bool {
-	match, err := regexp.MatchString(*matchingString, toMatch)
+func RegexMatch(matchingString string, toMatch string) bool {
+	match, err := regexp.MatchString(matchingString, toMatch)
 	if err != nil {
 		return false
 	}
