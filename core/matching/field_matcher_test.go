1a2439b2f57e83fa525354c05a7f6e1d94b33ab1
Benji Hooper <benji@sixp.in>
2017-03-14 10:28:08 +0000

    Adding tests for FieldMatcher

--- core/matching/field_matcher_test.go
+++ core/matching/field_matcher_test.go
@@ -1,6 +1,7 @@
 package matching_test
 
 import (
+	"encoding/xml"
 	"testing"
 
 	"github.com/SpectoLabs/hoverfly/core/matching"
@@ -9,13 +10,19 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func Test_FieldMatcher_MatchesTrueWithNilMatchers(t *testing.T) {
+func Test_FieldMatcher_MatchesTrue_WithNilMatchers(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(matching.FieldMatcher(nil, "no")).To(BeTrue())
+	Expect(matching.FieldMatcher(nil, "test")).To(BeTrue())
 }
 
-func Test_FieldMatcher_MatchesTrueWithACombinationOfMatchers(t *testing.T) {
+func Test_FieldMatcher_MatchesTrue_WithMatchersNotDefined(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{}, "test")).To(BeTrue())
+}
+
+func Test_FieldMatcher_WithMultipleMatchers_MatchesTrue(t *testing.T) {
 	RegisterTestingT(t)
 
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
@@ -24,7 +31,16 @@ func Test_FieldMatcher_MatchesTrueWithACombinationOfMatchers(t *testing.T) {
 	}, `testtesttest`)).To(BeTrue())
 }
 
-func Test_FieldMatcher_MatchesFalseWithACombinationOfMatchers(t *testing.T) {
+func Test_FieldMatcher_WithMultipleMatchers_AlsoMatchesTrue(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
+		XpathMatch: util.StringToPointer("/list/item[1]/field"),
+		RegexMatch: util.StringToPointer("test"),
+	}, xml.Header+"<list><item><field>test</field></item></list>")).To(BeTrue())
+}
+
+func Test_FieldMatcher_WithMultipleMatchers_MatchesFalse(t *testing.T) {
 	RegisterTestingT(t)
 
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
@@ -33,11 +49,11 @@ func Test_FieldMatcher_MatchesFalseWithACombinationOfMatchers(t *testing.T) {
 	}, `testtesttest`)).To(BeFalse())
 }
 
-func Test_FieldMatcher_MatchesFalseWithADifferentCombinationOfMatchers(t *testing.T) {
+func Test_FieldMatcher__WithMultipleMatchers_AlsoMatchesFalse(t *testing.T) {
 	RegisterTestingT(t)
 
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
-		GlobMatch:  util.StringToPointer("*test"),
-		RegexMatch: util.StringToPointer("tst"),
+		GlobMatch:     util.StringToPointer("*test"),
+		JsonPathMatch: util.StringToPointer("$.test[1]"),
 	}, `testtesttest`)).To(BeFalse())
 }
