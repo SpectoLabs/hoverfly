fb7a98d8a7e9d360e15aa643f91050f24402a76c
Benji Hooper <benji@sixp.in>
2017-03-31 18:28:03 +0100

    Added a way of making default targets

--- hoverctl/wrapper/target.go
+++ hoverctl/wrapper/target.go
@@ -6,4 +6,22 @@ type Target struct {
 	AdminPort int    `yaml:"admin.port"`
 	AuthToken string `yaml:"auth.token"`
 	Pid       int    `yaml:"pid"`
+
+	Webserver bool
+	ProxyPort int
+	CachePath string
+
+	CertificatePath  string
+	KeyPath          string
+	DisableTls       bool
+	UpstreamProxyUrl string
+}
+
+func NewDefaultTarget() *Target {
+	return &Target{
+		Name:      "default",
+		Host:      "localhost",
+		AdminPort: 8888,
+		ProxyPort: 8500,
+	}
 }
