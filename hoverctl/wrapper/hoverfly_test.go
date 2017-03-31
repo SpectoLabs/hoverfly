12c8149b2a64f8d0a236c69bcfe760522404f3fd
Benji Hooper <benji@sixp.in>
2017-03-31 15:57:45 +0100

    isLocal has no need to be tidied to a struct

--- hoverctl/wrapper/hoverfly_test.go
+++ hoverctl/wrapper/hoverfly_test.go
@@ -6,32 +6,20 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func Test_Hoverfly_isLocal_WhenLocalhost(t *testing.T) {
+func Test_isLocal_WhenLocalhost(t *testing.T) {
 	RegisterTestingT(t)
 
-	hoverfly := Hoverfly{Host: "localhost"}
-
-	result := hoverfly.isLocal()
-
-	Expect(result).To(BeTrue())
+	Expect(isLocal("localhost")).To(BeTrue())
 }
 
-func Test_Hoverfly_isLocal_WhenLocalhostIP(t *testing.T) {
+func Test_isLocal_WhenLocalhostIP(t *testing.T) {
 	RegisterTestingT(t)
 
-	hoverfly := Hoverfly{Host: "127.0.0.1"}
-
-	result := hoverfly.isLocal()
-
-	Expect(result).To(BeTrue())
+	Expect(isLocal("127.0.0.1")).To(BeTrue())
 }
 
-func Test_Hoverfly_isLocal_WhenAnotherDNS(t *testing.T) {
+func Test_isLocal_WhenAnotherDNS(t *testing.T) {
 	RegisterTestingT(t)
 
-	hoverfly := Hoverfly{Host: "specto.io"}
-
-	result := hoverfly.isLocal()
-
-	Expect(result).To(BeFalse())
+	Expect(isLocal("specto.io")).To(BeFalse())
 }
