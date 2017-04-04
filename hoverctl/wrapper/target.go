d40f4face220676dd1c1d43c54028ba2c378f8d1
Benji Hooper <benji@sixp.in>
2017-04-04 12:42:41 +0100

    Moved target creation from config into target.go

--- hoverctl/wrapper/target.go
+++ hoverctl/wrapper/target.go
@@ -25,3 +25,35 @@ func NewDefaultTarget() *Target {
 		ProxyPort: 8500,
 	}
 }
+
+func getTargetsFromConfig(configTargets map[string]interface{}) map[string]Target {
+	targets := map[string]Target{}
+
+	for key, target := range configTargets {
+		targetMap := target.(map[interface{}]interface{})
+
+		targetHoverfly := Target{}
+
+		targetHoverfly.Name = key
+
+		if targetMap["host"] != nil {
+			targetHoverfly.Host = targetMap["host"].(string)
+		}
+
+		if targetMap["admin.port"] != nil {
+			targetHoverfly.AdminPort = targetMap["admin.port"].(int)
+		}
+
+		if targetMap["auth.token"] != nil {
+			targetHoverfly.AuthToken = targetMap["auth.token"].(string)
+		}
+
+		if targetMap["pid"] != nil {
+			targetHoverfly.Pid = targetMap["pid"].(int)
+		}
+
+		targets[key] = targetHoverfly
+	}
+
+	return targets
+}
---
+++ hoverctl/wrapper/target_test.go
@@ -0,0 +1,55 @@
+package wrapper
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func Test_getTargetsFromConfig_host(t *testing.T) {
+	RegisterTestingT(t)
+
+	targets := getTargetsFromConfig(map[string]interface{}{
+		"default": map[interface{}]interface{}{
+			"host": "test.org",
+		},
+	})
+
+	Expect(targets).To(HaveLen(1))
+	Expect(targets).To(HaveKeyWithValue("default", Target{
+		Name: "default",
+		Host: "test.org",
+	}))
+}
+
+func Test_getTargetsFromConfig_adminport(t *testing.T) {
+	RegisterTestingT(t)
+
+	targets := getTargetsFromConfig(map[string]interface{}{
+		"other": map[interface{}]interface{}{
+			"admin.port": 1234,
+		},
+	})
+
+	Expect(targets).To(HaveLen(1))
+	Expect(targets).To(HaveKeyWithValue("other", Target{
+		Name:      "other",
+		AdminPort: 1234,
+	}))
+}
+
+func Test_getTargetsFromConfig_authtoken(t *testing.T) {
+	RegisterTestingT(t)
+
+	targets := getTargetsFromConfig(map[string]interface{}{
+		"anotherother": map[interface{}]interface{}{
+			"auth.token": "token123:456",
+		},
+	})
+
+	Expect(targets).To(HaveLen(1))
+	Expect(targets).To(HaveKeyWithValue("anotherother", Target{
+		Name:      "anotherother",
+		AuthToken: "token123:456",
+	}))
+}
