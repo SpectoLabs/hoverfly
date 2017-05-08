72cb9bcccabd0f47fcc89d7d117450781fc49554
Benji Hooper <benji@sixp.in>
2017-05-08 18:42:06 +0100

    Added config host, admin-port, proxy-port and auth-token to make it easier to get these values out of config files

--- functional-tests/hoverctl/config_test.go
+++ functional-tests/hoverctl/config_test.go
@@ -8,6 +8,14 @@ import (
 
 var _ = Describe("When I use hoverctl", func() {
 
+	BeforeEach(func() {
+		functional_tests.Run(hoverctlBinary, "targets", "create", "default")
+	})
+
+	AfterEach(func() {
+		WipeConfig()
+	})
+
 	Context("I can get the configuration being used by hoverctl", func() {
 
 		It("prints the location of the config.yaml", func() {
@@ -28,7 +36,101 @@ var _ = Describe("When I use hoverctl", func() {
 			Expect(output).To(ContainSubstring("admin.port: 8888"))
 			Expect(output).To(ContainSubstring("proxy.port: 8500"))
 		})
-
 	})
 
+	Context("Can get the target host", func() {
+
+		It("returns the host config of a target", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-host", "--host", "test.host.com")
+			output := functional_tests.Run(hoverctlBinary, "-t", "test-host", "config", "host")
+
+			Expect(output).To(ContainSubstring("test.host.com"))
+		})
+
+		It("returns the host config of the default target", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-host-default", "--host", "test.host.default.com")
+			functional_tests.Run(hoverctlBinary, "targets", "default", "test-host-default")
+			output := functional_tests.Run(hoverctlBinary, "config", "host")
+
+			Expect(output).To(ContainSubstring("test.host.default.com"))
+		})
+	})
+
+	Context("Can get the target admin-port", func() {
+
+		It("returns the admin-port config of a target", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-admin-port", "--admin-port", "2345")
+			output := functional_tests.Run(hoverctlBinary, "-t", "test-admin-port", "config", "admin-port")
+
+			Expect(output).To(ContainSubstring("2345"))
+		})
+
+		It("returns the admin-port config of the default target", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-admin-port-default", "--admin-port", "1234")
+			functional_tests.Run(hoverctlBinary, "targets", "default", "test-admin-port-default")
+			output := functional_tests.Run(hoverctlBinary, "config", "admin-port")
+
+			Expect(output).To(ContainSubstring("1234"))
+		})
+	})
+
+	Context("Can get the target proxy-port", func() {
+
+		It("returns the proxy-port config of a target", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-proxy-port", "--proxy-port", "2345")
+			output := functional_tests.Run(hoverctlBinary, "-t", "test-proxy-port", "config", "proxy-port")
+
+			Expect(output).To(ContainSubstring("2345"))
+		})
+
+		It("returns the proxy-port config of the default target", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-proxy-port-default", "--proxy-port", "1234")
+			functional_tests.Run(hoverctlBinary, "targets", "default", "test-proxy-port-default")
+			output := functional_tests.Run(hoverctlBinary, "config", "proxy-port")
+
+			Expect(output).To(ContainSubstring("1234"))
+		})
+	})
+
+	Context("Can get the target auth-token", func() {
+
+		var hoverfly *functional_tests.Hoverfly
+
+		BeforeEach(func() {
+			hoverfly = functional_tests.NewHoverfly()
+			hoverfly.Start()
+		})
+
+		AfterEach(func() {
+			hoverfly.Stop()
+		})
+
+		It("returns the auth-token config of a target", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-auth-token", "--admin-port", hoverfly.GetAdminPort())
+
+			functional_tests.Run(hoverctlBinary, "-t", "test-auth-token", "login", "--username", "benjih", "--password", "password")
+			output := functional_tests.Run(hoverctlBinary, "-t", "test-auth-token", "config", "auth-token")
+
+			Expect(output).ToNot(ContainSubstring("No auth token"))
+		})
+
+		It("returns the auth-token config of the default target", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-auth-token-default", "--admin-port", hoverfly.GetAdminPort())
+			functional_tests.Run(hoverctlBinary, "targets", "default", "test-auth-token-default")
+
+			functional_tests.Run(hoverctlBinary, "login", "--username", "benjih", "--password", "password")
+			output := functional_tests.Run(hoverctlBinary, "config", "auth-token")
+
+			Expect(output).ToNot(ContainSubstring("No auth token"))
+		})
+
+		It("returns an error if there is no auth-token", func() {
+			functional_tests.Run(hoverctlBinary, "targets", "create", "test-auth-token-default", "--admin-port", hoverfly.GetAdminPort())
+
+			functional_tests.Run(hoverctlBinary, "login", "--username", "benjih", "--password", "password")
+			output := functional_tests.Run(hoverctlBinary, "config", "auth-token")
+
+			Expect(output).To(ContainSubstring("No auth token"))
+		})
+	})
 })
