16b4c08d4753856cb4b6654b3c0d38324ee65aca
mogronalol <andrew.morgan@opencredo.com>
2017-05-24 10:36:24 +0200

    Fixed #353 Refactored FieldMatching to return a zero count with each match by default

--- core/matching/field_matcher.go
+++ core/matching/field_matcher.go
@@ -4,38 +4,50 @@ import (
 	"github.com/SpectoLabs/hoverfly/core/models"
 )
 
-func FieldMatcher(field *models.RequestFieldMatchers, toMatch string) bool {
+func FieldMatcher(field *models.RequestFieldMatchers, toMatch string) * FieldMatch {
 	if field == nil {
-		return true
+		return countlessFieldMatch(true)
 	}
 
 	if field.ExactMatch != nil && !ExactMatch(*field.ExactMatch, toMatch) {
-		return false
+		return countlessFieldMatch(false)
 	}
 
 	if field.XmlMatch != nil && !XmlMatch(*field.XmlMatch, toMatch) {
-		return false
+		return countlessFieldMatch(false)
 	}
 
 	if field.XpathMatch != nil && !XpathMatch(*field.XpathMatch, toMatch) {
-		return false
+		return countlessFieldMatch(false)
 	}
 
 	if field.JsonMatch != nil && !JsonMatch(*field.JsonMatch, toMatch) {
-		return false
+		return countlessFieldMatch(false)
 	}
 
 	if field.JsonPathMatch != nil && !JsonPathMatch(*field.JsonPathMatch, toMatch) {
-		return false
+		return countlessFieldMatch(false)
 	}
 
 	if field.RegexMatch != nil && !RegexMatch(*field.RegexMatch, toMatch) {
-		return false
+		return countlessFieldMatch(false)
 	}
 
 	if field.GlobMatch != nil && !GlobMatch(*field.GlobMatch, toMatch) {
-		return false
+		return countlessFieldMatch(false)
 	}
 
-	return true
+	return countlessFieldMatch(true)
+}
+
+func countlessFieldMatch(matched bool)  * FieldMatch  {
+	return &FieldMatch{
+		Matched: matched,
+		TotalMatches: 0,
+	}
+}
+
+type FieldMatch struct {
+	Matched bool
+	TotalMatches int
 }
