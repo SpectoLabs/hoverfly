c8167c29b65429a8acecbf9066e90eb65f25cf8f
mogronalol <andrew.morgan@opencredo.com>
2017-05-24 10:36:24 +0200

    Fixed #353 Refactored FieldMatching to return a zero count with each match by default

--- core/matching/field_matcher_test.go
+++ core/matching/field_matcher_test.go
@@ -13,7 +13,7 @@ import (
 func Test_FieldMatcher_MatchesTrue_WithNilMatchers(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(matching.FieldMatcher(nil, "test")).To(BeTrue())
+	Expect(matching.FieldMatcher(nil, "test").Matched).To(BeTrue())
 }
 
 func Test_FieldMatcher_MatchesTrueWithJsonMatch(t *testing.T) {
@@ -21,7 +21,7 @@ func Test_FieldMatcher_MatchesTrueWithJsonMatch(t *testing.T) {
 
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonMatch: util.StringToPointer(`{"test":true}`),
-	}, `{"test": true}`)).To(BeTrue())
+	}, `{"test": true}`).Matched).To(BeTrue())
 }
 
 func Test_FieldMatcher_MatchesFalseWithJsonMatch(t *testing.T) {
@@ -29,7 +29,7 @@ func Test_FieldMatcher_MatchesFalseWithJsonMatch(t *testing.T) {
 
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		JsonMatch: util.StringToPointer(`{"test":true}`),
-	}, `{"test": [ ] }`)).To(BeFalse())
+	}, `{"test": [ ] }`).Matched).To(BeFalse())
 }
 
 func Test_FieldMatcher_MatchesTrueWithXmlMatch(t *testing.T) {
@@ -37,7 +37,7 @@ func Test_FieldMatcher_MatchesTrueWithXmlMatch(t *testing.T) {
 
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XmlMatch: util.StringToPointer(`<document></document>`),
-	}, `<document></document>`)).To(BeTrue())
+	}, `<document></document>`).Matched).To(BeTrue())
 }
 
 func Test_FieldMatcher_MatchesFalseWithXmlMatch(t *testing.T) {
@@ -47,13 +47,13 @@ func Test_FieldMatcher_MatchesFalseWithXmlMatch(t *testing.T) {
 		XmlMatch: util.StringToPointer(`<document></document>`),
 	}, `<document>
 		<test>data</test>
-	</document>`)).To(BeFalse())
+	</document>`).Matched).To(BeFalse())
 }
 
 func Test_FieldMatcher_MatchesTrue_WithMatchersNotDefined(t *testing.T) {
 	RegisterTestingT(t)
 
-	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{}, "test")).To(BeTrue())
+	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{}, "test").Matched).To(BeTrue())
 }
 
 func Test_FieldMatcher_WithMultipleMatchers_MatchesTrue(t *testing.T) {
@@ -62,7 +62,7 @@ func Test_FieldMatcher_WithMultipleMatchers_MatchesTrue(t *testing.T) {
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		ExactMatch: util.StringToPointer("testtesttest"),
 		RegexMatch: util.StringToPointer("test"),
-	}, `testtesttest`)).To(BeTrue())
+	}, `testtesttest`).Matched).To(BeTrue())
 }
 
 func Test_FieldMatcher_WithMultipleMatchers_AlsoMatchesTrue(t *testing.T) {
@@ -71,7 +71,7 @@ func Test_FieldMatcher_WithMultipleMatchers_AlsoMatchesTrue(t *testing.T) {
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		XpathMatch: util.StringToPointer("/list/item[1]/field"),
 		RegexMatch: util.StringToPointer("test"),
-	}, xml.Header+"<list><item><field>test</field></item></list>")).To(BeTrue())
+	}, xml.Header+"<list><item><field>test</field></item></list>").Matched).To(BeTrue())
 }
 
 func Test_FieldMatcher_WithMultipleMatchers_MatchesFalse(t *testing.T) {
@@ -80,7 +80,7 @@ func Test_FieldMatcher_WithMultipleMatchers_MatchesFalse(t *testing.T) {
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		ExactMatch: util.StringToPointer("testtesttest"),
 		RegexMatch: util.StringToPointer("tst"),
-	}, `testtesttest`)).To(BeFalse())
+	}, `testtesttest`).Matched).To(BeFalse())
 }
 
 func Test_FieldMatcher__WithMultipleMatchers_AlsoMatchesFalse(t *testing.T) {
@@ -89,5 +89,17 @@ func Test_FieldMatcher__WithMultipleMatchers_AlsoMatchesFalse(t *testing.T) {
 	Expect(matching.FieldMatcher(&models.RequestFieldMatchers{
 		GlobMatch:     util.StringToPointer("*test"),
 		JsonPathMatch: util.StringToPointer("$.test[1]"),
-	}, `testtesttest`)).To(BeFalse())
+	}, `testtesttest`).Matched).To(BeFalse())
 }
+
+//func Test_FieldMatcher_CountsMatchesOnMatch(t *testing.T)  {
+//	RegisterTestingT(t)
+//
+//	_, count := matching.FieldMatcher(&models.RequestFieldMatchers{
+//		GlobMatch:  util.StringToPointer("*test"),
+//		ExactMatch: util.StringToPointer("testtesttest"),
+//		RegexMatch: util.StringToPointer(".*"),
+//	}, `testtesttest`)
+//
+//	Expect(count).To(Equal(3))
+//}
