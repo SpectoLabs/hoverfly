994fc7d76b692763e1b09ed21b26467b07405759
mogronalol <andrew.morgan@opencredo.com>
2017-05-24 15:36:00 +0200

    Fixed #353 Added strongest request matcher to find the closest match

---
+++ core/matching/strongest_request_matcher.go
@@ -0,0 +1,73 @@
+package matching
+
+import (
+	"errors"
+
+	"github.com/SpectoLabs/hoverfly/core/models"
+)
+
+
+func StrongestMatchRequestMatcher(req models.RequestDetails, webserver bool, simulation *models.Simulation) (requestMatch, closestMatch *models.RequestMatcherResponsePair, err error) {
+
+	var closestMatchTotalMatches int
+
+	for _, matchingPair := range simulation.MatchingPairs {
+		// TODO: not matching by default on URL and body - need to enable this
+		// TODO: enable matching on scheme
+
+		var totalMatches int
+		matched := true
+
+		requestMatcher := matchingPair.RequestMatcher
+
+		fieldMatch := CountingFieldMatcher(requestMatcher.Body, req.Body)
+		if !fieldMatch.Matched {
+			matched = false
+		}
+		totalMatches += fieldMatch.TotalMatches
+
+		if !webserver {
+			match := CountingFieldMatcher(requestMatcher.Destination, req.Destination)
+			if !match.Matched {
+				matched = false
+			}
+			totalMatches += match.TotalMatches
+		}
+
+		fieldMatch = CountingFieldMatcher(requestMatcher.Path, req.Path)
+		if !fieldMatch.Matched {
+			matched = false
+		}
+		totalMatches += fieldMatch.TotalMatches
+
+		fieldMatch = CountingFieldMatcher(requestMatcher.Query, req.Query)
+		if !fieldMatch.Matched {
+			matched = false
+		}
+		totalMatches += fieldMatch.TotalMatches
+
+		fieldMatch = CountingFieldMatcher(requestMatcher.Method, req.Method)
+		if fieldMatch.Matched {
+			matched = false
+		}
+		totalMatches += fieldMatch.TotalMatches
+
+		if !HeaderMatcher(requestMatcher.Headers, req.Headers) {
+			continue
+		}
+
+		if matched == true {
+			requestMatch = &models.RequestMatcherResponsePair{
+				RequestMatcher: requestMatcher,
+				Response:       matchingPair.Response,
+			}
+			return
+		} else if totalMatches > closestMatchTotalMatches {
+			closestMatchTotalMatches = totalMatches
+			closestMatch = &models.RequestMatcherResponsePair{}
+			*closestMatch = matchingPair
+		}
+	}
+	err = errors.New("No match found")
+	return
+}
