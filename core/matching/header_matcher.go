d4283e3ce5c6dfd39f90d7150b6c15801837508e
mogronalol <andrew.morgan@opencredo.com>
2017-05-25 15:29:30 +0200

    Fixed #353 Counting header matches

--- core/matching/header_matcher.go
+++ core/matching/header_matcher.go
@@ -4,11 +4,14 @@ import (
 	"strings"
 
 	glob "github.com/ryanuber/go-glob"
+	"fmt"
 )
 
-func HeaderMatcher(matchingHeaders, toMatch map[string][]string) * FieldMatch {
+func CountlessHeaderMatcher(matchingHeaders, toMatch map[string][]string) * FieldMatch {
 
 	for matcherHeaderKey, matcherHeaderValues := range matchingHeaders {
+
+		// Make everything lowercase, as headers are case insensitive
 		for requestHeaderKey, requestHeaderValues := range toMatch {
 			delete(toMatch, requestHeaderKey)
 			toMatch[strings.ToLower(requestHeaderKey)] = requestHeaderValues
@@ -20,6 +23,8 @@ func HeaderMatcher(matchingHeaders, toMatch map[string][]string) * FieldMatch {
 		}
 
 		for _, matcherHeaderValue := range matcherHeaderValues {
+			fmt.Println(toMatchHeaderValues)
+
 			matcherHeaderValueMatched := false
 			for _, toMatchHeaderValue := range toMatchHeaderValues {
 				if glob.Glob(strings.ToLower(matcherHeaderValue), strings.ToLower(toMatchHeaderValue)) {
@@ -33,4 +38,42 @@ func HeaderMatcher(matchingHeaders, toMatch map[string][]string) * FieldMatch {
 		}
 	}
 	return countlessFieldMatch(true)
+}
+
+func CountingHeaderMatcher(matchingHeaders, toMatch map[string][]string) * FieldMatch {
+
+	matched := true
+	var matchCount int
+
+	for matcherHeaderKey, matcherHeaderValues := range matchingHeaders {
+
+		// Make everything lowercase, as headers are case insensitive
+		for requestHeaderKey, requestHeaderValues := range toMatch {
+			delete(toMatch, requestHeaderKey)
+			toMatch[strings.ToLower(requestHeaderKey)] = requestHeaderValues
+		}
+
+		toMatchHeaderValues, found := toMatch[strings.ToLower(matcherHeaderKey)]
+		if !found {
+			matched = false
+		}
+
+		for _, matcherHeaderValue := range matcherHeaderValues {
+			matcherHeaderValueMatched := false
+			for _, toMatchHeaderValue := range toMatchHeaderValues {
+				if glob.Glob(strings.ToLower(matcherHeaderValue), strings.ToLower(toMatchHeaderValue)) {
+					matcherHeaderValueMatched = true
+					matchCount++
+				}
+			}
+
+			if !matcherHeaderValueMatched {
+				matched = false
+			}
+		}
+	}
+	return &FieldMatch{
+		Matched: matched,
+		TotalMatches: matchCount,
+	}
 }
