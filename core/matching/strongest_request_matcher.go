eb7bb9c5e930c600d3f5358e9eab101be73b5f00
mogronalol <andrew.morgan@opencredo.com>
2017-05-25 13:39:11 +0200

    Fixed #353 Strongest and closest match logic and testing

--- core/matching/strongest_request_matcher.go
+++ core/matching/strongest_request_matcher.go
@@ -2,14 +2,15 @@ package matching
 
 import (
 	"errors"
-
 	"github.com/SpectoLabs/hoverfly/core/models"
+	"fmt"
 )
 
 
 func StrongestMatchRequestMatcher(req models.RequestDetails, webserver bool, simulation *models.Simulation) (requestMatch, closestMatch *models.RequestMatcherResponsePair, err error) {
 
 	var closestMatchTotalMatches int
+	var strongestMatchTotalMatches int
 
 	for _, matchingPair := range simulation.MatchingPairs {
 		// TODO: not matching by default on URL and body - need to enable this
@@ -22,6 +23,7 @@ func StrongestMatchRequestMatcher(req models.RequestDetails, webserver bool, sim
 
 		fieldMatch := CountingFieldMatcher(requestMatcher.Body, req.Body)
 		if !fieldMatch.Matched {
+			fmt.Println("Did not match on body")
 			matched = false
 		}
 		totalMatches += fieldMatch.TotalMatches
@@ -29,6 +31,7 @@ func StrongestMatchRequestMatcher(req models.RequestDetails, webserver bool, sim
 		if !webserver {
 			match := CountingFieldMatcher(requestMatcher.Destination, req.Destination)
 			if !match.Matched {
+				fmt.Println("Did not match on destination")
 				matched = false
 			}
 			totalMatches += match.TotalMatches
@@ -36,38 +39,47 @@ func StrongestMatchRequestMatcher(req models.RequestDetails, webserver bool, sim
 
 		fieldMatch = CountingFieldMatcher(requestMatcher.Path, req.Path)
 		if !fieldMatch.Matched {
+			fmt.Println("Did not match on path")
 			matched = false
 		}
 		totalMatches += fieldMatch.TotalMatches
 
 		fieldMatch = CountingFieldMatcher(requestMatcher.Query, req.Query)
 		if !fieldMatch.Matched {
+			fmt.Println("Did not match on query")
 			matched = false
 		}
 		totalMatches += fieldMatch.TotalMatches
 
 		fieldMatch = CountingFieldMatcher(requestMatcher.Method, req.Method)
-		if fieldMatch.Matched {
+		if !fieldMatch.Matched {
+			fmt.Println("Did not match on method")
 			matched = false
 		}
 		totalMatches += fieldMatch.TotalMatches
 
 		if !HeaderMatcher(requestMatcher.Headers, req.Headers) {
-			continue
+			fmt.Println("Did not match on headers")
+			matched = false
 		}
 
-		if matched == true {
+		if matched == true && totalMatches >= strongestMatchTotalMatches {
 			requestMatch = &models.RequestMatcherResponsePair{
 				RequestMatcher: requestMatcher,
 				Response:       matchingPair.Response,
 			}
-			return
-		} else if totalMatches > closestMatchTotalMatches {
+			strongestMatchTotalMatches = totalMatches
+		} else if matched == false && requestMatch == nil && totalMatches >= closestMatchTotalMatches {
 			closestMatchTotalMatches = totalMatches
 			closestMatch = &models.RequestMatcherResponsePair{}
 			*closestMatch = matchingPair
+			fmt.Println("Not Matched")
 		}
 	}
-	err = errors.New("No match found")
+
+	if requestMatch == nil {
+		err = errors.New("No match found")
+	}
+
 	return
 }
