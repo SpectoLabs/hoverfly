3e9621f65306b40f02e4f2f18628ce0ce55c6cc5
mogronalol <andrew.morgan@opencredo.com>
2017-05-25 15:29:30 +0200

    Fixed #353 Counting header matches

--- core/matching/header_matcher_test.go
+++ core/matching/header_matcher_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func Test_HeaderMatcher(t *testing.T) {
+func Test_CountlessHeaderMatcher(t *testing.T) {
 	RegisterTestingT(t)
 
 	matcherHeaders := map[string][]string{
@@ -15,10 +15,10 @@ func Test_HeaderMatcher(t *testing.T) {
 		"header2": {"val2"},
 	}
 
-	Expect(matching.HeaderMatcher(matcherHeaders, matcherHeaders).Matched).To(BeTrue())
+	Expect(matching.CountlessHeaderMatcher(matcherHeaders, matcherHeaders).Matched).To(BeTrue())
 }
 
-func Test_HeaderMatcher_IgnoreTestCaseInsensitive(t *testing.T) {
+func Test_CountlessHeaderMatcher_IgnoreTestCaseInsensitive(t *testing.T) {
 	RegisterTestingT(t)
 
 	matcherHeaders := map[string][]string{
@@ -29,10 +29,10 @@ func Test_HeaderMatcher_IgnoreTestCaseInsensitive(t *testing.T) {
 		"HEADER1": {"val1"},
 		"Header2": {"VAL2"},
 	}
-	Expect(matching.HeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
+	Expect(matching.CountlessHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
 }
 
-func Test_HeaderMatcher_MatchingHeadersHasMoreKeysThanRequestMatchesFalse(t *testing.T) {
+func Test_CountlessHeaderMatcher_MatchingHeadersHasMoreKeysThanRequestMatchesFalse(t *testing.T) {
 	RegisterTestingT(t)
 
 	matcherHeaders := map[string][]string{
@@ -42,10 +42,10 @@ func Test_HeaderMatcher_MatchingHeadersHasMoreKeysThanRequestMatchesFalse(t *tes
 	reqHeaders := map[string][]string{
 		"header1": {"val1"},
 	}
-	Expect(matching.HeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeFalse())
+	Expect(matching.CountlessHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeFalse())
 }
 
-func Test_HeaderMatcher_MatchingHeadersHasMoreValuesThanRequestMatchesFalse(t *testing.T) {
+func Test_CountlessHeaderMatcher_MatchingHeadersHasMoreValuesThanRequestMatchesFalse(t *testing.T) {
 	RegisterTestingT(t)
 
 	matcherHeaders := map[string][]string{
@@ -54,7 +54,7 @@ func Test_HeaderMatcher_MatchingHeadersHasMoreValuesThanRequestMatchesFalse(t *t
 	reqHeaders := map[string][]string{
 		"header2": {"val1"},
 	}
-	Expect(matching.HeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeFalse())
+	Expect(matching.CountlessHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeFalse())
 }
 
 func Test_HeaderMatch_MatchingHeadersHasLessKeysThanRequestMatchesTrue(t *testing.T) {
@@ -67,7 +67,7 @@ func Test_HeaderMatch_MatchingHeadersHasLessKeysThanRequestMatchesTrue(t *testin
 		"HEADER1": {"val1"},
 		"header2": {"val2"},
 	}
-	Expect(matching.HeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
+	Expect(matching.CountlessHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
 }
 
 func Test_HeaderMatch_RequestHeadersContainsAllOFMatcherHeaders(t *testing.T) {
@@ -79,5 +79,162 @@ func Test_HeaderMatch_RequestHeadersContainsAllOFMatcherHeaders(t *testing.T) {
 	reqHeaders := map[string][]string{
 		"header2": {"val1", "val2"},
 	}
-	Expect(matching.HeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
+	Expect(matching.CountlessHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
+}
+
+func Test_HeaderMatch_ShouldNotMatchUnlessAllHeadersValuesAreFound(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcherHeaders := map[string][]string{
+		"header2": {"val1", "val2"},
+	}
+	reqHeaders := map[string][]string{
+		"header2": {"val1", "nomatch"},
+	}
+	Expect(matching.CountlessHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeFalse())
+}
+
+func Test_CountingHeaderMatcher(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcherHeaders := map[string][]string{
+		"header1": {"val1"},
+		"header2": {"val2"},
+	}
+
+	Expect(matching.CountingHeaderMatcher(matcherHeaders, matcherHeaders).Matched).To(BeTrue())
+}
+
+func Test_CountingHeaderMatcher_IgnoreTestCaseInsensitive(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcherHeaders := map[string][]string{
+		"header1": {"val1"},
+		"header2": {"val2"},
+	}
+	reqHeaders := map[string][]string{
+		"HEADER1": {"val1"},
+		"Header2": {"VAL2"},
+	}
+	Expect(matching.CountingHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
+}
+
+func Test_CountingHeaderMatcher_MatchingHeadersHasMoreKeysThanRequestMatchesFalse(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcherHeaders := map[string][]string{
+		"header1": {"val1"},
+		"header2": {"val2"},
+	}
+	reqHeaders := map[string][]string{
+		"header1": {"val1"},
+	}
+	Expect(matching.CountingHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeFalse())
+}
+
+func Test_CountingHeaderMatcher_MatchingHeadersHasMoreValuesThanRequestMatchesFalse(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcherHeaders := map[string][]string{
+		"header2": {"val1", "val2"},
+	}
+	reqHeaders := map[string][]string{
+		"header2": {"val1"},
+	}
+	Expect(matching.CountingHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeFalse())
+}
+
+func Test_CountingHeaderMatcher_MatchingHeadersHasLessKeysThanRequestMatchesTrue(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcherHeaders := map[string][]string{
+		"header2": {"val2"},
+	}
+	reqHeaders := map[string][]string{
+		"HEADER1": {"val1"},
+		"header2": {"val2"},
+	}
+	Expect(matching.CountingHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
+}
+
+func Test_CountingHeaderMatcher_RequestHeadersContainsAllOFMatcherHeaders(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcherHeaders := map[string][]string{
+		"header2": {"val2"},
+	}
+	reqHeaders := map[string][]string{
+		"header2": {"val1", "val2"},
+	}
+	Expect(matching.CountingHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeTrue())
+}
+
+func Test_CountingHeaderMatcher_ShouldNotMatchUnlessAllHeadersValuesAreFound(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcherHeaders := map[string][]string{
+		"header2": {"val1", "val2"},
+	}
+	reqHeaders := map[string][]string{
+		"header2": {"val1", "nomatch"},
+	}
+	Expect(matching.CountingHeaderMatcher(matcherHeaders, reqHeaders).Matched).To(BeFalse())
+}
+
+func Test_CountingHeaderMatcher_CountsMatches_WhenThereIsAMatch(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcher := matching.CountingHeaderMatcher(
+		map[string][]string{
+			"header1": {"val1", "val2"},
+		},
+		map[string][]string{
+			"header1": {"val1", "val2"},
+			"extraHeader": {"extraHeader1", "extraHeader2"},
+		})
+
+	Expect(matcher.Matched).To(BeTrue())
+	Expect(matcher.TotalMatches).To(Equal(2))
+
+	matcher = matching.CountingHeaderMatcher(
+		map[string][]string{
+			"header1": {"val1", "val2"},
+			"header2": {"val3"},
+		},
+		map[string][]string{
+			"header1": {"val1", "val2"},
+			"header2": {"val3", "extra"},
+			"extraHeader": {"extraHeader1", "extraHeader2"},
+		})
+
+	Expect(matcher.Matched).To(BeTrue())
+	Expect(matcher.TotalMatches).To(Equal(3))
+}
+
+func Test_CountingHeaderMatcher_CountsMatches_WhenThereIsNoMatch(t *testing.T) {
+	RegisterTestingT(t)
+
+	matcher := matching.CountingHeaderMatcher(
+		map[string][]string{
+			"header1": {"val1", "val2"},
+			"header2": {"val3", "nomatch"},
+		},
+		map[string][]string{
+			"header1": {"val1", "val2"},
+			"header2": {"val3", "extra"},
+			"extraHeader": {"extraHeader1", "extraHeader2"},
+		})
+
+	Expect(matcher.Matched).To(BeFalse())
+	Expect(matcher.TotalMatches).To(Equal(3))
+}
+
+func Test_CountingHeaderMatcher_CountZero_WhenFieldIsNil(t *testing.T) {
+	RegisterTestingT(t)
+
+	// Glob, regex, and exact
+	matcher := matching.CountingFieldMatcher(nil, `testtesttest`)
+
+	Expect(matcher.Matched).To(BeTrue())
+	Expect(matcher.TotalMatches).To(Equal(0))
 }
