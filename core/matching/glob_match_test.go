656702c0d135ae551c95f7597eb2199d55c581cd
Benji Hooper <benji@sixp.in>
2017-05-30 14:08:01 +0100

    Added some more tests to the glob matcher

--- core/matching/glob_match_test.go
+++ core/matching/glob_match_test.go
@@ -13,6 +13,42 @@ func Test_GlobMatch_MatchesTrueWithGlobMatch(t *testing.T) {
 	Expect(matching.GlobMatch("t*st", `test`)).To(BeTrue())
 }
 
+func Test_GlobMatch_MatchesTrueWithGlobMatch_MatchesZeroExtraCharactersAtEnd(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.GlobMatch("test*", `test`)).To(BeTrue())
+}
+
+func Test_GlobMatch_MatchesTrueWithGlobMatch_MatchesZeroExtraCharactersAtStart(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.GlobMatch("*test", `test`)).To(BeTrue())
+}
+
+func Test_GlobMatch_MatchesTrueWithGlobMatch_MatchesZeroExtraCharactersAtStartAndEnd(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.GlobMatch("*test*", `test`)).To(BeTrue())
+}
+
+func Test_GlobMatch_MatchesTrueWithGlobMatch_MatchesUpperCase(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.GlobMatch("*est", `Test`)).To(BeTrue())
+}
+
+func Test_GlobMatch_MatchesTrueWithGlobMatch_MatchesLowerCase(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.GlobMatch("*est", `test`)).To(BeTrue())
+}
+
+func Test_GlobMatch_MatchesFalseWithGlobMatch_UpperCase(t *testing.T) {
+	RegisterTestingT(t)
+
+	Expect(matching.GlobMatch("*esT", `test`)).To(BeFalse())
+}
+
 func Test_GlobMatch_MatchesFalseWithIncorrectGlobMatch(t *testing.T) {
 	RegisterTestingT(t)
 
