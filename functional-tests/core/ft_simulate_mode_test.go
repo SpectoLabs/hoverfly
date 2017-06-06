26315166c7f19b24ad0e1c2fd461ec06c837a625
mogronalol <andrew.morgan@opencredo.com>
2017-06-06 11:47:51 +0200

    Return cache of closest miss

--- functional-tests/core/ft_simulate_mode_test.go
+++ functional-tests/core/ft_simulate_mode_test.go
@@ -105,7 +105,7 @@ var _ = Describe("When I run Hoverfly in simulate mode", func() {
 		Expect(string(body)).To(Equal("first and weakest match"))
 	})
 
-	It("Should respond with the closest miss", func() {
+	It("Should respond with the closest miss, once from matchers & once from cache", func() {
 
 		hoverfly.ImportSimulation(functional_tests.ClosestMissProofSimulation)
 
@@ -116,6 +116,65 @@ var _ = Describe("When I run Hoverfly in simulate mode", func() {
 		Expect(err).To(BeNil())
 
 		expected := `Hoverfly Error!
+
+There was an error when matching
+
+Got error: Could not find a match for request, create or record a valid matcher first!
+
+The following request was made, but was not matched by Hoverfly:
+
+{
+    "Path": "/closest-miss",
+    "Method": "GET",
+    "Destination": "destination.com",
+    "Scheme": "http",
+    "Query": "",
+    "Body": "",
+    "Headers": {
+        "Accept-Encoding": [
+            "gzip"
+        ],
+        "User-Agent": [
+            "Go-http-client/1.1"
+        ]
+    }
+}
+
+The closest miss was the following matcher:
+
+{
+    "path": {
+        "exactMatch": "/closest-miss"
+    },
+    "destination": {
+        "exactMatch": "destination.com"
+    },
+    "body": {
+        "exactMatch": "body"
+    }
+}
+
+But it did not match on the following fields:
+
+[body]
+
+Which if hit would have given the following response:
+
+{
+    "status": 200,
+    "body": "",
+    "encodedBody": false,
+    "headers": null
+}`
+		Expect(string(body)).To(Equal(expected))
+
+		slingRequest = sling.New().Get("http://destination.com/closest-miss")
+		response = hoverfly.Proxy(slingRequest)
+
+		body, err = ioutil.ReadAll(response.Body)
+		Expect(err).To(BeNil())
+
+		expected = `Hoverfly Error!
 
 There was an error when matching
 
