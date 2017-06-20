a9a565f8c65f377dc0375df5861f1e7f861f8d87
Benji Hooper <benji@sixp.in>
2017-06-20 11:17:46 +0100

    Added -logs-size to specify the amount of logs you want stored

--- functional-tests/core/ft_api_v2_logs_test.go
+++ functional-tests/core/ft_api_v2_logs_test.go
@@ -1,11 +1,13 @@
 package hoverfly_test
 
 import (
+	"encoding/json"
 	"io/ioutil"
 
 	"strconv"
 	"time"
 
+	"github.com/SpectoLabs/hoverfly/core/handlers/v2"
 	"github.com/SpectoLabs/hoverfly/functional-tests"
 	"github.com/antonholmquist/jason"
 	"github.com/dghubble/sling"
@@ -31,146 +33,191 @@ var _ = Describe("/api/v2/logs", func() {
 
 	BeforeEach(func() {
 		hoverfly = functional_tests.NewHoverfly()
-		hoverfly.Start()
-		hoverfly.SetMode("simulate")
-		hoverfly.ImportSimulation(functional_tests.JsonPayload)
-		hoverfly.Proxy(sling.New().Get("http://destination-server.com"))
 	})
 
 	AfterEach(func() {
 		hoverfly.Stop()
 	})
 
-	Context("GET", func() {
+	Context("with log-size=100", func() {
 
-		It("should get logs", func() {
-			req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs")
-			res := functional_tests.DoRequest(req)
-			Expect(res.StatusCode).To(Equal(200))
-			responseJson, err := ioutil.ReadAll(res.Body)
-			Expect(err).To(BeNil())
-
-			jsonObject, err := jason.NewObjectFromBytes(responseJson)
-			Expect(err).To(BeNil())
-
-			logsArray, err := jsonObject.GetObjectArray("logs")
-			Expect(err).To(BeNil())
-
-			Expect(len(logsArray)).To(BeNumerically(">=", 4))
-
-			logs := MakeLogsArray(logsArray)
-
-			Expect(logs).Should(ContainElement("Proxy prepared..."))
-			Expect(logs).Should(ContainElement("current proxy configuration"))
-			Expect(logs).Should(ContainElement("serving proxy"))
-			Expect(logs).Should(ContainElement("Admin interface is starting..."))
+		BeforeEach(func() {
+			hoverfly.Start("-logs-size=100")
 		})
 
-		It("should limit the logs it returns", func() {
-			req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs?limit=1")
-			res := functional_tests.DoRequest(req)
-			Expect(res.StatusCode).To(Equal(200))
-			responseJson, err := ioutil.ReadAll(res.Body)
-			Expect(err).To(BeNil())
-
-			jsonObject, err := jason.NewObjectFromBytes(responseJson)
-			Expect(err).To(BeNil())
-
-			logsArray, err := jsonObject.GetObjectArray("logs")
-			Expect(err).To(BeNil())
-
-			Expect(len(logsArray)).To(Equal(1))
-
-			Expect(logsArray[0].GetString("msg")).Should(Equal("payloads imported"))
+		AfterEach(func() {
+			hoverfly.Stop()
 		})
 
-		It("should query the logs by from time", func() {
-			time.Sleep(time.Second)
-			now := time.Now()
+		Context("GET", func() {
 
-			hoverfly.Proxy(sling.New().Get("https://hoverfly.io"))
+			It("should not exceed size", func() {
+				for i := 0; i < 111; i++ {
+					hoverfly.Proxy(sling.New().Get("http://hoverfly.io"))
+				}
 
-			req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs?from=" + strconv.FormatInt(now.Unix(), 10))
-			res := functional_tests.DoRequest(req)
-			Expect(res.StatusCode).To(Equal(200))
-			responseJson, err := ioutil.ReadAll(res.Body)
-			Expect(err).To(BeNil())
+				req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs")
+				res := functional_tests.DoRequest(req)
 
-			jsonObject, err := jason.NewObjectFromBytes(responseJson)
-			Expect(err).To(BeNil())
+				Expect(res.StatusCode).To(Equal(200))
 
-			logsArray, err := jsonObject.GetObjectArray("logs")
-			Expect(err).To(BeNil())
+				responseJson, err := ioutil.ReadAll(res.Body)
+				Expect(err).To(BeNil())
 
-			Expect(len(logsArray)).To(Equal(2))
+				var logs v2.LogsView
 
-			for _, log := range logsArray {
-				timeStr, _ := log.GetString("time")
-				logTime, _ := time.Parse(time.RFC3339, timeStr)
-				Expect(logTime.Unix()).Should(BeNumerically(">=", now.Unix()))
-			}
+				err = json.Unmarshal(responseJson, &logs)
+				Expect(err).To(BeNil())
+
+				Expect(logs.Logs).To(HaveLen(100))
+			})
 		})
 	})
 
-	Context("GET Accept text/plain", func() {
+	Context("with standard configuration", func() {
 
-		It("should get logs", func() {
-			req := sling.New().Get("http://localhost:"+hoverfly.GetAdminPort()+"/api/v2/logs").Add("Accept", "text/plain")
-			res := functional_tests.DoRequest(req)
-			Expect(res.StatusCode).To(Equal(200))
-			responseBody, err := ioutil.ReadAll(res.Body)
-			Expect(err).To(BeNil())
-
-			Expect(responseBody).To(ContainSubstring("Proxy prepared..."))
-			Expect(responseBody).To(ContainSubstring("=."))
-			Expect(responseBody).To(ContainSubstring("=simulate"))
-			Expect(responseBody).To(ContainSubstring("=" + hoverfly.GetProxyPort()))
+		BeforeEach(func() {
+			hoverfly.Start()
+			hoverfly.SetMode("simulate")
+			hoverfly.ImportSimulation(functional_tests.JsonPayload)
+			hoverfly.Proxy(sling.New().Get("http://destination-server.com"))
 		})
 
-		It("should limit the logs it returns", func() {
-			req := sling.New().Get("http://localhost:"+hoverfly.GetAdminPort()+"/api/v2/logs?limit=1").Add("Accept", "text/plain")
-			res := functional_tests.DoRequest(req)
-			Expect(res.StatusCode).To(Equal(200))
-			responseBody, err := ioutil.ReadAll(res.Body)
-			Expect(err).To(BeNil())
-
-			Expect(responseBody).To(ContainSubstring("payloads imported"))
-
-			Expect(responseBody).ToNot(ContainSubstring("Using memory backend"))
-			Expect(responseBody).ToNot(ContainSubstring("Proxy prepared"))
-			Expect(responseBody).ToNot(ContainSubstring("current proxy configuration"))
-			Expect(responseBody).ToNot(ContainSubstring("Admin interface is starting..."))
-		})
-	})
-
-	Context("GET Content-Type text/plain", func() {
-
-		It("should get logs", func() {
-			req := sling.New().Get("http://localhost:"+hoverfly.GetAdminPort()+"/api/v2/logs").Add("Content-Type", "text/plain")
-			res := functional_tests.DoRequest(req)
-			Expect(res.StatusCode).To(Equal(200))
-			responseBody, err := ioutil.ReadAll(res.Body)
-			Expect(err).To(BeNil())
-
-			Expect(responseBody).To(ContainSubstring("Proxy prepared..."))
-			Expect(responseBody).To(ContainSubstring("=."))
-			Expect(responseBody).To(ContainSubstring("=simulate"))
-			Expect(responseBody).To(ContainSubstring("=" + hoverfly.GetProxyPort()))
+		AfterEach(func() {
+			hoverfly.Stop()
 		})
 
-		It("should limit the logs it returns", func() {
-			req := sling.New().Get("http://localhost:"+hoverfly.GetAdminPort()+"/api/v2/logs?limit=1").Add("Content-Type", "text/plain")
-			res := functional_tests.DoRequest(req)
-			Expect(res.StatusCode).To(Equal(200))
-			responseBody, err := ioutil.ReadAll(res.Body)
-			Expect(err).To(BeNil())
+		Context("GET", func() {
 
-			Expect(responseBody).To(ContainSubstring("payloads imported"))
+			It("should get logs", func() {
+				req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs")
+				res := functional_tests.DoRequest(req)
+				Expect(res.StatusCode).To(Equal(200))
+				responseJson, err := ioutil.ReadAll(res.Body)
+				Expect(err).To(BeNil())
 
-			Expect(responseBody).ToNot(ContainSubstring("Using memory backend"))
-			Expect(responseBody).ToNot(ContainSubstring("Proxy prepared"))
-			Expect(responseBody).ToNot(ContainSubstring("current proxy configuration"))
-			Expect(responseBody).ToNot(ContainSubstring("Admin interface is starting..."))
+				jsonObject, err := jason.NewObjectFromBytes(responseJson)
+				Expect(err).To(BeNil())
+
+				logsArray, err := jsonObject.GetObjectArray("logs")
+				Expect(err).To(BeNil())
+
+				Expect(len(logsArray)).To(BeNumerically(">=", 4))
+
+				logs := MakeLogsArray(logsArray)
+
+				Expect(logs).Should(ContainElement("Proxy prepared..."))
+				Expect(logs).Should(ContainElement("current proxy configuration"))
+				Expect(logs).Should(ContainElement("serving proxy"))
+				Expect(logs).Should(ContainElement("Admin interface is starting..."))
+			})
+
+			It("should limit the logs it returns", func() {
+				req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs?limit=1")
+				res := functional_tests.DoRequest(req)
+				Expect(res.StatusCode).To(Equal(200))
+				responseJson, err := ioutil.ReadAll(res.Body)
+				Expect(err).To(BeNil())
+
+				jsonObject, err := jason.NewObjectFromBytes(responseJson)
+				Expect(err).To(BeNil())
+
+				logsArray, err := jsonObject.GetObjectArray("logs")
+				Expect(err).To(BeNil())
+
+				Expect(len(logsArray)).To(Equal(1))
+
+				Expect(logsArray[0].GetString("msg")).Should(Equal("payloads imported"))
+			})
+
+			It("should query the logs by from time", func() {
+				time.Sleep(time.Second)
+				now := time.Now()
+
+				hoverfly.Proxy(sling.New().Get("https://hoverfly.io"))
+
+				req := sling.New().Get("http://localhost:" + hoverfly.GetAdminPort() + "/api/v2/logs?from=" + strconv.FormatInt(now.Unix(), 10))
+				res := functional_tests.DoRequest(req)
+				Expect(res.StatusCode).To(Equal(200))
+				responseJson, err := ioutil.ReadAll(res.Body)
+				Expect(err).To(BeNil())
+
+				jsonObject, err := jason.NewObjectFromBytes(responseJson)
+				Expect(err).To(BeNil())
+
+				logsArray, err := jsonObject.GetObjectArray("logs")
+				Expect(err).To(BeNil())
+
+				Expect(len(logsArray)).To(Equal(2))
+
+				for _, log := range logsArray {
+					timeStr, _ := log.GetString("time")
+					logTime, _ := time.Parse(time.RFC3339, timeStr)
+					Expect(logTime.Unix()).Should(BeNumerically(">=", now.Unix()))
+				}
+			})
+		})
+
+		Context("GET Accept text/plain", func() {
+
+			It("should get logs", func() {
+				req := sling.New().Get("http://localhost:"+hoverfly.GetAdminPort()+"/api/v2/logs").Add("Accept", "text/plain")
+				res := functional_tests.DoRequest(req)
+				Expect(res.StatusCode).To(Equal(200))
+				responseBody, err := ioutil.ReadAll(res.Body)
+				Expect(err).To(BeNil())
+
+				Expect(responseBody).To(ContainSubstring("Proxy prepared..."))
+				Expect(responseBody).To(ContainSubstring("=."))
+				Expect(responseBody).To(ContainSubstring("=simulate"))
+				Expect(responseBody).To(ContainSubstring("=" + hoverfly.GetProxyPort()))
+			})
+
+			It("should limit the logs it returns", func() {
+				req := sling.New().Get("http://localhost:"+hoverfly.GetAdminPort()+"/api/v2/logs?limit=1").Add("Accept", "text/plain")
+				res := functional_tests.DoRequest(req)
+				Expect(res.StatusCode).To(Equal(200))
+				responseBody, err := ioutil.ReadAll(res.Body)
+				Expect(err).To(BeNil())
+
+				Expect(responseBody).To(ContainSubstring("payloads imported"))
+
+				Expect(responseBody).ToNot(ContainSubstring("Using memory backend"))
+				Expect(responseBody).ToNot(ContainSubstring("Proxy prepared"))
+				Expect(responseBody).ToNot(ContainSubstring("current proxy configuration"))
+				Expect(responseBody).ToNot(ContainSubstring("Admin interface is starting..."))
+			})
+		})
+
+		Context("GET Content-Type text/plain", func() {
+
+			It("should get logs", func() {
+				req := sling.New().Get("http://localhost:"+hoverfly.GetAdminPort()+"/api/v2/logs").Add("Content-Type", "text/plain")
+				res := functional_tests.DoRequest(req)
+				Expect(res.StatusCode).To(Equal(200))
+				responseBody, err := ioutil.ReadAll(res.Body)
+				Expect(err).To(BeNil())
+
+				Expect(responseBody).To(ContainSubstring("Proxy prepared..."))
+				Expect(responseBody).To(ContainSubstring("=."))
+				Expect(responseBody).To(ContainSubstring("=simulate"))
+				Expect(responseBody).To(ContainSubstring("=" + hoverfly.GetProxyPort()))
+			})
+
+			It("should limit the logs it returns", func() {
+				req := sling.New().Get("http://localhost:"+hoverfly.GetAdminPort()+"/api/v2/logs?limit=1").Add("Content-Type", "text/plain")
+				res := functional_tests.DoRequest(req)
+				Expect(res.StatusCode).To(Equal(200))
+				responseBody, err := ioutil.ReadAll(res.Body)
+				Expect(err).To(BeNil())
+
+				Expect(responseBody).To(ContainSubstring("payloads imported"))
+
+				Expect(responseBody).ToNot(ContainSubstring("Using memory backend"))
+				Expect(responseBody).ToNot(ContainSubstring("Proxy prepared"))
+				Expect(responseBody).ToNot(ContainSubstring("current proxy configuration"))
+				Expect(responseBody).ToNot(ContainSubstring("Admin interface is starting..."))
+			})
 		})
 	})
 })
