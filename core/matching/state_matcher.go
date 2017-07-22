10e6e4acf30c45138ccf4874b428888977606020
mogronalol <andrew.morgan@opencredo.com>
2017-07-22 15:25:55 +0200

    State matcher which counts a score

--- core/matching/state_matcher.go
+++ core/matching/state_matcher.go
@@ -26,6 +26,28 @@ func UnscoredStateMatcher(currentState map[string]string, requiredState map[stri
 	return FieldMatchWithNoScore(true)
 }
 
-//func ScoredFieldMatcher(currentState map[string]string, requiredState map[string]string) *FieldMatch {
-//
-//}
+func ScoredStateMatcher(currentState map[string]string, requiredState map[string]string) *FieldMatch {
+
+	score := 0
+	matched := true
+
+	if requiredState == nil || len(requiredState) == 0 {
+		return FieldMatchWithNoScore(true)
+	}
+
+	for key, value := range requiredState {
+		if _, ok := currentState[key]; !ok {
+			matched = false
+		}
+		if currentState[key] != value {
+			matched = false
+		} else {
+			score++
+		}
+	}
+
+	return &FieldMatch{
+		Matched: matched,
+		MatchScore: score,
+	}
+}
--- core/matching/state_matcher_test.go
+++ core/matching/state_matcher_test.go
@@ -73,3 +73,90 @@ func Test_UnscoredShouldMatchIsKeysAndValuesAreTheSame(t *testing.T) {
 	Expect(match.Matched).To(BeTrue())
 	Expect(match.MatchScore).To(Equal(0))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func Test_ScoredShouldMatchIfBothCurrentAndRequiredStateAreNil(t *testing.T) {
+	RegisterTestingT(t)
+
+	match := ScoredStateMatcher(nil, nil)
+
+	Expect(match.Matched).To(BeTrue())
+	Expect(match.MatchScore).To(Equal(0))
+}
+
+func Test_ScoredShouldMatchIfCurrentStateIsNilAndRequiredStateIsEmpty(t *testing.T) {
+	RegisterTestingT(t)
+
+	match := ScoredStateMatcher(nil, make(map[string]string))
+
+	Expect(match.Matched).To(BeTrue())
+	Expect(match.MatchScore).To(Equal(0))
+}
+
+func Test_ScoredShouldMatchIfCurrentStateIEmptyAndRequiredStateIsNil(t *testing.T) {
+	RegisterTestingT(t)
+
+	match := ScoredStateMatcher(make(map[string]string), nil)
+
+	Expect(match.Matched).To(BeTrue())
+	Expect(match.MatchScore).To(Equal(0))
+}
+
+func Test_ScoredShouldNotMatchIfRequiredStateLengthIsGreaterThanActualStateLength(t *testing.T) {
+	RegisterTestingT(t)
+
+	match := ScoredStateMatcher(make(map[string]string), map[string]string{"foo": "bar"})
+
+	Expect(match.Matched).To(BeFalse())
+	Expect(match.MatchScore).To(Equal(0))
+}
+
+func Test_ScoredShouldNotMatchIfLengthsAreTheSameButKeysAreDifferent(t *testing.T) {
+	RegisterTestingT(t)
+
+	match := ScoredStateMatcher(
+		map[string]string{"foo": "bar", "cheese": "ham"},
+		map[string]string{"adasd": "bar", "sadsad": "ham"})
+
+	Expect(match.Matched).To(BeFalse())
+	Expect(match.MatchScore).To(Equal(0))
+}
+
+func Test_ScoredShouldNotMatchIfKeysAreTheSameButValuesAreDifferent(t *testing.T) {
+	RegisterTestingT(t)
+
+	match := ScoredStateMatcher(
+		map[string]string{"foo": "bar", "cheese": "ham"},
+		map[string]string{"foo": "adsad", "cheese": "ham"})
+
+	Expect(match.Matched).To(BeFalse())
+	Expect(match.MatchScore).To(Equal(1))
+}
+
+func Test_ScoredShouldMatchIsKeysAndValuesAreTheSame(t *testing.T) {
+	RegisterTestingT(t)
+
+	match := ScoredStateMatcher(
+		map[string]string{"foo": "bar", "cheese": "ham"},
+		map[string]string{"foo": "bar", "cheese": "ham"})
+
+	Expect(match.Matched).To(BeTrue())
+	Expect(match.MatchScore).To(Equal(2))
+}
