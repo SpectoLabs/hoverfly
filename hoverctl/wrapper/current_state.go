869152791b1fd6840dca7320e4dff94dac3912c8
mogronalol <andrew.morgan@opencredo.com>
2017-08-02 22:22:14 +0200

    Basic skeleton for managing state via the CLI

---
+++ hoverctl/wrapper/current_state.go
@@ -0,0 +1,55 @@
+package wrapper
+
+import (
+	"github.com/SpectoLabs/hoverfly/hoverctl/configuration"
+	ioutil "io/ioutil"
+	"encoding/json"
+)
+
+func GetCurrentState(target configuration.Target) (map[string]string, error) {
+
+	res, err := doRequest(target, "GET", v2ApiCurrentState, "", nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	bytes, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var currentState = make(map[string]string)
+
+	err = json.Unmarshal(bytes, &currentState)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return currentState, nil
+}
+
+func PatchCurrentState(target configuration.Target, key, value string) (error) {
+
+	toPatch := make(map[string]string)
+	marshal, err := json.Marshal(toPatch)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = doRequest(target, "PATCH", v2ApiCurrentState, string(marshal), nil)
+
+	return err
+}
+
+func DeleteCurrentState(target configuration.Target) (error) {
+
+	_, err := doRequest(target, "DELETE", v2ApiCurrentState, "", nil)
+
+	return err
+}
