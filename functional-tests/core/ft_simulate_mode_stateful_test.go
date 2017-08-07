5a58120b6c1c2eff8c8a5531a7795fff79cb7c21
Benji Hooper <benji@sixp.in>
2017-08-07 15:20:41 +0100

    Removing debug test logging

--- functional-tests/core/ft_simulate_mode_stateful_test.go
+++ functional-tests/core/ft_simulate_mode_stateful_test.go
@@ -3,8 +3,6 @@ package hoverfly_test
 import (
 	"io/ioutil"
 
-	"fmt"
-
 	"github.com/SpectoLabs/hoverfly/functional-tests"
 	"github.com/dghubble/sling"
 	. "github.com/onsi/ginkgo"
@@ -36,91 +34,53 @@ var _ = Describe("When I run Hoverfly in simulate mode", func() {
 
 		assertState(stateURL, `{"state":{}}`)
 
-		GinkgoWriter.Write([]byte(`1`))
-
 		resp := hoverfly.Proxy(sling.New().Get("http://test-server.com/basket"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`empty`)))
-
-		GinkgoWriter.Write([]byte(`2`))
 
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/add-eggs"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`added eggs`)))
 
-		GinkgoWriter.Write([]byte(`3`))
-
 		assertState(stateURL, `{"state": {"eggs":"present"}}`)
-
-		GinkgoWriter.Write([]byte(`4`))
 
 		foo := hoverfly.Proxy(sling.New().Get("http://test-server.com/basket"))
 
 		bytes, _ := ioutil.ReadAll(foo.Body)
 
-		fmt.Println(string(bytes))
-
-		GinkgoWriter.Write(bytes)
-
 		Expect(bytes).To(Equal([]byte(`eggs`)))
-
-		GinkgoWriter.Write([]byte(`5`))
 
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/add-bacon"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`added bacon`)))
 
-		GinkgoWriter.Write([]byte(`6`))
-
 		assertState(stateURL, `{"state":{"eggs":"present","bacon":"present"}}`)
-
-		GinkgoWriter.Write([]byte(`7`))
 
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/basket"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`eggs, bacon`)))
 
-		GinkgoWriter.Write([]byte(`8`))
-
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/remove-eggs"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`removed eggs`)))
 
-		GinkgoWriter.Write([]byte(`9`))
-
 		assertState(stateURL, `{"state": {"bacon":"present"}}`)
-
-		GinkgoWriter.Write([]byte(`10`))
 
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/basket"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`bacon`)))
 
-		GinkgoWriter.Write([]byte(`11`))
-
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/remove-bacon"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`removed bacon`)))
 
-		GinkgoWriter.Write([]byte(`12`))
-
 		assertState(stateURL, `{"state":{}}`)
-
-		GinkgoWriter.Write([]byte(`13`))
 
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/basket"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`empty`)))
-
-		GinkgoWriter.Write([]byte(`14`))
 
 		// Repeat it all to make sure caching has not broken anything
 
 		assertState(stateURL, `{"state":{}}`)
 
-		GinkgoWriter.Write([]byte(`15`))
-
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/basket"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`empty`)))
 
-		GinkgoWriter.Write([]byte(`16`))
-
 		resp = hoverfly.Proxy(sling.New().Get("http://test-server.com/add-eggs"))
 		Expect(ioutil.ReadAll(resp.Body)).To(Equal([]byte(`added eggs`)))
-
-		GinkgoWriter.Write([]byte(`17`))
 
 		assertState(stateURL, `{"state":{"eggs":"present"}}`)
 
