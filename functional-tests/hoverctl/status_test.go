be0b6098c694e9803c43dc27b28ee11aee8bc3ce
Benji Hooper <benji@sixp.in>
2017-09-20 17:09:32 +0100

    Adde status command to hoverctl

---
+++ functional-tests/hoverctl/status_test.go
@@ -0,0 +1,82 @@
+package hoverctl_suite
+
+import (
+	"github.com/SpectoLabs/hoverfly/functional-tests"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("when I use hoverctl status", func() {
+
+	var (
+		hoverfly *functional_tests.Hoverfly
+	)
+
+	Describe("with a running hoverfly", func() {
+
+		BeforeEach(func() {
+			hoverfly = functional_tests.NewHoverfly()
+			hoverfly.Start()
+
+			functional_tests.Run(hoverctlBinary, "targets", "update", "local", "--admin-port", hoverfly.GetAdminPort())
+		})
+
+		AfterEach(func() {
+			hoverfly.Stop()
+		})
+
+		Describe("should get the status of Hoverfly", func() {
+
+			It("Print it", func() {
+				output := functional_tests.Run(hoverctlBinary, "status")
+				Expect(output).To(ContainSubstring("Hoverfly   | running"))
+				Expect(output).To(ContainSubstring("Admin port |    " + hoverfly.GetAdminPort()))
+				Expect(output).To(ContainSubstring("Proxy port |     8500"))
+				Expect(output).To(ContainSubstring("Mode       | simulate"))
+				Expect(output).To(ContainSubstring("Middleware | disabled"))
+			})
+
+			It("should get the mode from Hoverfly", func() {
+				hoverfly.SetMode("capture")
+
+				output := functional_tests.Run(hoverctlBinary, "status")
+				Expect(output).To(ContainSubstring("Mode       | capture"))
+
+				hoverfly.SetMode("synthesize")
+
+				output = functional_tests.Run(hoverctlBinary, "status")
+				Expect(output).To(ContainSubstring("Mode       | synthesize"))
+
+				hoverfly.SetMode("modify")
+
+				output = functional_tests.Run(hoverctlBinary, "status")
+				Expect(output).To(ContainSubstring("Mode       | modify"))
+			})
+
+			It("should get the middleware from Hoverfly", func() {
+				output := functional_tests.Run(hoverctlBinary, "status")
+				Expect(output).To(ContainSubstring("Middleware | disabled"))
+
+				hoverfly.SetMiddleware("python", functional_tests.Middleware)
+
+				output = functional_tests.Run(hoverctlBinary, "status")
+				Expect(output).To(ContainSubstring("Middleware | enabled"))
+
+				Expect(output).To(ContainSubstring("Hoverfly is using local middleware with the command python and the script:"))
+				Expect(output).To(ContainSubstring(functional_tests.Middleware))
+			})
+
+		})
+	})
+
+	Describe("without a running hoverfly", func() {
+
+		Describe("should not get the status of Hoverfly", func() {
+
+			It("should print an error", func() {
+				output := functional_tests.Run(hoverctlBinary, "status")
+				Expect(output).To(ContainSubstring("Could not connect to Hoverfly at localhost:8888"))
+			})
+		})
+	})
+})
---
+++ hoverctl/cmd/status.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/SpectoLabs/hoverfly/hoverctl/wrapper"
+	"github.com/spf13/cobra"
+)
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Get the current status of Hoverfly",
+	Long: `
+If Hoverfly is running, this command will show an overview
+of the instance of Hoverfly. This includes reporting the
+mode and middleware set.
+`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		checkTargetAndExit(target)
+
+		mode, err := wrapper.GetMode(*target)
+		handleIfError(err)
+
+		middleware, err := wrapper.GetMiddleware(*target)
+		handleIfError(err)
+
+		var middlewareStatus string
+
+		if middleware.Binary == "" && middleware.Script == "" && middleware.Remote == "" {
+			middlewareStatus = "disabled"
+		} else {
+			middlewareStatus = "enabled"
+		}
+
+		data := [][]string{
+			{"Hoverfly", "running"},
+			{"Admin port", strconv.Itoa(target.AdminPort)},
+			{"Proxy port", strconv.Itoa(target.ProxyPort)},
+			{"Mode", mode.Mode},
+			{"Middleware", middlewareStatus},
+		}
+
+		drawTable(data, false)
+		if middlewareStatus == "enabled" {
+			fmt.Println("")
+			if middleware.Remote != "" {
+				fmt.Println("Hoverfly is using remote middleware:\n" + middleware.Remote)
+			} else {
+				fmt.Println("Hoverfly is using local middleware with the command " + middleware.Binary + " and the script:\n" + middleware.Script)
+			}
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(statusCmd)
+}
