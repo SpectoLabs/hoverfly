8351446ed42eeae58c9be8a072b24f0cf5603543
Benji Hooper <benji@sixp.in>
2018-03-16 17:25:43 +0000

    Using k8s.io/client-go/util/jsonpath instead of github.com/NodePrime/jsonpath

--- core/matching/json_path_match.go
+++ core/matching/json_path_match.go
@@ -1,19 +1,47 @@
 package matching
 
-import "github.com/NodePrime/jsonpath"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"k8s.io/client-go/util/jsonpath"
+)
 
 func JsonPathMatch(matchingString string, toMatch string) bool {
-	paths, err := jsonpath.ParsePaths(matchingString)
+	matchingString = prepareJsonPathQuery(matchingString)
+
+	jsonPath := jsonpath.New("")
+
+	err := jsonPath.Parse(matchingString)
 	if err != nil {
 		return false
 	}
 
-	eval, err := jsonpath.EvalPathsInBytes([]byte(toMatch), paths)
+	var data map[string]interface{}
+	if err := json.Unmarshal([]byte(toMatch), &data); err != nil {
+		return false
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = jsonPath.Execute(buf, data)
 	if err != nil {
 		return false
 	}
 
-	_, ok := eval.Next()
+	returnedString := buf.String()
+	if returnedString == matchingString {
+		return false
+	}
 
-	return ok
+	return true
+}
+
+func prepareJsonPathQuery(query string) string {
+	if string(query[0:1]) != "{" && string(query[len(query)-1:]) != "}" {
+		query = fmt.Sprintf("{%s}", query)
+	}
+
+	return query
 }
