e2265ba09f3a4359bbb77715a68a174be512fbee
Benji Hooper <benji@sixp.in>
2018-05-03 16:49:17 +0100

    Refactored the field matcher to make it easier and less error prone to add new matchers

---
+++ core/matching/matchers/matchers.go
@@ -0,0 +1,13 @@
+package matchers
+
+type MatcherFunc func(data interface{}, toMatch string) bool
+
+var Matchers = map[string]MatcherFunc{
+	Exact:    ExactMatch,
+	Glob:     GlobMatch,
+	Json:     JsonMatch,
+	JsonPath: JsonPathMatch,
+	Regex:    RegexMatch,
+	Xml:      XmlMatch,
+	Xpath:    XpathMatch,
+}
