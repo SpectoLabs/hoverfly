418764f3b1e42ad7b1138340bff584f90140adfa
Benji Hooper <benji@sixp.in>
2018-05-03 10:35:25 +0100

    Use all defined matchers on a field in the FieldMatcher()

--- core/matching/field_matcher.go
+++ core/matching/field_matcher.go
@@ -13,50 +13,50 @@ func FieldMatcher(fields []models.RequestFieldMatchers, toMatch string) *FieldMa
 		return fieldMatch
 	}
 
-	field := fields[0]
-
-	switch field.Matcher {
-	case matchers.Exact:
-		if matchers.ExactMatch(field.Value, toMatch) {
-			fieldMatch.MatchScore = fieldMatch.MatchScore + 2
-		} else {
-			fieldMatch.Matched = false
-		}
-	case matchers.Xml:
-		if matchers.XmlMatch(field.Value, toMatch) {
-			fieldMatch.MatchScore = fieldMatch.MatchScore + 1
-		} else {
-			fieldMatch.Matched = false
-		}
-	case matchers.Xpath:
-		if matchers.XpathMatch(field.Value, toMatch) {
-			fieldMatch.MatchScore = fieldMatch.MatchScore + 1
-		} else {
-			fieldMatch.Matched = false
-		}
-	case matchers.Json:
-		if matchers.JsonMatch(field.Value, toMatch) {
-			fieldMatch.MatchScore = fieldMatch.MatchScore + 1
-		} else {
-			fieldMatch.Matched = false
-		}
-	case matchers.JsonPath:
-		if matchers.JsonPathMatch(field.Value, toMatch) {
-			fieldMatch.MatchScore = fieldMatch.MatchScore + 1
-		} else {
-			fieldMatch.Matched = false
-		}
-	case matchers.Regex:
-		if matchers.RegexMatch(field.Value, toMatch) {
-			fieldMatch.MatchScore = fieldMatch.MatchScore + 1
-		} else {
-			fieldMatch.Matched = false
-		}
-	case matchers.Glob:
-		if matchers.GlobMatch(field.Value, toMatch) {
-			fieldMatch.MatchScore = fieldMatch.MatchScore + 1
-		} else {
-			fieldMatch.Matched = false
+	for _, field := range fields {
+		switch field.Matcher {
+		case matchers.Exact:
+			if matchers.ExactMatch(field.Value, toMatch) {
+				fieldMatch.MatchScore = fieldMatch.MatchScore + 2
+			} else {
+				fieldMatch.Matched = false
+			}
+		case matchers.Xml:
+			if matchers.XmlMatch(field.Value, toMatch) {
+				fieldMatch.MatchScore = fieldMatch.MatchScore + 1
+			} else {
+				fieldMatch.Matched = false
+			}
+		case matchers.Xpath:
+			if matchers.XpathMatch(field.Value, toMatch) {
+				fieldMatch.MatchScore = fieldMatch.MatchScore + 1
+			} else {
+				fieldMatch.Matched = false
+			}
+		case matchers.Json:
+			if matchers.JsonMatch(field.Value, toMatch) {
+				fieldMatch.MatchScore = fieldMatch.MatchScore + 1
+			} else {
+				fieldMatch.Matched = false
+			}
+		case matchers.JsonPath:
+			if matchers.JsonPathMatch(field.Value, toMatch) {
+				fieldMatch.MatchScore = fieldMatch.MatchScore + 1
+			} else {
+				fieldMatch.Matched = false
+			}
+		case matchers.Regex:
+			if matchers.RegexMatch(field.Value, toMatch) {
+				fieldMatch.MatchScore = fieldMatch.MatchScore + 1
+			} else {
+				fieldMatch.Matched = false
+			}
+		case matchers.Glob:
+			if matchers.GlobMatch(field.Value, toMatch) {
+				fieldMatch.MatchScore = fieldMatch.MatchScore + 1
+			} else {
+				fieldMatch.Matched = false
+			}
 		}
 	}
 
