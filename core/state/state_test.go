adf4755e60f767407cbc2fafad9743248e17e1d2
Benji Hooper <benji@sixp.in>
2018-05-18 17:52:24 +0100

    Set state when importing simulation

---
+++ core/state/state_test.go
@@ -0,0 +1,31 @@
+package state_test
+
+import (
+	"testing"
+
+	"github.com/SpectoLabs/hoverfly/core/state"
+	. "github.com/onsi/gomega"
+)
+
+func Test_NewStateFromState_ReturnsEmptyState(t *testing.T) {
+	RegisterTestingT(t)
+
+	b := state.NewStateFromState(map[string]string{})
+	Expect(b).ToNot(BeNil())
+	Expect(b.State).To(Equal(map[string]string{}))
+}
+
+func Test_NewStateFromState_InitializesStateForSequenceKeys(t *testing.T) {
+	RegisterTestingT(t)
+
+	b := state.NewStateFromState(map[string]string{
+		"test":       "test",
+		"sequence":   "true",
+		"sequence:1": "true",
+	})
+	Expect(b).ToNot(BeNil())
+	Expect(b.State).To(Equal(map[string]string{
+		"sequence":   "1",
+		"sequence:1": "1",
+	}))
+}
