878228587b5f785600819f95f553a639a728a8e9
Benji Hooper <benji@sixp.in>
2018-05-18 19:08:23 +0100

    Moved GetNewSequenceKey onto State object

--- core/state/state_test.go
+++ core/state/state_test.go
@@ -18,14 +18,39 @@ func Test_NewStateFromState_ReturnsEmptyState(t *testing.T) {
 func Test_NewStateFromState_InitializesStateForSequenceKeys(t *testing.T) {
 	RegisterTestingT(t)
 
-	b := state.NewStateFromState(map[string]string{
+	s := state.NewStateFromState(map[string]string{
 		"test":       "test",
 		"sequence:0": "true",
 		"sequence:1": "true",
 	})
-	Expect(b).ToNot(BeNil())
-	Expect(b.State).To(Equal(map[string]string{
+	Expect(s).ToNot(BeNil())
+	Expect(s.State).To(Equal(map[string]string{
 		"sequence:0": "1",
 		"sequence:1": "1",
 	}))
+}
+
+func Test_State_GetNewSequenceKey_ReturnsFirstFreeInSequence(t *testing.T) {
+	RegisterTestingT(t)
+
+	s := state.NewState()
+	Expect(s.GetNewSequenceKey()).To(Equal("sequence:0"))
+
+	s.SetState(map[string]string{
+		"sequence:0": "1",
+	})
+	Expect(s.GetNewSequenceKey()).To(Equal("sequence:1"))
+
+	s.SetState(map[string]string{
+		"sequence:0": "1",
+		"sequence:1": "1",
+	})
+	Expect(s.GetNewSequenceKey()).To(Equal("sequence:2"))
+
+	s.SetState(map[string]string{
+		"sequence:0": "1",
+		"sequence:1": "1",
+		"sequence:2": "1",
+	})
+	Expect(s.GetNewSequenceKey()).To(Equal("sequence:3"))
 }
