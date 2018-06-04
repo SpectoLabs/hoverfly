d2624257bd67a78d89c43e51a0ee40a9c2fab2b5
Benji Hooper <benji@sixp.in>
2018-06-04 10:37:05 +0100

    Updated example simulations in docs to v4 schema

--- docs/pages/simulations/update.sh
+++ docs/pages/simulations/update.sh
@@ -2,18 +2,18 @@
 
 echo "You have the latest Hoverfly, right?"
 hoverctl start
-hoverctl import all-matchers-simulation.json
-hoverctl export all-matchers-simulation.json
-hoverctl import basic-delay-simulation.json
-hoverctl export basic-delay-simulation.json
-hoverctl import basic-encoded-simulation.json
-hoverctl export basic-encoded-simulation.json
-hoverctl import basic-simulation.json
-hoverctl export basic-simulation.json
-hoverctl import get-method-delay-simulation.json
-hoverctl export get-method-delay-simulation.json
-hoverctl import multiple-hosts-delay-simulation.json
-hoverctl export multiple-hosts-delay-simulation.json
-hoverctl import multiple-locations-delay-simulation.json
-hoverctl export multiple-locations-delay-simulation.json
+hoverctl import pages/simulations/all-matchers-simulation.json
+hoverctl export pages/simulations/all-matchers-simulation.json
+hoverctl import pages/simulations/basic-delay-simulation.json
+hoverctl export pages/simulations/basic-delay-simulation.json
+hoverctl import pages/simulations/basic-encoded-simulation.json
+hoverctl export pages/simulations/basic-encoded-simulation.json
+hoverctl import pages/simulations/basic-simulation.json
+hoverctl export pages/simulations/basic-simulation.json
+hoverctl import pages/simulations/get-method-delay-simulation.json
+hoverctl export pages/simulations/get-method-delay-simulation.json
+hoverctl import pages/simulations/multiple-hosts-delay-simulation.json
+hoverctl export pages/simulations/multiple-hosts-delay-simulation.json
+hoverctl import pages/simulations/multiple-locations-delay-simulation.json
+hoverctl export pages/simulations/multiple-locations-delay-simulation.json
 hoverctl stop
