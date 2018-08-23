e65a7068ccae54a356253d11561a0613f518e3bd
Benji Hooper <benji@sixp.in>
2018-08-23 13:18:17 +0100

    Added PACFile configuration and /api/v2/hoverfly/pac endpoints for getting and setting PAC file

---
+++ core/handlers/v2/hoverfly_pac_handler.go
@@ -0,0 +1,56 @@
+package v2
+
+import (
+	"io/ioutil"
+	"net/http"
+
+	"github.com/SpectoLabs/hoverfly/core/handlers"
+	"github.com/codegangsta/negroni"
+	"github.com/go-zoo/bone"
+)
+
+type HoverflyPAC interface {
+	GetPACFile() []byte
+	SetPACFile([]byte)
+}
+
+type HoverflyPACHandler struct {
+	Hoverfly HoverflyPAC
+}
+
+func (this *HoverflyPACHandler) RegisterRoutes(mux *bone.Mux, am *handlers.AuthHandler) {
+	mux.Get("/api/v2/hoverfly/pac", negroni.New(
+		negroni.HandlerFunc(am.RequireTokenAuthentication),
+		negroni.HandlerFunc(this.Get),
+	))
+
+	mux.Put("/api/v2/hoverfly/pac", negroni.New(
+		negroni.HandlerFunc(am.RequireTokenAuthentication),
+		negroni.HandlerFunc(this.Put),
+	))
+	mux.Options("/api/v2/hoverfly/pac", negroni.New(
+		negroni.HandlerFunc(this.Options),
+	))
+}
+
+func (this *HoverflyPACHandler) Get(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+	pacFile := this.Hoverfly.GetPACFile()
+
+	handlers.WriteResponse(w, pacFile)
+}
+
+func (this *HoverflyPACHandler) Put(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+	bodyBytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		handlers.WriteErrorResponse(w, err.Error(), 400)
+		return
+	}
+	this.Hoverfly.SetPACFile(bodyBytes)
+
+	this.Get(w, req, next)
+}
+
+func (this *HoverflyPACHandler) Options(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	w.Header().Add("Allow", "OPTIONS, GET, PUT")
+	handlers.WriteResponse(w, []byte(""))
+}
