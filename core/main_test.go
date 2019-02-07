0748d91b7abbc23578982685423c082376592c3b
Tommy Situ <tommy.situ@specto.io>
2019-02-07 11:42:55 +0000

    Add profiling test

---
+++ core/main_test.go
@@ -0,0 +1,91 @@
+package hoverfly
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/SpectoLabs/hoverfly/core/handlers/v2"
+	. "github.com/onsi/gomega"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func BenchmarkNewWebserverProxy(b *testing.B) {
+
+	RegisterTestingT(b)
+	hoverfly := NewHoverflyWithConfiguration(&Configuration{
+		Webserver: true,
+		ProxyPort: "8500",
+		AdminPort: "8888",
+		Mode:      "simulate",
+	})
+
+	simulation := v2.SimulationViewV5{}
+	_ = json.Unmarshal([]byte(`{
+	"data": {
+		"pairs": [{
+			"response": {
+				"status": 200,
+				"body": "5iNe8dxWH5Ca8pZqAfEHv3rgC0SsvKNLu6o3K",
+				"encodedBody": false,
+				"headers": {
+					"Accept-Ranges": ["bytes"],
+					"Cache-Control": ["max-age=3600"],
+					"Connection": ["keep-alive"],
+					"Content-Type": ["text/html; charset=utf-8"],
+					"Date": ["Tue, 30 May 2017 13:23:09 GMT"],
+					"Etag": ["\"82b6bafbc0c4af5e3886d07802f4b62d\""],
+					"Hoverfly": ["Was-Here"],
+					"Last-Modified": ["Fri, 19 May 2017 10:59:12 GMT"],
+					"Server": ["nginx"],
+					"Strict-Transport-Security": ["max-age=31556926"],
+					"Transfer-Encoding": ["chunked"],
+					"Vary": ["Accept-Encoding"]
+				}
+			},
+			"request": {
+				"path": [{
+					"matcher": "exact",
+					"value": "/bar"
+				}],
+				"method": [{
+					"matcher": "exact",
+					"value": "GET"
+				}],
+				"query": {},
+				"body": [{
+					"matcher": "exact",
+					"value": ""
+				}]
+			}
+		}],
+		"globalActions": {
+			"delays": []
+		}
+	},
+	"meta": {
+		"schemaVersion": "v5",
+		"hoverflyVersion": "v0.17.0",
+		"timeExported": "2017-05-30T14:23:44+01:00"
+	}
+}`), &simulation)
+
+	fmt.Println(hoverfly.StartProxy())
+	time.Sleep(time.Second)
+	hoverfly.PutSimulation(simulation)
+	request, _ := http.NewRequest(http.MethodGet, "http://localhost:8500/bar", nil)
+	var resp *http.Response
+
+	b.Run("BenchmarkNewWebserverProxy", func(b *testing.B) {
+
+		for n := 0; n < b.N; n++ {
+			resp = hoverfly.processRequest(request)
+			//http.Get("http://localhost:8500/bar")
+		}
+	})
+
+	//Expect(err).To(BeNil())
+	Expect(resp.StatusCode).To(Equal(200))
+
+	hoverfly.StopProxy()
+}
