c6fcbb1b9340094fe39f17aecbf73033f46de31b
Tommy Situ <tommy.situ@specto.io>
2019-02-08 13:49:53 +0000

    Remove cache response encoder

--- core/models/cached_response.go
+++ core/models/cached_response.go
@@ -1,33 +1,7 @@
 package models
 
-import (
-	"bytes"
-	"encoding/gob"
-)
-
 type CachedResponse struct {
 	Request      RequestDetails
 	MatchingPair *RequestMatcherResponsePair
 	ClosestMiss  *ClosestMiss
-}
-
-func (this CachedResponse) Encode() ([]byte, error) {
-	buf := new(bytes.Buffer)
-	enc := gob.NewEncoder(buf)
-	err := enc.Encode(this)
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-func NewCachedResponseFromBytes(data []byte) (*CachedResponse, error) {
-	var cachedResponse *CachedResponse
-	buf := bytes.NewBuffer(data)
-	dec := gob.NewDecoder(buf)
-	err := dec.Decode(&cachedResponse)
-	if err != nil {
-		return nil, err
-	}
-	return cachedResponse, nil
 }
