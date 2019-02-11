c7abd04c9b28b901cb3a1fc3b95e895a3106e972
Tommy Situ <tommy.situ@specto.io>
2019-02-11 15:12:17 +0000

    Create sub benchmarks

--- core/benchmark_test.go
+++ core/benchmark_test.go
@@ -13,6 +13,7 @@ import (
 func BenchmarkProcessRequest(b *testing.B) {
 
 	RegisterTestingT(b)
+
 	hoverfly := NewHoverflyWithConfiguration(&Configuration{
 		Webserver: true,
 		ProxyPort: "8500",
@@ -20,55 +21,55 @@ func BenchmarkProcessRequest(b *testing.B) {
 		Mode:      "simulate",
 	})
 
-//	simulation := v2.SimulationViewV5{}
-//	_ = json.Unmarshal([]byte(`{
-//	"data": {
-//		"pairs": [{
-//			"response": {
-//				"status": 200,
-//				"body": "5iNe8dxWH5Ca8pZqAfEHv3rgC0SsvKNLu6o3K",
-//				"encodedBody": false,
-//				"headers": {
-//					"Accept-Ranges": ["bytes"],
-//					"Cache-Control": ["max-age=3600"],
-//					"Connection": ["keep-alive"],
-//					"Content-Type": ["text/html; charset=utf-8"],
-//					"Date": ["Tue, 30 May 2017 13:23:09 GMT"],
-//					"Etag": ["\"82b6bafbc0c4af5e3886d07802f4b62d\""],
-//					"Hoverfly": ["Was-Here"],
-//					"Last-Modified": ["Fri, 19 May 2017 10:59:12 GMT"],
-//					"Server": ["nginx"],
-//					"Strict-Transport-Security": ["max-age=31556926"],
-//					"Transfer-Encoding": ["chunked"],
-//					"Vary": ["Accept-Encoding"]
-//				}
-//			},
-//			"request": {
-//				"path": [{
-//					"matcher": "exact",
-//					"value": "/bar"
-//				}],
-//				"method": [{
-//					"matcher": "exact",
-//					"value": "GET"
-//				}],
-//				"query": {},
-//				"body": [{
-//					"matcher": "exact",
-//					"value": ""
-//				}]
-//			}
-//		}],
-//		"globalActions": {
-//			"delays": []
-//		}
-//	},
-//	"meta": {
-//		"schemaVersion": "v5",
-//		"hoverflyVersion": "v0.17.0",
-//		"timeExported": "2017-05-30T14:23:44+01:00"
-//	}
-//}`), &simulation)
+	simulation := v2.SimulationViewV5{}
+	_ = json.Unmarshal([]byte(`{
+	"data": {
+		"pairs": [{
+			"response": {
+				"status": 200,
+				"body": "5iNe8dxWH5Ca8pZqAfEHv3rgC0SsvKNLu6o3K",
+				"encodedBody": false,
+				"headers": {
+					"Accept-Ranges": ["bytes"],
+					"Cache-Control": ["max-age=3600"],
+					"Connection": ["keep-alive"],
+					"Content-Type": ["text/html; charset=utf-8"],
+					"Date": ["Tue, 30 May 2017 13:23:09 GMT"],
+					"Etag": ["\"82b6bafbc0c4af5e3886d07802f4b62d\""],
+					"Hoverfly": ["Was-Here"],
+					"Last-Modified": ["Fri, 19 May 2017 10:59:12 GMT"],
+					"Server": ["nginx"],
+					"Strict-Transport-Security": ["max-age=31556926"],
+					"Transfer-Encoding": ["chunked"],
+					"Vary": ["Accept-Encoding"]
+				}
+			},
+			"request": {
+				"path": [{
+					"matcher": "exact",
+					"value": "/bar"
+				}],
+				"method": [{
+					"matcher": "exact",
+					"value": "GET"
+				}],
+				"query": {},
+				"body": [{
+					"matcher": "exact",
+					"value": ""
+				}]
+			}
+		}],
+		"globalActions": {
+			"delays": []
+		}
+	},
+	"meta": {
+		"schemaVersion": "v5",
+		"hoverflyVersion": "v0.17.0",
+		"timeExported": "2017-05-30T14:23:44+01:00"
+	}
+}`), &simulation)
 
 	templated := v2.SimulationViewV5{}
 	_ = json.Unmarshal([]byte(`{
@@ -121,29 +122,31 @@ func BenchmarkProcessRequest(b *testing.B) {
 	}
 }`), &templated)
 
+	benchmarks := []struct{
+		name string
+		simulation v2.SimulationViewV5
+	} {
+		{"Simple simulation", simulation},
+		{"Templated simulation", templated},
+	}
 
 	fmt.Println(hoverfly.StartProxy())
 	time.Sleep(time.Second)
 	request, _ := http.NewRequest(http.MethodGet, "http://localhost:8500/bar", nil)
 	var resp *http.Response
-	//hoverfly.PutSimulation(simulation)
-	//
-	//b.Run("Simple simulation", func(b *testing.B) {
-	//
-	//	for n := 0; n < b.N; n++ {
-	//		resp = hoverfly.processRequest(request)
-	//	}
-	//})
 
+	for _, bm := range benchmarks {
+		hoverfly.DeleteSimulation()
+		hoverfly.PutSimulation(bm.simulation)
 
-	hoverfly.PutSimulation(templated)
+		b.Run(bm.name, func(b *testing.B) {
 
-	b.Run("Templated simulation", func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				resp = hoverfly.processRequest(request)
+			}
+		})
+	}
 
-		for n := 0; n < b.N; n++ {
-			resp = hoverfly.processRequest(request)
-		}
-	})
 
 	Expect(resp.StatusCode).To(Equal(200))
 
