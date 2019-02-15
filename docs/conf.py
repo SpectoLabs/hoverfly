4ea18bcc08303f25cc8c541190ebf6b6773f0e5d
Tommy Situ <tommy.situ@specto.io>
2019-02-15 15:58:29 +0000

    Updated hoverfly version to v1.0.0-rc.2

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ copyright = u'2017, SpectoLabs'
 author = u'SpectoLabs'
 
 
-version = 'v1.0.0-rc.1'
+version = 'v1.0.0-rc.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
