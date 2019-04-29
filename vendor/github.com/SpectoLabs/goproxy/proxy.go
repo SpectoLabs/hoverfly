f96f95e7ce3ceace7397143b6aced0b01fe194ab
Tommy Situ <tommy.situ@specto.io>
2019-04-29 20:59:12 +0100

    Roll back goproxy update which impacts performance

--- vendor/github.com/SpectoLabs/goproxy/proxy.go
+++ vendor/github.com/SpectoLabs/goproxy/proxy.go
@@ -16,8 +16,6 @@ type ProxyHttpServer struct {
 	// session variable must be aligned in i386
 	// see http://golang.org/src/pkg/sync/atomic/doc.go#L41
 	sess int64
-	// KeepDestinationHeaders indicates the proxy should retain any headers present in the http.Response before proxying
-	KeepDestinationHeaders bool
 	// setting Verbose to true will log information on each request sent to the proxy
 	Verbose         bool
 	Logger          *log.Logger
@@ -34,11 +32,9 @@ type ProxyHttpServer struct {
 
 var hasPort = regexp.MustCompile(`:\d+$`)
 
-func copyHeaders(dst, src http.Header, keepDestHeaders bool) {
-	if !keepDestHeaders {
-		for k := range dst {
-			dst.Del(k)
-		}
+func copyHeaders(dst, src http.Header) {
+	for k, _ := range dst {
+		dst.Del(k)
 	}
 	for k, vs := range src {
 		dst[k] = vs
@@ -151,7 +147,7 @@ func (proxy *ProxyHttpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		// if origBody != resp.Body {
 		// resp.Header.Del("Content-Length")
 		// }
-		copyHeaders(w.Header(), resp.Header, proxy.KeepDestinationHeaders)
+		copyHeaders(w.Header(), resp.Header)
 		w.WriteHeader(resp.StatusCode)
 		nr, err := io.Copy(w, resp.Body)
 		if err := resp.Body.Close(); err != nil {
@@ -175,9 +171,9 @@ func NewProxyHttpServer() *ProxyHttpServer {
 		NonproxyHandler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			http.Error(w, "This is a proxy server. Does not respond to non-proxy requests.", 500)
 		}),
-		Tr: &http.Transport{TLSClientConfig: tlsClientSkipVerify, Proxy: http.ProxyFromEnvironment},
+		Tr: &http.Transport{TLSClientConfig: tlsClientSkipVerify,
+			Proxy: http.ProxyFromEnvironment},
 	}
 	proxy.ConnectDial = dialerFromEnv(&proxy)
-
 	return &proxy
 }
