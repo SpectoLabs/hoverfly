7679eff095a1e0f3e2d0b98cca9301d4dfc43471
Tommy Situ <tommy.situ@specto.io>
2019-04-30 15:33:29 +0100

    Updated hoverfly version to v1.0.0

--- core/hoverfly.go
+++ core/hoverfly.go
@@ -61,7 +61,7 @@ func NewHoverfly() *Hoverfly {
 		responsesDiff:  make(map[v2.SimpleRequestDefinitionView][]v2.DiffReport),
 	}
 
-	hoverfly.version = "v1.0.0-rc.2"
+	hoverfly.version = "v1.0.0"
 
 	log.AddHook(hoverfly.StoreLogsHook)
 
--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ copyright = u'2017, SpectoLabs'
 author = u'SpectoLabs'
 
 
-version = 'v1.0.0-rc.2'
+version = 'v1.0.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
