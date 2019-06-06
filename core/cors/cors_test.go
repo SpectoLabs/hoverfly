44d024732b900e79449e51aa402f77f6c90400e9
Tommy Situ <tommy.situ@specto.io>
2019-06-06 18:35:04 +0100

    #843 Only return Expose-Headers if it's not empty

--- core/cors/cors_test.go
+++ core/cors/cors_test.go
@@ -157,3 +157,23 @@ func Test_AddCORSHeaders_ShouldNotSetAllowCredentialsHeaderIfFalse(t *testing.T)
 	Expect(resp.Header.Get("Access-Control-Allow-Credentials")).To(Equal(""))
 	Expect(resp.Header.Get("Access-Control-Expose-Headers")).To(Equal(""))
 }
+
+func Test_AddCORSHeaders_ShouldSetExposeHeadersIfPresent(t *testing.T) {
+	RegisterTestingT(t)
+
+	unit := DefaultCORSConfigs()
+	unit.ExposeHeaders = "Content-Type"
+
+
+	r, err := http.NewRequest(http.MethodGet, "http://somehost.com", nil)
+	Expect(err).To(BeNil())
+	r.Header.Set("Origin", "http://originhost.com")
+
+	resp := &http.Response{}
+	unit.AddCORSHeaders(r, resp)
+
+	Expect(resp).ToNot(BeNil())
+	Expect(resp.Header.Get("Access-Control-Allow-Origin")).To(Equal("http://originhost.com"))
+	Expect(resp.Header.Get("Access-Control-Allow-Credentials")).To(Equal("true"))
+	Expect(resp.Header.Get("Access-Control-Expose-Headers")).To(Equal("Content-Type"))
+}
