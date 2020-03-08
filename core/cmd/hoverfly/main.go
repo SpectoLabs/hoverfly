d20408d19f123b7d844cda097e1448fe48a667a5
Nikita Safonov <ns3777k@gmail.com>
2020-03-09 02:44:53 +0300

    omit os.Getwd call in main.go

--- core/cmd/hoverfly/main.go
+++ core/cmd/hoverfly/main.go
@@ -195,16 +195,10 @@ func isFlagPassed(name string) bool {
 func main() {
 	hoverfly := hv.NewHoverfly()
 
-	dir, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
 	flag.Var(&importFlags, "import", "Import from file or from URL (i.e. '-import my_service.json' or '-import http://mypage.com/service_x.json'")
 	flag.Var(&destinationFlags, "dest", "Specify which hosts to process (i.e. '-dest fooservice.org -dest barservice.org -dest catservice.org') - other hosts will be ignored will passthrough'")
 	flag.Var(&logOutputFlags, "logs-output", "Specify locations for output logs, options are \"console\" and \"file\" (default \"console\")")
-	flag.StringVar(&responseBodyFilesPath, "response-body-files-path", dir, "When a response contains a relative bodyFile, it will be resolved against this path")
+	flag.StringVar(&responseBodyFilesPath, "response-body-files-path", "", "When a response contains a relative bodyFile, it will be resolved against this path (default is CWD)")
 
 	flag.Parse()
 
