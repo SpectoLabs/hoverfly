2dcabf94aa17d49020b0d66dcfc07c4e2950c164
Nikita Safonov <ns3777k@gmail.com>
2020-05-20 00:26:57 +0300

    fix logNormal response delay in middleware and add tests (#935)

--- functional-tests/core/testdata/response_lognormal_delay_middleware.py
+++ functional-tests/core/testdata/response_lognormal_delay_middleware.py
@@ -6,7 +6,6 @@ import logging
 logging.basicConfig(filename='response_delay_middleware_request.log', level=logging.DEBUG)
 logging.debug('Middleware "modify_request" called')
 
-
 def main():
     data = sys.stdin.readlines()
     # this is a json string in one line so we are interested in that one line
@@ -18,7 +17,7 @@ def main():
     payload_dict['response']['body'] = "CHANGED_RESPONSE_BODY"
     payload_dict['response']['status'] = 200
     payload_dict['response']['headers'] = {'Content-Length': ["21"]}
-    payload_dict['response']['fixedDelay'] = 130
+    payload_dict['response']['logNormalDelay'] = {'min': 130, 'max': 160, 'mean': 150, 'median': 140}
 
     # returning new payload
     print(json.dumps(payload_dict))
