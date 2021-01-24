b9c581c55450aeeba1494da1505f9b6ab45c6462
Nikita Safonov <ns3777k@gmail.com>
2021-01-24 18:06:01 +0300

    remove old isURL

--- core/import.go
+++ core/import.go
@@ -6,15 +6,11 @@ import (
 	"github.com/SpectoLabs/hoverfly/core/delay"
 	"github.com/SpectoLabs/hoverfly/core/state"
 	"github.com/SpectoLabs/hoverfly/core/util"
-	"net/url"
-	"os"
-	"path"
-	"regexp"
-	"strconv"
-	"strings"
-
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path"
+	"strconv"
 
 	"github.com/SpectoLabs/hoverfly/core/handlers/v2"
 	"github.com/SpectoLabs/hoverfly/core/models"
@@ -49,29 +45,6 @@ func (hf *Hoverfly) Import(uri string) error {
 		return hf.ImportFromDisk(uri)
 	}
 	return fmt.Errorf("Failed to import payloads, given file '%s' does not exist", uri)
-}
-
-// URL is regexp to match http urls
-const URL string = `^((ftp|https?):\/\/)(\S+(:\S*)?@)?((([1-9]\d?|1\d\d|2[01]\d|22[0-3])(\.(1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.([0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(([a-zA-Z0-9]+([-\.][a-zA-Z0-9]+)*)|((www\.)?))?(([a-z\x{00a1}-\x{ffff}0-9]+-?-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.([a-z\x{00a1}-\x{ffff}]{2,}))?))(:(\d{1,5}))?((\/|\?|#)[^\s]*)?$`
-
-var rxURL = regexp.MustCompile(URL)
-
-func isURL(str string) bool {
-	if str == "" || len(str) >= 2083 || len(str) <= 3 || strings.HasPrefix(str, ".") {
-		return false
-	}
-	u, err := url.Parse(str)
-	if err != nil {
-		return false
-	}
-	if strings.HasPrefix(u.Host, ".") {
-		return false
-	}
-	if u.Host == "" && (u.Path != "" && !strings.Contains(u.Path, ".")) {
-		return false
-	}
-
-	return rxURL.MatchString(str)
 }
 
 func exists(path string) (bool, error) {
