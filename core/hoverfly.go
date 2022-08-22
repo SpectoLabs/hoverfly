ac4cf3c7e2adaa0bd0527a5bbee0fb38d119bd2e
Tommy Situ <tommy_situ@hotmail.com>
2022-08-22 21:56:04 +0100

    Updated hoverfly version to v1.3.7

--- core/hoverfly.go
+++ core/hoverfly.go
@@ -63,7 +63,7 @@ func NewHoverfly() *Hoverfly {
 		responsesDiff:  make(map[v2.SimpleRequestDefinitionView][]v2.DiffReport),
 	}
 
-	hoverfly.version = "v1.3.6"
+	hoverfly.version = "v1.3.7"
 
 	log.AddHook(hoverfly.StoreLogsHook)
 
