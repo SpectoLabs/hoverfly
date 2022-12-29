765627b424b96d3f1eb7a12d17980ed5a4b1bd0a
kapish.m <kapish15026@iiitd.ac.in>
2022-12-29 23:46:57 +0530

    Handle array of string and empty interfaces for match

--- core/matching/matchers/contains_match.go
+++ core/matching/matchers/contains_match.go
@@ -1,6 +1,7 @@
 package matchers
 
 import (
+	"reflect"
 	"strings"
 
 	"github.com/SpectoLabs/hoverfly/core/util"
@@ -9,9 +10,19 @@ import (
 var Contains = "contains"
 
 func ContainsMatch(match interface{}, toMatch string) bool {
-	matchStringArr, ok := match.([]string)
-	if !ok {
+	val := reflect.ValueOf(match)
+	if val.Kind() != reflect.Slice {
 		return false
+	}
+	var matchStringArr []string
+	for i := 0; i < val.Len(); i++ {
+		currentValue := val.Index(i)
+		if currentValue.Kind() == reflect.Interface {
+			matchStringArr = append(matchStringArr, currentValue.Elem().String())
+		} else {
+			matchStringArr = append(matchStringArr, currentValue.String())
+		}
+
 	}
 	toMatchArr := strings.Split(toMatch, ";")
 	return util.Contains(matchStringArr, toMatchArr)
