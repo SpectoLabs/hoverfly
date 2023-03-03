e3ced22ac31c8cf8b7f45b8c8a197fa5b774ec62
kapishmalik <kapish15026@iiitd.ac.in>
2023-03-03 17:55:01 +0530

    add functional tests for post serve actions

--- functional-tests/functional_tests.go
+++ functional-tests/functional_tests.go
@@ -19,7 +19,7 @@ import (
 
 	"io"
 
-	"github.com/SpectoLabs/hoverfly/core/handlers/v2"
+	v2 "github.com/SpectoLabs/hoverfly/core/handlers/v2"
 	"github.com/dghubble/sling"
 	"github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -121,6 +121,48 @@ func (this Hoverfly) GetMode() *v2.ModeView {
 	Expect(err).To(BeNil())
 
 	return currentState
+}
+
+func (this Hoverfly) GetAllPostServeAction() *v2.PostServeActionDetailsView {
+	PostServeActionDetailsView := &v2.PostServeActionDetailsView{}
+	resp := DoRequest(sling.New().Get(fmt.Sprintf("http://localhost:%v/api/v2/hoverfly/post-serve-actions", this.adminPort)))
+
+	body, err := ioutil.ReadAll(resp.Body)
+	Expect(err).To(BeNil())
+
+	err = json.Unmarshal(body, PostServeActionDetailsView)
+	Expect(err).To(BeNil())
+
+	return PostServeActionDetailsView
+}
+
+func (this Hoverfly) SetPostServeAction(actionName, binary, scriptContent string, delayInMs int) *v2.PostServeActionDetailsView {
+	actionView := v2.ActionView{ActionName: actionName, Binary: binary, DelayInMs: delayInMs, ScriptContent: scriptContent}
+
+	resp := DoRequest(sling.New().Put(fmt.Sprintf("http://localhost:%v/api/v2/hoverfly/post-serve-actions", this.adminPort)).BodyJSON(actionView))
+
+	PostServeActionDetailsView := &v2.PostServeActionDetailsView{}
+	body, err := ioutil.ReadAll(resp.Body)
+	Expect(err).To(BeNil())
+
+	err = json.Unmarshal(body, PostServeActionDetailsView)
+	Expect(err).To(BeNil())
+
+	return PostServeActionDetailsView
+}
+
+func (this Hoverfly) DeletePostServeAction(actionName string) *v2.PostServeActionDetailsView {
+
+	resp := DoRequest(sling.New().Delete(fmt.Sprintf("http://localhost:%v/api/v2/hoverfly/post-serve-actions/%v", this.adminPort, actionName)))
+
+	PostServeActionDetailsView := &v2.PostServeActionDetailsView{}
+	body, err := ioutil.ReadAll(resp.Body)
+	Expect(err).To(BeNil())
+
+	err = json.Unmarshal(body, PostServeActionDetailsView)
+	Expect(err).To(BeNil())
+
+	return PostServeActionDetailsView
 }
 
 func (this Hoverfly) SetMode(mode string) {
