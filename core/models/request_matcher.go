8eb2b2b36442ba8199d19c61be840e64a8f4b7f8
kapishmalik <kapish15026@iiitd.ac.in>
2023-03-13 00:55:48 +0530

    small bug fix

--- core/models/request_matcher.go
+++ core/models/request_matcher.go
@@ -313,7 +313,8 @@ func (this RequestMatcher) ToEagerlyCacheable() *RequestDetails {
 	if this.Query != nil && len(*this.Query) > 0 {
 		for key, valueMatchers := range *this.Query {
 			for _, valueMatcher := range valueMatchers {
-				if valueMatcher.Matcher != matchers.Exact || ((valueMatcher.Matcher != matchers.Array) && (valueMatcher.Config == nil || len(valueMatcher.Config) == 0)) {
+				if valueMatcher.Matcher != matchers.Exact 
+				|| !(valueMatcher.Matcher == matchers.Array && (valueMatcher.Config == nil || len(valueMatcher.Config) == 0)) {
 					return nil
 				}
 				query[key] = []string{valueMatcher.Value.(string)}
