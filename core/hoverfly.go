654561266cdef27355046c09cef19af923f3b716
Tommy Situ <tommy_situ@hotmail.com>
2024-01-07 23:03:49 +0000

    Updated hoverfly version to v1.6.2

--- core/hoverfly.go
+++ core/hoverfly.go
@@ -66,7 +66,7 @@ func NewHoverfly() *Hoverfly {
 		PostServeActionDetails: action.NewPostServeActionDetails(),
 	}
 
-	hoverfly.version = "v1.6.1"
+	hoverfly.version = "v1.6.2"
 
 	log.AddHook(hoverfly.StoreLogsHook)
 
--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ project = 'Hoverfly'
 copyright = '2023, SpectoLabs'
 author = 'SpectoLabs'
 
-version = 'v1.6.1'
+version = 'v1.6.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
