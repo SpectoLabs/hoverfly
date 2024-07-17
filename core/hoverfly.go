5d9cbab8b01bb7be694ed247fe3ee21ec92171d4
Tommy Situ <tommy_situ@hotmail.com>
2024-07-17 23:25:12 +0100

    Updated hoverfly version to v1.10.1

--- core/hoverfly.go
+++ core/hoverfly.go
@@ -66,7 +66,7 @@ func NewHoverfly() *Hoverfly {
 		PostServeActionDetails: action.NewPostServeActionDetails(),
 	}
 
-	hoverfly.version = "v1.10.0"
+	hoverfly.version = "v1.10.1"
 
 	log.AddHook(hoverfly.StoreLogsHook)
 
