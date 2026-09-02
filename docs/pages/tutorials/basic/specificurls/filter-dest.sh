7f7c8cef6476216d2e8c9c2a3b2f812c08f28e88
spramanigit <sspramani@gmail.com>
2026-09-01 21:50:06 -0400

    docs: replace dead jsontest.com URLs in filter-dest.sh

--- docs/pages/tutorials/basic/specificurls/filter-dest.sh
+++ docs/pages/tutorials/basic/specificurls/filter-dest.sh
@@ -1,8 +1,8 @@
 hoverctl start
 hoverctl destination "ip" 
 hoverctl mode capture
-curl --proxy http://localhost:8500 http://ip.jsontest.com
-curl --proxy http://localhost:8500 http://time.jsontest.com
+curl --proxy http://localhost:8500 https://api.ipify.org
+curl --proxy http://localhost:8500 https://httpbin.org
 hoverctl logs
 hoverctl export simulation.json
 hoverctl stop
