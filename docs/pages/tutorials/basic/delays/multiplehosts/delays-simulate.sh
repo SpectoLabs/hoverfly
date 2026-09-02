bc4ed0da95fe3036afe86bc589ebb431ba777c56
spramanigit <sspramani@gmail.com>
2026-09-01 21:41:10 -0400

    docs: replace dead jsontest.com URLs in delays-simulate.sh

--- docs/pages/tutorials/basic/delays/multiplehosts/delays-simulate.sh
+++ docs/pages/tutorials/basic/delays/multiplehosts/delays-simulate.sh
@@ -1,5 +1,5 @@
 hoverctl start
 hoverctl import simulation.json
-curl --proxy localhost:8500 http://time.jsontest.com
-curl --proxy localhost:8500 http://date.jsontest.com
+curl --proxy localhost:8500 https://api.ipify.org
+curl --proxy localhost:8500 https://httpbin.org
 hoverctl stop
