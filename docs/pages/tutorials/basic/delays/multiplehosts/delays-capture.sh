3b08321934d12d85cb8d1f83890fea2a65b51aef
spramanigit <sspramani@gmail.com>
2026-09-01 21:40:22 -0400

    docs: replace dead jsontest.com URLs in delays-capture.sh

--- docs/pages/tutorials/basic/delays/multiplehosts/delays-capture.sh
+++ docs/pages/tutorials/basic/delays/multiplehosts/delays-capture.sh
@@ -1,6 +1,6 @@
 hoverctl start
 hoverctl mode capture
-curl --proxy localhost:8500 http://time.jsontest.com
-curl --proxy localhost:8500 http://date.jsontest.com
+curl --proxy localhost:8500 https://api.ipify.org
+curl --proxy localhost:8500 https://httpbin.org
 hoverctl export simulation.json
 hoverctl stop
